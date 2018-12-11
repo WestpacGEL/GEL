869828f14dc98fa42b6937020b26bf1bd61b9551
Joss Mackison <joss.mackison@gmail.com>
2018-12-11 14:43:00 +1100

    docz attempt

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+export default {
+	// codeSandbox: false,
+	// debug: true,
+	description: 'The styleguide for component use within the GEL platform.',
+	files: '**/*.mdx',
+	src: './',
+	// theme: 'docz-theme.js',
+	themeConfig: {
+		mode: 'dark',
+	},
+	title: 'GEL Style Guide',
+	// wrapper: 'docs/wrapper',
+};
