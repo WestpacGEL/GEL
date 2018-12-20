21d54cb304f1b27f7b5ca39cea95d65b0776c69b
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-12-20 15:20:40 +1100

    cleaning
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- components/core/examples/00-example.js
+++ components/core/examples/00-example.js
@@ -1,11 +1,4 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import { ThemeProvider } from '../src/index.js';
-
-export default () => (
-	<div>
-		I think it's working???
-		<ThemeProvider />
-	</div>
-);
+export default () => <div>I think it's working???</div>;
--- components/hooks/examples/00-use-container-query.js
+++ components/hooks/examples/00-use-container-query.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { useContainerQuery } from '../src';
+import { useContainerQuery } from '../src/';
 
 export default () => {
 	const textareaRef = useRef();
