4a21e137e693e34c757fd2e803fc42528cdf8cec
Joss Mackison <joss.mackison@gmail.com>
2019-01-08 17:47:47 +1100

    remove unused test files so build passes

--- components/icon/tests/integration/grid.cypress.js
+++
@@ -1,9 +0,0 @@
-describe('Icon', () => {
-	before(() => {
-		cy.visit(`http://localhost:8080/`);
-	});
-
-	it('TODO', () => {
-		// cy.get('...').click();
-	});
-});
--- components/icon/tests/unit/grid.spec.js
+++
@@ -1,3 +0,0 @@
-test('TODO', () => {
-	// expect(stringVal(5)).toBe('5px');
-});
