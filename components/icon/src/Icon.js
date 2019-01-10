4ee65c8d73653b9a8149a362ca45d4d7beff752f
Joss Mackison <joss.mackison@gmail.com>
2019-01-11 10:24:25 +1100

    prettier

--- components/icon/src/Icon.js
+++ components/icon/src/Icon.js
@@ -43,7 +43,13 @@ export const Icon = ({ children, color, label, size }) => {
 	// improve CSS reuse.
 	return (
 		<Wrapper color={color} size={sizeInt} style={{ color }}>
-			<svg aria-label={label} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" focusable="false">
+			<svg
+				aria-label={label}
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				role="img"
+				focusable="false"
+			>
 				{children}
 			</svg>
 		</Wrapper>
