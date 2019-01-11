f4a20709e9aba8d08c0bbcf83ccf61e8c00bc9b4
Joss Mackison <joss.mackison@gmail.com>
2019-01-11 11:15:07 +1100

    brand is now supplied to the GEL theme provider as an Object, not a key

--- components/core/src/Theme.js
+++ components/core/src/Theme.js
@@ -1,54 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 
-// TODO should come from @wespac/brand/*
-const brands = {
-	BOM: {
-		colors: {
-			gray: '#666',
-			primary: '#1F252C',
-		},
-	},
-	BSA: {
-		colors: {
-			gray: '#666',
-			primary: '#002F6C',
-		},
-	},
-	BTFG: {
-		colors: {
-			gray: '#666',
-			primary: '#7998AC',
-		},
-	},
-	STG: {
-		colors: {
-			gray: '#666',
-			primary: '#78BE20',
-		},
-	},
-	WBC: {
-		colors: {
-			gray: '#666',
-			primary: '#D5002B',
-		},
-	},
-	WBG: {
-		colors: {
-			gray: '#666',
-			primary: '#9F0029',
-		},
-	},
+const defaultThemeFn = provided => provided;
+const isEmpty = obj => Object.keys(obj).length === 0;
+
+export const GEL = ({ brand, theme, ...props }) => {
+	if (typeof theme !== 'function') {
+		throw new Error('The theme must be a function.');
+	}
+	if (!brand || isEmpty(brand)) {
+		throw new Error('A brand is required.');
+	}
+
+	const themeObj = theme(brand);
+
+	if (!themeObj || isEmpty(themeObj)) {
+		throw new Error('The brand or theme provided is invalid.');
+	}
+
+	return <ThemeProvider theme={themeObj} {...props} />;
 };
 
-const defaultBrandKey = 'WBC';
-const defaultThemeFn = provided => provided;
-
-export const GEL = ({ brand, theme, ...props }) => (
-	<ThemeProvider theme={theme(brands[brand])} {...props} />
-);
-
 GEL.defaultProps = {
-	brand: defaultBrandKey,
 	theme: defaultThemeFn,
 };
