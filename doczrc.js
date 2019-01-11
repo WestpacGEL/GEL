451390299b9d46fc03d9d90ce6db54673492e20f
Joss Mackison <joss.mackison@gmail.com>
2019-01-11 18:01:23 +1100

    little fixes

--- doczrc.js
+++ doczrc.js
@@ -3,7 +3,7 @@ export default {
 	// debug: true,
 	description: 'The Design System for component use within the GEL platform.',
 	files: '**/*.mdx',
-	src: './components',
+	// src: './components',
 	dest: './docs',
 	base: '/GEL/',
 	// themeConfig: {
