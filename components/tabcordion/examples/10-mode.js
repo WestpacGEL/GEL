6ed320cd5443068d25d5c6751d0a5c65df209916
Joss Mackison <joss.mackison@gmail.com>
2019-01-15 13:11:39 +1100

    prettier

--- components/tabcordion/examples/10-mode.js
+++ components/tabcordion/examples/10-mode.js
@@ -18,14 +18,18 @@ export default () => (
 		<h3>Always accordion</h3>
 		<Tabcordion mode="accordion" instanceId="always-accordion">
 			{data.map(t => (
-				<Tab key={t.label} label={t.label}>{t.content}</Tab>
+				<Tab key={t.label} label={t.label}>
+					{t.content}
+				</Tab>
 			))}
 		</Tabcordion>
 
 		<h3>Always tabs</h3>
 		<Tabcordion mode="tabs" instanceId="always-tabs">
 			{data.map(t => (
-				<Tab key={t.label} label={t.label}>{t.content}</Tab>
+				<Tab key={t.label} label={t.label}>
+					{t.content}
+				</Tab>
 			))}
 		</Tabcordion>
 	</>
--- components/tabcordion/examples/20-appearance.js
+++ components/tabcordion/examples/20-appearance.js
@@ -9,13 +9,17 @@ export default () => (
 		<h3>Soft</h3>
 		<Tabcordion mode="tabs" appearance="soft">
 			{data.map(t => (
-				<Tab key={t.label} label={t.label}>{t.content}</Tab>
+				<Tab key={t.label} label={t.label}>
+					{t.content}
+				</Tab>
 			))}
 		</Tabcordion>
 		<h3>Lego</h3>
 		<Tabcordion mode="tabs" appearance="lego">
 			{data.map(t => (
-				<Tab key={t.label} label={t.label}>{t.content}</Tab>
+				<Tab key={t.label} label={t.label}>
+					{t.content}
+				</Tab>
 			))}
 		</Tabcordion>
 	</>
