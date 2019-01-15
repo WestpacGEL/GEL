2269eabb8c3a253ee5b05e23a6aab6903aad0b7e
Joss Mackison <joss.mackison@gmail.com>
2019-01-15 16:39:10 +1100

    slight improvement to icon docs

--- components/icon/examples/_util.js
+++ components/icon/examples/_util.js
@@ -1,13 +1,39 @@
+/** @jsx jsx */
 import React from 'react';
-import { styled } from '@westpac/core';
+import { jsx, styled } from '@westpac/core';
 
-export const Row = styled.div({
+export const RowPrimitive = styled.div({
 	alignItems: 'end',
-	justifyContent: 'start',
 	display: 'grid',
 	gridAutoFlow: 'column',
 	gridGap: 8,
+	justifyContent: 'start',
 });
+export const RowWrap = styled.div({
+	marginTop: '1em',
+
+	'&:first-of-type': {
+		marginTop: 0,
+	},
+});
+export const RowLabel = styled.h4({
+	color: '#6B778C',
+	fontSize: '0.85rem',
+	fontWeight: 500,
+	marginBottom: '0.5em',
+	marginTop: 0,
+	textTransform: 'uppercase',
+});
+
+export const Row = ({ children, label, ...props }) =>
+	label ? (
+		<RowWrap>
+			<RowLabel>{label}</RowLabel>
+			<RowPrimitive {...props}>{children}</RowPrimitive>
+		</RowWrap>
+	) : (
+		<RowPrimitive {...props}>{children}</RowPrimitive>
+	);
 
 export const Grid = styled.div({
 	display: 'grid',
