ec267343687cd20b9e8135199221609b3b8326e1
Joss Mackison <joss@thinkmill.com.au>
2019-01-18 10:26:31 +1100

    memoize facepaint util

--- components/grid/src/utils.js
+++ components/grid/src/utils.js
@@ -1,9 +1,10 @@
 import facepaint from 'facepaint';
+import weakMemo from '@emotion/weak-memoize';
 
 const minWidth = width => `@media (min-width: ${width}px)`;
 const mapBreakpoints = ([key, value]) => minWidth(value);
 
 // NOTE: `breakpoints` come through context from the brand via <GEL brand={brand} />
-// TODO: investigate perf for facepaint. should happen at build time, with no
-// cost to the user -- need to confirm.
-export const paint = breakpoints => facepaint(Object.entries(breakpoints).map(mapBreakpoints));
+export const paint = weakMemo(breakpoints =>
+	facepaint(Object.entries(breakpoints).map(mapBreakpoints))
+);
