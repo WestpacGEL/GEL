23ca8f877e668bb8005679541086622e160ad4b8
Joss Mackison <joss@thinkmill.com.au>
2019-01-18 13:37:14 +1100

    prepare a component template

---
+++ helpers/.component-template/src/__COMPONENT_NAME__.js
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { jsx, useTheme } from '@westpac/core';
+
+/**
+ * __COMPONENT_TITLE__: __COMPONENT_DESCRIPTION__
+ */
+export const __COMPONENT_NAME__ = ({ ...props }) => {
+	return <div css={{}} {...props} />;
+};
+
+// ==============================
+// Types
+// ==============================
+
+__COMPONENT_NAME__.propTypes = {
+	/**
+	 * Describe `someProperty` here
+	 */
+	someProperty: PropTypes.string,
+};
+
+__COMPONENT_NAME__.defaultProps = {};
