cfee30ae974369915e9dcd6cbfe70fff4666f28b
Jonny Stening <info@jonathanstening.com>
2019-05-22 15:01:26 +1000

    adding badge component

---
+++ components/badge/examples/00-default.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Badge } from '../src/index.js';
+
+export default () => (
+	<Badge>Default</Badge>
+);
---
+++ components/badge/examples/10-appearance.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Badge } from '../src/index.js';
+
+export default () => (
+	<>
+		<p>
+			<Badge appearance="primary">Primary</Badge>{' '}
+			<Badge appearance="hero">Hero</Badge>{' '}
+			<Badge appearance="neutral">Neutral</Badge>{' '}
+			<Badge appearance="faint">Faint</Badge>
+		</p>
+		<p>
+			<Badge appearance="success">Success</Badge>{' '}
+			<Badge appearance="info">Info</Badge>{' '}
+			<Badge appearance="warning">Warning</Badge>{' '}
+			<Badge appearance="danger">Danger</Badge>
+		</p>
+	</>
+);
