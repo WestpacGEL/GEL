aa1a04146afec514b610f6b855d06fd27a3c8303
Jonny Stening <info@jonathanstening.com>
2019-06-12 14:59:08 +1000

    spread Icon props so we can provide className prop to Icon (in Button)

--- components/icon/src/Icon.js
+++ components/icon/src/Icon.js
@@ -35,14 +35,19 @@ const IconWrapper = ({ color, size, ...props }) => (
 // Component
 // ==============================
 
-export const Icon = ({ children, color, label, size }) => {
+export const Icon = ({ children, color, label, size, ...props }) => {
 	const sizeInt = getSizeInt(size);
 
 	// TODO Investigate:
 	// I suspect that using the style attribute to apply the color property will
 	// improve CSS reuse.
 	return (
-		<IconWrapper className="icon" color={color} size={sizeInt} style={{ color }}>
+		<IconWrapper
+			color={color}
+			size={sizeInt}
+			style={{ color }}
+			{...props}
+		>
 			<svg
 				aria-label={label}
 				xmlns="http://www.w3.org/2000/svg"
