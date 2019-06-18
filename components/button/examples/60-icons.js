10568ff381b30ce411f4b7e263fbe1d1385a6c40
Jonny Stening <info@jonathanstening.com>
2019-06-18 17:33:01 +1000

    button justify prop examples

--- components/button/examples/60-icons.js
+++ components/button/examples/60-icons.js
@@ -17,13 +17,16 @@ export default () => (
 			<Button appearance="primary" size="xlarge" icon={AccessibilityIcon}>Extra large</Button>
 		</p>
 		<p>
-			<Button appearance="primary" size="small" icon={ArrowRightIcon} block>Small block</Button>
+			<Button appearance="primary" size="small" icon={ArrowRightIcon} block justify>Small block justify</Button>
 		</p>
 		<p>
-			<Button appearance="primary" size="medium" icon={ArrowRightIcon} block>Medium block</Button>
+			<Button appearance="primary" size="medium" icon={ArrowRightIcon} block justify>Medium block justify</Button>
 		</p>
 		<p>
-			<Button appearance="primary" size="large" icon={ArrowRightIcon} block>Large block</Button>
+			<Button appearance="primary" size="large" icon={ArrowRightIcon} block justify>Large block justify</Button>
+		</p>
+		<p>
+			<Button appearance="primary" size="xlarge" icon={ArrowRightIcon} block justify>Extra large block justify</Button>
 		</p>
 		<p>
 			<Button appearance="primary" size="xlarge" icon={ArrowRightIcon} block>Extra large block</Button>
@@ -42,13 +45,16 @@ export default () => (
 			<Button appearance="primary" size="xlarge" icon={AccessibilityIcon} iconPosition="left">Extra large</Button>
 		</p>
 		<p>
-			<Button appearance="primary" size="small" icon={ArrowLeftIcon} iconPosition="left" block>Small block</Button>
+			<Button appearance="primary" size="small" icon={ArrowLeftIcon} iconPosition="left" block justify>Small block justify</Button>
 		</p>
 		<p>
-			<Button appearance="primary" size="medium" icon={ArrowLeftIcon} iconPosition="left" block>Medium block</Button>
+			<Button appearance="primary" size="medium" icon={ArrowLeftIcon} iconPosition="left" block justify>Medium block justify</Button>
 		</p>
 		<p>
-			<Button appearance="primary" size="large" icon={ArrowLeftIcon} iconPosition="left" block>Large block</Button>
+			<Button appearance="primary" size="large" icon={ArrowLeftIcon} iconPosition="left" block justify>Large block justify</Button>
+		</p>
+		<p>
+			<Button appearance="primary" size="xlarge" icon={ArrowLeftIcon} iconPosition="left" block justify>Extra large block justify</Button>
 		</p>
 		<p>
 			<Button appearance="primary" size="xlarge" icon={ArrowLeftIcon} iconPosition="left" block>Extra large block</Button>
