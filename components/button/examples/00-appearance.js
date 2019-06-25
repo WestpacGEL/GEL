9ae8fec1f25f9d614e72a5ef30a57e4f85398aca
Jonny Stening <info@jonathanstening.com>
2019-06-25 16:29:22 +1000

    tidy up Buttons

--- components/button/examples/00-appearance.js
+++ components/button/examples/00-appearance.js
@@ -4,26 +4,16 @@ import { Button } from '../src';
 
 export default () => (
 	<>
-		<h3>Default instance (no props)</h3>
+		<h3>Default instance (no styling props)</h3>
 		<Button>Default (primary)</Button>
 
 		<hr />
 
-		<h3>Default appearance</h3>
+		<h3>Default appearance types</h3>
 		<Button appearance="primary">Primary</Button>{' '}
 		<Button appearance="hero">Hero</Button>{' '}
 		<Button appearance="neutral">Neutral</Button>{' '}
 		<Button appearance="faint">Faint</Button>{' '}
 		<Button appearance="link">Link</Button>
-
-		<hr />
-
-		<h3>Soft appearance</h3>
-		<Button appearance="primary" soft>Primary</Button>{' '}
-		<Button appearance="hero" soft>Hero</Button>{' '}
-		<Button appearance="neutral" soft>Neutral</Button>{' '}
-		<Button appearance="faint" soft>Faint</Button>{' '}
-
-		<hr />
 	</>
 );
---
+++ components/button/examples/20-soft.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Button } from '../src';
+
+export default () => (
+	<>
+		<Button appearance="primary" soft>Primary</Button>{' '}
+		<Button appearance="hero" soft>Hero</Button>{' '}
+		<Button appearance="neutral" soft>Neutral</Button>{' '}
+		<Button appearance="faint" soft>Faint</Button>{' '}
+	</>
+);
