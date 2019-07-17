60013f2deef3a3708cb3fbd2db17e1ee1fe9596c
Flore Laforge <flore.laforge@westpac.com.au>
2019-07-17 11:52:17 +1000

    setting up examples

--- components/switch/examples/00-default.js
+++ components/switch/examples/00-default.js
@@ -4,7 +4,24 @@ import { Switch } from '../src';
 import { Box } from './_utils';
 
 export default () => (
-	<Box>
-		<Switch />
-	</Box>
+	<>
+		<Box>
+			<Switch srOnly name="ex1">
+				Enable notifications
+			</Switch>
+			<Switch srOnly name="ex2" text>
+				Enable notifications
+			</Switch>
+			<Switch srOnly name="ex3" text={['Yes', 'No']}>
+				Enable notifications
+			</Switch>
+		</Box>
+
+		<Box>
+			<Switch name="ex4">Enable notifications</Switch>
+			<Switch name="ex5" text>
+				Turn notifications
+			</Switch>
+		</Box>
+	</>
 );
