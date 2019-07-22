2c004ca9f41200f9a71cc7211735847ac83f5c28
Flore Laforge <flore.laforge@westpac.com.au>
2019-07-22 10:57:25 +1000

    refactor (alert)

--- components/alert/src/Alert.js
+++ components/alert/src/Alert.js
@@ -1,146 +1,112 @@
 /* @jsx jsx */
 
 import React, { useState } from 'react';
-import { css, jsx, useTheme } from '@westpac/core';
-import Color from 'color';
+import PropTypes from 'prop-types';
+import { jsx, useTheme, paint } from '@westpac/core';
 
-import { CloseIcon } from '../../icon/src'; //until icon package is published
+import { Button } from '../../button/src';
+import { CloseIcon } from '../../icon/src';
+
 import { CSSTransition } from 'react-transition-group';
-
-// ==============================
-// Utils
-// ==============================
 
 // ==============================
 // Component
 // ==============================
-const styleAppearance = appearance => {
-	const theme = useTheme();
-	return css({
-		color: theme.colors[appearance],
-		display: 'flex',
-		alignItems: 'center',
 
-		'& > .icon': {
-			marginRight: '6px',
-			marginTop: '1px',
-			float: 'left',
-		},
+export const Alert = ({ appearance, closable, icon: Icon, children }) => {
+	const [open, setOpen] = useState(true);
+	const { breakpoints, alert } = useTheme();
+	const mq = paint(breakpoints);
 
-		'& > .icon-alert': {
-			marginRight: '4px',
-		},
+	const styleCommon = {
+		marginBottom: alert.marginBottom,
+		padding: alert.padding,
+		paddingRight: [18, 30],
+		position: 'relative',
+		display: ['block', 'flex'],
+		zIndex: 1,
+		transition: 'opacity 0.3s ease-in-out',
 
 		a: {
 			color: 'inherit',
-		},
-	});
-};
-
-const styleBox = appearance => {
-	const theme = useTheme();
-	return css({
-		backgroundColor: Color('white')
-			.mix(Color(theme.colors[appearance]), 0.1)
-			.hex(),
-		display: 'block',
-		position: 'relative',
-		marginBottom: '21px',
-		zIndex: '1',
-		padding: '18px 34px 18px 18px',
-		borderTop: `1px solid ${Color('white')
-			.mix(Color(theme.colors[appearance]), 0.5)
-			.hex()}`,
-		borderBottom: `1px solid ${Color('white')
-			.mix(Color(theme.colors[appearance]), 0.5)
-			.hex()}`,
-		opacity: '1',
-		transition: 'opacity 0.3s ease-in-out',
-
-		button: {
-			color: theme.colors[appearance],
-			background: 'none',
-			border: 'none',
-			position: 'absolute',
-			right: '0',
-			top: '15px',
-			padding: '5px 12px',
-
-			'&:hover': {
-				cursor: 'pointer',
-				opacity: '0.5',
-			},
-		},
-		'.alert-box': {
-			position: 'relative',
-			flex: 1,
 		},
 
 		'&.anim-exit-active': {
 			opacity: 0,
 		},
-	});
-};
+	};
 
-const styleOverrides = css({
-	'&.system': {
-		backgroundColor: 'yellow',
-		borderColor: 'yellow',
-		color: 'black',
-	},
-});
+	const styleAppearance = {
+		...alert.appearance[appearance],
+	};
 
-const AlertBox = ({ appearance, icon: Icon, children }) => {
-	const [open, setOpen] = useState(true);
-	const [animAlert, setAnim] = useState(true);
+	const styleClose = {
+		color: alert.appearance[appearance].color,
+		position: ['relative', 'absolute'],
+		zIndex: 1,
+		float: ['right', 'none'],
+		top: 3,
+		right: 0,
+		marginTop: [-18, 0],
+		marginRight: [-18, 0],
+		opacity: 1,
+
+		':hover': {
+			opacity: 0.5,
+		},
+	};
+
+	const styleIcon = {
+		float: ['left', 'none'],
+		marginRight: [6, 12],
+		flex: 'none',
+		fontSize: [18, 24],
+		width: [18, 24],
+		height: [18, 24],
+	};
+
+	const styleBox = {
+		position: 'relative',
+		flex: 1,
+		top: ['auto', 2],
+	};
 
 	return (
-		<>
-			{open && (
-				<CSSTransition
-					in={animAlert}
-					classNames="anim"
-					timeout={400}
-					onExited={() => setOpen(false)}
-				>
-					<div
-						css={[styleAppearance(appearance), styleBox(appearance), styleOverrides]}
-						className={appearance === 'system' ? 'system' : ''}
-					>
-						{Icon && <Icon size="small" className="icon icon-alert" />}
-						<div className="alert-body">{children}</div>
-						{Icon && (
-							<button
-								onClick={() => {
-									setAnim(false);
-								}}
-							>
-								<CloseIcon size="small" />
-							</button>
-						)}
-					</div>
-				</CSSTransition>
-			)}
-		</>
+		<CSSTransition in={open} unmountOnExit classNames="anim" timeout={400}>
+			<div css={mq({ ...styleCommon, ...styleAppearance })}>
+				{closable && (
+					<Button
+						onClick={() => {
+							setOpen(false);
+						}}
+						icon={CloseIcon}
+						size="small"
+						appearance="link"
+						css={styleClose}
+					/>
+				)}
+				{Icon && <Icon className="icon icon-alert" css={mq({ ...styleIcon })} />}
+				<div css={mq({ ...styleBox })}>{children}</div>
+			</div>
+		</CSSTransition>
 	);
-};
-
-const AlertText = ({ appearance, icon: Icon, iconSize, children, as: As }) => {
-	return (
-		<As css={styleAppearance(appearance)}>
-			{Icon && <Icon size={iconSize} className="icon-alert" />} {children}
-		</As>
-	);
-};
-
-export const Alert = ({ type, ...props }) => {
-	return type === 'AlertText' ? <AlertText {...props} /> : <AlertBox {...props} />;
 };
 
 // ==============================
 // Types
 // ==============================
+
+Alert.propTypes = {
+	appearance: PropTypes.oneOf(['success', 'information', 'warning', 'danger', 'system']),
+
+	closable: PropTypes.bool,
+
+	icon: PropTypes.func,
+
+	children: PropTypes.node,
+};
+
 Alert.defaultProps = {
-	type: 'AlertText',
-	as: 'div',
+	appearance: 'information',
+	closable: false,
 };
