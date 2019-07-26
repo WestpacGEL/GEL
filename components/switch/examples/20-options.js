8bcdd2c50cc15bfc727445ac38322057c15c0443
Flore Laforge <flore.laforge@westpac.com.au>
2019-07-26 17:05:59 +1000

    feat(switch): add options examples

---
+++ components/switch/examples/20-options.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Switch } from '../src';
+import { Box } from './_utils';
+
+export default () => (
+	<>
+		<h3>Flipped switches</h3>
+		<Box>
+			<Switch name="ex1" flipped>
+				Enable notifications
+			</Switch>
+			<Switch name="ex2" values flipped>
+				Turn notifications
+			</Switch>
+		</Box>
+		<hr />
+
+		<h3>Block switches</h3>
+		<Box>
+			<Switch name="ex3" block>
+				Turn notifications
+			</Switch>
+			<Switch name="ex4" values block>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur corporis sint, facere
+				sequi provident eius similique ab velit, beatae aut architecto porro quidem neque
+				necessitatibus, quis nulla itaque labore harum, in nesciunt! Molestiae, voluptatum,
+				voluptas. Quo pariatur, voluptate ducimus nemo?
+			</Switch>
+		</Box>
+		<hr />
+
+		<h3>Flipped block switches</h3>
+		<Box>
+			<Switch name="ex4" flipped block>
+				Enable notifications
+			</Switch>
+			<Switch name="ex5" values flipped block>
+				Turn notifications
+			</Switch>
+		</Box>
+		<hr />
+	</>
+);
