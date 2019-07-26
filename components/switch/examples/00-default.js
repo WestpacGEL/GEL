a41a8d3d9b447e5e4568a09241acbe0bebd9eb2f
Flore Laforge <flore.laforge@westpac.com.au>
2019-07-26 14:39:15 +1000

    feat(switch): better examples

--- components/switch/examples/00-default.js
+++ components/switch/examples/00-default.js
@@ -5,41 +5,43 @@ import { Box } from './_utils';
 
 export default () => (
 	<>
+		<h3>Switches without visible text label (Screenreader only text)</h3>
 		<Box>
-			<Switch srOnly name="ex1">
-				Enable notifications
-			</Switch>
-			<Switch srOnly name="ex2" values>
-				Enable notifications
-			</Switch>
-			<Switch srOnly name="ex3" values={['Yes', 'No']}>
-				Enable notifications
-			</Switch>
+			<Switch srOnly name="ex1" />
+			<Switch srOnly name="ex2" values />
+			<Switch srOnly name="ex3" values={['Yes', 'No']} />
 		</Box>
+		<hr />
 
+		<h3>Switches with text label</h3>
 		<Box>
-			<Switch name="ex4" values={['Yes', 'No']}>
-				Enable notifications
-			</Switch>
+			<Switch name="ex4">Enable notifications</Switch>
 			<Switch name="ex5" values>
-				Turn notifications
-			</Switch>
-		</Box>
-		<Box>
-			<Switch name="ex4" values={['Yes', 'No']} checked>
 				Enable notifications
 			</Switch>
-			<Switch name="ex6" values checked>
+		</Box>
+		<hr />
+
+		<h3>Checked by default</h3>
+		<Box>
+			<Switch name="ex4" checked>
+				Enable notifications
+			</Switch>
+			<Switch name="ex5" values checked>
 				Turn notifications
 			</Switch>
 		</Box>
+		<hr />
+
+		<h3>Disabled switches</h3>
 		<Box>
-			<Switch name="ex4" values={['Yes', 'No']} disabled>
+			<Switch name="ex4" disabled>
 				Enable notifications
 			</Switch>
 			<Switch name="ex6" values disabled>
 				Turn notifications
 			</Switch>
 		</Box>
+		<hr />
 	</>
 );
