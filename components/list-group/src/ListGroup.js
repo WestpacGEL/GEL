8b51ac4e410bc18dbd19ecbdaaa6b9b62b8054c5
Flore Laforge <flore.laforge@westpac.com.au>
2019-08-14 09:59:11 +1000

    feat(list-group): create the component

--- components/list-group/src/ListGroup.js
+++ components/list-group/src/ListGroup.js
@@ -15,12 +15,32 @@ import { jsx, useTheme } from '@westpac/core';
 /**
  * List Group: List groups are a flexible and powerful component for displaying not only simple lists of elements, but complex ones with custom content. Ideal for settings pages or preferences.
  */
-export const ListGroup = ({ ...props }) => {
-	return (
-		<div css={{}} {...props}>
-			ListGroup
-		</div>
-	);
+export const ListGroup = ({ children }) => {
+	const common = {
+		listStyle: 'none',
+		margin: 0,
+		padding: 0,
+		display: 'inline-block',
+		border: '1px solid #d7d2cb',
+		borderBottom: '0 none',
+		borderRadius: '3px',
+
+		li: {
+			margin: 0,
+			padding: 0,
+			borderBottom: '1px solid #d7d2cb',
+			padding: '12px',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'space-between',
+
+			'>span': {
+				paddingRight: 12,
+			},
+		},
+	};
+
+	return <ul css={common}>{children}</ul>;
 };
 
 // ==============================
