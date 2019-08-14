6752e64a4f4008e510a284ff32205c8bd5b41a1b
Flore Laforge <flore.laforge@westpac.com.au>
2019-08-14 14:59:32 +1000

    feat(breadcrumb): build the breadcrumb

--- components/breadcrumb/src/Breadcrumb.js
+++ components/breadcrumb/src/Breadcrumb.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { jsx, useTheme } from '@westpac/core';
+import { SrOnly } from '../../accessibility-helpers/src';
+import { ArrowRightIcon } from '../../icon/src';
 
 // ==============================
 // Utils
@@ -15,10 +17,64 @@ import { jsx, useTheme } from '@westpac/core';
 /**
  * Breadcrumb: Breadcrumbs are styled navigational links used to indicate a user journey or path. They are a simple, effective and proven method to aid orientation.
  */
-export const Breadcrumb = ({ ...props }) => {
+export const Breadcrumb = ({ arrItem, ...props }) => {
+	const { breadcrumb } = useTheme();
+
+	const common = {
+		padding: '6px 18px',
+		marginBottom: '21px',
+		listStyle: 'none',
+		fontSize: '13px',
+	};
+
+	const styleItem = {
+		display: 'inline-block',
+		position: 'relative',
+	};
+
+	const styleLink = {
+		color: '#2d373e',
+		textDecoration: 'none',
+
+		'&:focus, &:hover': {
+			textDecoration: 'underline',
+		},
+	};
+
+	const styleSeparator = {
+		marginLeft: 3,
+		marginRight: 3,
+	};
+
 	return (
-		<div css={{}} {...props}>
-			Breadcrumb
+		<div {...props}>
+			<SrOnly>Page navigation:</SrOnly>
+
+			<ol css={common}>
+				{/* {test()} */}
+				{arrItem.map((item, index) =>
+					index === arrItem.length - 1 ? (
+						// Last breadcrumb
+						<li key={`breadcrumb-${index}`} css={styleItem}>
+							<span>
+								<SrOnly>Current page:</SrOnly>
+								{item[0]}
+							</span>
+						</li>
+					) : (
+						<li key={`breadcrumb-${index}`} css={styleItem}>
+							<a href={item[1]} css={styleLink}>
+								{item[0]}
+							</a>
+							<ArrowRightIcon
+								size="small"
+								color={breadcrumb.separator.color}
+								css={styleSeparator}
+							/>
+						</li>
+					)
+				)}
+			</ol>
 		</div>
 	);
 };
