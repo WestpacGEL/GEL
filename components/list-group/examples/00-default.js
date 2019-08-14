e6cbb56578cd62205ed62edd44cd26e99840c408
Flore Laforge <flore.laforge@westpac.com.au>
2019-08-14 11:14:40 +1000

    style(list-group): examples styles

--- components/list-group/examples/00-default.js
+++ components/list-group/examples/00-default.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from '../src';
 import { ButtonGroup, ButtonGroupButton } from '../../button-group/src';
+import { styleExample } from './_utils';
 
 export default () => (
 	<ListGroup>
 		<ListGroupItem>
 			Send me sms reminders
-			<ButtonGroup size="small" name="example-default-1">
+			<ButtonGroup size="small" name="example-default-1" css={styleExample}>
 				<ButtonGroupButton>Yes</ButtonGroupButton>
 				<ButtonGroupButton>No</ButtonGroupButton>
 			</ButtonGroup>
 		</ListGroupItem>
 		<ListGroupItem>
 			Give me free money
-			<ButtonGroup size="small" name="example-default-2">
+			<ButtonGroup size="small" name="example-default-2" css={styleExample}>
 				<ButtonGroupButton>Yes</ButtonGroupButton>
 				<ButtonGroupButton>No</ButtonGroupButton>
 			</ButtonGroup>
 		</ListGroupItem>
 		<ListGroupItem>
 			Call me all the time
-			<ButtonGroup size="small" name="example-default-3">
+			<ButtonGroup size="small" name="example-default-3" css={styleExample}>
 				<ButtonGroupButton>Yes</ButtonGroupButton>
 				<ButtonGroupButton>No</ButtonGroupButton>
 			</ButtonGroup>
 		</ListGroupItem>
 		<ListGroupItem>
 			Deleted messages
-			<ButtonGroup size="small" name="example-default-4">
+			<ButtonGroup size="small" name="example-default-4" css={styleExample}>
 				<ButtonGroupButton>Yes</ButtonGroupButton>
 				<ButtonGroupButton>No</ButtonGroupButton>
 			</ButtonGroup>
--- components/list-group/examples/_utils.js
+++ components/list-group/examples/_utils.js
@@ -2,12 +2,6 @@
 
 import { jsx } from '@westpac/core';
 
-export const Box = props => (
-	<div
-		css={{
-			alignItems: 'center',
-			display: 'flex',
-		}}
-		{...props}
-	/>
-);
+export const styleExample = {
+	paddingLeft: 12,
+};
