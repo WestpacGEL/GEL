3fc4ba02b6004f01055cf9b4b10c8d73a7ae2f61
Flore Laforge <flore.laforge@westpac.com.au>
2019-08-14 10:08:04 +1000

    feat(list-group): add examples

--- components/list-group/examples/00-default.js
+++ components/list-group/examples/00-default.js
@@ -1,10 +1,36 @@
 import React from 'react';
-
 import { ListGroup } from '../src';
-import { Box } from './_utils';
+import { ButtonGroup, ButtonGroupButton } from '../../button-group/src';
 
 export default () => (
-	<Box>
-		<ListGroup />
-	</Box>
+	<ListGroup>
+		<li>
+			<span>Send me sms reminders </span>
+			<ButtonGroup size="small" name="example-default-1">
+				<ButtonGroupButton>Yes</ButtonGroupButton>
+				<ButtonGroupButton>No</ButtonGroupButton>
+			</ButtonGroup>
+		</li>
+		<li>
+			<span>Give me free money</span>
+			<ButtonGroup size="small" name="example-default-2">
+				<ButtonGroupButton>Yes</ButtonGroupButton>
+				<ButtonGroupButton>No</ButtonGroupButton>
+			</ButtonGroup>
+		</li>
+		<li>
+			<span>Call me all the time</span>
+			<ButtonGroup size="small" name="example-default-3">
+				<ButtonGroupButton>Yes</ButtonGroupButton>
+				<ButtonGroupButton>No</ButtonGroupButton>
+			</ButtonGroup>
+		</li>
+		<li>
+			<span>Deleted messages</span>
+			<ButtonGroup size="small" name="example-default-4">
+				<ButtonGroupButton>Yes</ButtonGroupButton>
+				<ButtonGroupButton>No</ButtonGroupButton>
+			</ButtonGroup>
+		</li>
+	</ListGroup>
 );
