c0168c202da838640104cb3dda32f2ef7d4f61ef
Jonny Stening <info@jonathanstening.com>
2019-08-15 11:34:57 +1000

    refactor(form-input): update form-input

--- components/form-input/src/FormInput.js
+++ components/form-input/src/FormInput.js
@@ -1,147 +1,147 @@
 /** @jsx jsx */
 
-import React from "react";
-import PropTypes from "prop-types";
-import { jsx, useTheme } from "@westpac/core";
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { jsx, useTheme } from '@westpac/core';
+import { FormContext } from '../../form/src';
 
-import svgToTinyDataURI from "mini-svg-data-uri";
+import svgToTinyDataURI from 'mini-svg-data-uri';
 
 // ==============================
 // Utils
 // ==============================
+
+const round = f => Math.round(f * 100) / 100; //2DP
 
 // ==============================
 // Component
 // ==============================
 
 export const FormInput = ({
-  size,
-  width,
-  invalid,
-  tag: Tag,
-  spacing,
-  inline,
-  children,
-  ...props
+	size,
+	width,
+	invalid,
+	tag: Tag,
+	spacing,
+	inline,
+	children,
+	...props
 }) => {
-  const { colors, formInput, typography } = useTheme();
+	const { colors, formInput, typography } = useTheme();
+	const formContext = useContext(FormContext);
 
-  // Common styling
-  const styleCommon = {
-    display: inline ? "inline-block" : "block",
-    width: inline ? "auto" : "100%",
-    appearance: "none",
-    lineHeight: formInput.lineHeight,
-    fontWeight: formInput.fontWeight,
-    color: formInput.color,
-    backgroundColor: formInput.backgroundColor,
-    border: `${formInput.borderWidth}px solid`,
-    borderColor:
-      invalid || props.ariaInvalid
-        ? formInput.appearance.invalid.borderColor
-        : formInput.appearance.default.borderColor,
-    borderRadius: formInput.borderRadius,
-    transition: "border 0.2s ease",
-    verticalAlign: Tag === "textarea" ? "top" : inline ? "middle" : null,
+	const formInputInline = formContext.inline || inline;
 
-    "&::placeholder": {
-      opacity: 1, // Override Firefox's unusual default opacity
-      ...formInput.placeholder
-    },
+	// Common styling
+	const styleCommon = {
+		display: formInputInline ? 'inline-block' : 'block',
+		width: formInputInline ? 'auto' : '100%',
+		appearance: 'none',
+		lineHeight: formInput.lineHeight,
+		fontWeight: formInput.fontWeight,
+		color: formInput.color,
+		backgroundColor: formInput.backgroundColor,
+		border: `${formInput.borderWidth} solid`,
+		borderColor:
+			invalid || props.ariaInvalid
+				? formInput.appearance.invalid.borderColor
+				: formInput.appearance.default.borderColor,
+		borderRadius: formInput.borderRadius,
+		transition: 'border 0.2s ease',
+		verticalAlign: Tag === 'textarea' ? 'top' : formInputInline ? 'middle' : null,
+		padding: formInput.size[size].padding.join(' '),
+		fontSize: formInput.size[size].fontSize,
+		height: `calc(${formInput.lineHeight}em + ${(p => `${p[0]} + ${p[2] || p[0]}`)(
+			formInput.size[size].padding
+		)} + ${(bw => `${bw} + ${bw}`)(formInput.borderWidth)})`,
 
-    // Focus styling (for all, not just keyboard users)
-    ":focus": {
-      ...typography.link.focus
-    },
+		'&::placeholder': {
+			opacity: 1, // Override Firefox's unusual default opacity
+			...formInput.placeholder,
+		},
 
-    // Disabled and read-only inputs
-    ":disabled, &[readonly]": {
-      cursor: "not-allowed",
-      opacity: 1, // iOS fix for unreadable disabled content
-      ...formInput.disabled
-    },
+		// Focus styling (for all, not just keyboard users)
+		':focus': {
+			...typography.link.focus,
+		},
 
-    // Disable number input spinners/steppers
-    "&::-webkit-outer-spin-button, &::-webkit-inner-spin-button": {
-      margin: 0,
-      appearance: "none"
-    }
-  };
+		// Disabled and read-only inputs
+		':disabled, &[readonly]': {
+			cursor: 'not-allowed',
+			opacity: 1, // iOS fix for unreadable disabled content
+			...formInput.disabled,
+		},
 
-  // Input size styling
-  const styleSize = {
-    padding: formInput.size[size].padding.join(" "),
-    fontSize: formInput.size[size].fontSize,
-    height: formInput.size[size].height
-  };
+		// Disable number input spinners/steppers
+		'&::-webkit-outer-spin-button, &::-webkit-inner-spin-button': {
+			margin: 0,
+			appearance: 'none',
+		},
+	};
 
-  // Input fixed width styling
-  const styleFixedWidth = () => {
-    const factor = formInput.fontXFactor; //'W' compared to 'x' character (relative to font)
-    const paddings = parseInt(formInput.size[size].padding[1], 10) * 2;
-    const borders = parseInt(formInput.borderWidth, 10) * 2;
-    const extras = paddings + borders;
+	const caretSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8"><path fill="${
+		colors.muted
+	}" fill-rule="evenodd" d="M0 0l7 8 7-8z"/></svg>`;
+	const caretGap = formInput.select.caretGap;
+	const caretWidth = '14px';
 
-    return width
-      ? {
-          maxWidth: `calc(${extras}px + ${(width * factor).toFixed(1)}ex)`
-        }
-      : null;
-  };
+	const styleAppearance = {
+		select: {
+			paddingRight: `calc(${formInput.size[size].padding[1]} + ${caretWidth} + ${caretGap})`,
+			backgroundImage: `url("${svgToTinyDataURI(caretSVG)}")`,
+			backgroundRepeat: 'no-repeat',
+			backgroundPosition: `right ${formInput.size[size].padding[1]} center`,
 
-  // Select element styling
-  const styleSelect = () => {
-    const caretWidth = 14;
-    const caretSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="${caretWidth}" height="8" viewBox="0 0 14 8"><path fill="${
-      colors.muted
-    }" fill-rule="evenodd" d="M0 0l7 8 7-8z"/></svg>`;
-    const caretGap = formInput.select.caretGap;
-    const paddingRight =
-      parseInt(formInput.size[size].padding[1], 10) + caretWidth + caretGap;
+			// Remove the caret on `<select>`s in IE10+.
+			'&::-ms-expand': {
+				display: 'none',
+			},
 
-    return Tag === "select"
-      ? {
-          paddingRight: `${paddingRight}px`,
-          backgroundImage: `url("${svgToTinyDataURI(caretSVG)}")`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: `right ${formInput.size[size].padding[1]} center`,
+			'@media print': {
+				backgroundColor: 'transparent',
 
-          // Remove the caret on `<select>`s in IE10+.
-          "&::-ms-expand": {
-            display: "none"
-          },
+				':disabled': {
+					backgroundColor: '#fff',
+				},
+			},
+		},
+		textarea: {
+			...formInput.textarea.size[size],
+		},
+	};
 
-          "@media print": {
-            backgroundColor: "transparent",
+	// Input fixed width styling
+	const styleFixedWidth = () => {
+		const factor = formInput.fontXFactor; //'W' compared to 'x' character (relative to font)
+		let extras = `${(p => `${p} + ${p}`)(formInput.size[size].padding[1])} + ${(b => `${b} + ${b}`)(
+			formInput.borderWidth
+		)}`;
 
-            ":disabled": {
-              backgroundColor: "#fff"
-            }
-          }
-        }
-      : null;
-  };
+		// Add width for caret if necessary
+		if (Tag === 'select') {
+			extras = `${extras} + ${caretWidth} + ${caretGap}`;
+		}
 
-  // Textarea element styling
-  const styleTextarea = {
-    ...(Tag === "textarea" && formInput.textarea.size[size])
-  };
+		return width
+			? {
+					maxWidth: `calc(${extras} + ${round(width * factor)}ex)`,
+			  }
+			: null;
+	};
 
-  return (
-    <Tag
-      css={{
-        ...styleCommon,
-        ...styleSize,
-        ...styleFixedWidth(),
-        ...styleSelect(),
-        ...styleTextarea
-      }}
-      type={Tag === "input" ? "text" : undefined}
-      {...props}
-    >
-      {Tag === "select" ? children : null}
-    </Tag>
-  );
+	return (
+		<Tag
+			css={{
+				...styleCommon,
+				...styleAppearance[Tag],
+				...styleFixedWidth(),
+			}}
+			type={Tag === 'input' ? 'text' : undefined}
+			{...props}
+		>
+			{Tag === 'select' ? children : null}
+		</Tag>
+	);
 };
 
 // ==============================
@@ -149,48 +149,48 @@ export const FormInput = ({
 // ==============================
 
 const options = {
-  size: ["small", "medium", "large", "xlarge"],
-  width: [2, 3, 4, 5, 10, 20, 30],
-  tag: ["input", "select", "textarea"]
+	size: ['small', 'medium', 'large', 'xlarge'],
+	width: [2, 3, 4, 5, 10, 20, 30],
+	tag: ['input', 'select', 'textarea'],
 };
 
 FormInput.propTypes = {
-  /**
-   * The component size.
-   *
-   * Defaults to "medium"
-   */
-  size: PropTypes.oneOf(options.size),
+	/**
+	 * The component size.
+	 *
+	 * Defaults to "medium"
+	 */
+	size: PropTypes.oneOf(options.size),
 
-  /**
-   * The component width.
-   */
-  width: PropTypes.oneOf(options.width),
+	/**
+	 * The component width.
+	 */
+	width: PropTypes.oneOf(options.width),
 
-  /**
-   * Invalid input mode.
-   *
-   * Defaults to "false"
-   */
-  invalid: PropTypes.bool,
+	/**
+	 * Invalid input mode.
+	 *
+	 * Defaults to "false"
+	 */
+	invalid: PropTypes.bool,
 
-  /**
-   * The component tag.
-   *
-   * Defaults to "input"
-   */
-  tag: PropTypes.oneOf(options.tag),
+	/**
+	 * The component tag.
+	 *
+	 * Defaults to "input"
+	 */
+	tag: PropTypes.oneOf(options.tag),
 
-  /**
-   * The component children.
-   *
-   * Only select inputs render children.
-   */
-  children: PropTypes.node
+	/**
+	 * The component children.
+	 *
+	 * Only select inputs render children.
+	 */
+	children: PropTypes.node,
 };
 
 FormInput.defaultProps = {
-  size: "medium",
-  invalid: false,
-  tag: "input"
+	size: 'medium',
+	invalid: false,
+	tag: 'input',
 };
