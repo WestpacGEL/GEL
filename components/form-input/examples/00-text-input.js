71dfc4bc09667b50c10bc163a748189de73b76b6
Jonny Stening <info@jonathanstening.com>
2019-08-15 11:51:36 +1000

    style(form-input): prettier

--- components/form-input/examples/00-text-input.js
+++ components/form-input/examples/00-text-input.js
@@ -10,9 +10,7 @@ export default () => (
 	<>
 		<h2>Default instance (no styling props)</h2>
 		<FormInput />
-
 		<hr />
-
 		<h2>Size</h2>
 		{sizes.map((s, i, arr) => (
 			<Fragment key={i}>
@@ -20,31 +18,21 @@ export default () => (
 				{i < arr.length - 1 && <br />}
 			</Fragment>
 		))}
-
 		<hr />
-
 		<h2>Invalid</h2>
 		<FormInput invalid />
-
 		<hr />
-
 		<h2>Disabled</h2>
 		<FormInput disabled />
 		<br />
 		<FormInput disabled value="This input is disabled and contains a value" />
-
 		<hr />
-
 		<h2>Readonly</h2>
 		<FormInput readOnly value="This value is readonly" />
-
 		<hr />
-
 		<h2>Inline</h2>
 		<FormInput inline /> <FormInput inline /> <Button type="submit">Submit</Button>
-
 		<hr />
-		
 		<h2>Fixed width</h2>
 		{widths.map((w, i, arr) => (
 			<Fragment key={i}>
