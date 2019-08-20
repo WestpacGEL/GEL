d1e66524121fae2ee502b79b8b232c356ff5aaa0
Flore Laforge <flore.laforge@westpac.com.au>
2019-08-20 15:06:05 +1000

    refactor(list-group): clean up

--- components/list-group/src/ListGroup.js
+++ components/list-group/src/ListGroup.js
@@ -5,10 +5,6 @@ import PropTypes from 'prop-types';
 import { jsx, useTheme } from '@westpac/core';
 
 // ==============================
-// Utils
-// ==============================
-
-// ==============================
 // Component
 // ==============================
 
