6b3b02577d7ef9fbd1989e234fba00ff447b4cb8
Jonny Stening <info@jonathanstening.com>
2019-08-21 23:22:05 +1000

    style(form): reorder exports

--- components/form/src/index.js
+++ components/form/src/index.js
@@ -1,12 +1,12 @@
-export { Form } from './Form';
-export { FormContext } from './Form.context';
 export { ChitChat } from './ChitChat';
 export { ErrorMessage } from './ErrorMessage';
 export { Fieldset } from './Fieldset';
+export { Form } from './Form';
+export { FormContext } from './Form.context';
 export { FormGroup } from './FormGroup';
-export { Hint } from './Hint';
-export { InputCluster } from './InputCluster';
-export { InputClusterItem } from './InputClusterItem';
 export { FormLabel } from './FormLabel';
 export { FormSection } from './FormSection';
 export { FormSectionImg } from './FormSectionImg';
+export { Hint } from './Hint';
+export { InputCluster } from './InputCluster';
+export { InputClusterItem } from './InputClusterItem';
