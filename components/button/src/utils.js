1ee4150ff3944b8864fb12f25d275eaf8242ac36
Jonny Stening <info@jonathanstening.com>
2019-08-21 10:30:51 +1000

    chore(button): tidy up

--- components/button/src/utils.js
+++
@@ -1,4 +0,0 @@
-const minWidth = width => `@media (min-width: ${width})`;
-const mapBreakpoints = ([key, value]) => minWidth(value);
-
-export const mediaQueries = breakpoints => Object.entries(breakpoints).map(mapBreakpoints);
