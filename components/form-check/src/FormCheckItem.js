ebcc29b6f4c2a5544a1ae2b2f741f916ac8e74f4
Jonny Stening <info@jonathanstening.com>
2019-08-26 14:21:46 +1000

    refactor(form-check): updates post review

--- components/form-check/src/FormCheckItem.js
+++ components/form-check/src/FormCheckItem.js
@@ -23,8 +23,8 @@ export const FormCheckItem = ({
 }) => {
 	const { formCheck, typography } = useTheme();
 	const isKeyboardUser = useContext(UserModeContext);
-
 	const [isChecked, setIsChecked] = useState(checked);
+	const [formCheckId] = useState(`formCheck-${shortid.generate()}`);
 
 	useEffect(
 		() => {
@@ -41,8 +41,6 @@ export const FormCheckItem = ({
 		}
 	};
 
-	const formCheckId = `formCheck-${shortid.generate()}`;
-
 	const controlWidth = formCheck.size[size].control.width;
 	const controlHeight = formCheck.size[size].control.width;
 	const checkWidth = formCheck.size[size].check[type].width;
@@ -52,117 +50,115 @@ export const FormCheckItem = ({
 	const checkboxStroke = formCheck.size[size].check['checkbox'].stroke;
 
 	// Common styling
-	const styleCommon = {
-		position: 'relative',
-		display: inline ? 'inline-block' : 'block',
-		verticalAlign: inline ? 'top' : null,
-		marginRight: inline ? formCheck.size[size].item.marginRight : null,
-		marginBottom: formCheck.size[size].item.marginBottom,
-		minHeight: controlHeight,
-		...(val => (flip ? { paddingRight: val } : { paddingLeft: val }))(controlWidth),
-	};
-
-	// Input control styling
-	const styleInput = {
-		position: 'absolute',
-		zIndex: 1,
-		top: 0,
-		...(flip ? { right: 0 } : { left: 0 }),
-		width: controlWidth,
-		height: controlHeight,
-		cursor: 'pointer',
-		margin: 0,
-		opacity: 0, //hide
-
-		':disabled, fieldset:disabled &': {
-			cursor: 'default',
-		},
-	};
-
-	// Label styling
-	const styleLabel = {
-		display: 'inline-block',
-		paddingTop: formCheck.size[size].label.paddingTop,
-		paddingBottom: formCheck.size[size].label.paddingBottom,
-		...(val => (flip ? { paddingRight: val } : { paddingLeft: val }))(
-			formCheck.size[size].label.gap
-		),
-		marginBottom: 0,
-		cursor: 'pointer',
-
-		// remove 300ms pause on mobile
-		touchAction: 'manipulation',
-
-		// Disabled state
-		'input:disabled + &, fieldset:disabled &': {
-			cursor: 'default',
-			...formCheck.label.disabled,
+	const style = {
+		common: {
+			position: 'relative',
+			display: inline ? 'inline-block' : 'block',
+			verticalAlign: inline && 'top',
+			marginRight: inline && formCheck.size[size].item.marginRight,
+			marginBottom: formCheck.size[size].item.marginBottom,
+			minHeight: controlHeight,
+			[flip ? 'paddingRight' : 'paddingLeft']: controlWidth,
 		},
 
-		// Control outline
-		'::before': {
-			content: '""',
-			boxSizing: 'border-box',
+		// Input control styling
+		input: {
 			position: 'absolute',
+			zIndex: 1,
 			top: 0,
-			...(flip ? { right: 0 } : { left: 0 }),
+			[flip ? 'right' : 'left']: 0,
 			width: controlWidth,
 			height: controlHeight,
-			border: `${formCheck.control.borderWidth} solid ${formCheck.control.default.borderColor}`,
-			background: formCheck.control.default.backgroundColor,
-			borderRadius: formCheck.control.check[type].borderRadius,
+			cursor: 'pointer',
+			margin: 0,
+			opacity: 0, //hide
 
-			// Focus state
-			'input:focus + &': {
-				...(isKeyboardUser && typography.link.focus), //TODO: needs access to `isKeyboardUser` state
+			':disabled, fieldset:disabled &': {
+				cursor: 'default',
 			},
+		},
+
+		// Label styling
+		label: {
+			display: 'inline-block',
+			paddingTop: formCheck.size[size].label.paddingTop,
+			paddingBottom: formCheck.size[size].label.paddingBottom,
+			[flip ? 'paddingRight' : 'paddingLeft']: formCheck.size[size].label.gap,
+			marginBottom: 0,
+			cursor: 'pointer',
+
+			// remove 300ms pause on mobile
+			touchAction: 'manipulation',
 
 			// Disabled state
 			'input:disabled + &, fieldset:disabled &': {
-				...formCheck.control.disabled,
+				cursor: 'default',
+				...formCheck.label.disabled,
 			},
-		},
 
-		// Control 'check' (tick or dot)
-		'::after': {
-			content: '""',
-			position: 'absolute',
-			border: `solid ${formCheck.control.default.borderColor}`,
-			opacity: 0, //hide
-			top: `calc(((${controlHeight} - ${checkHeight}) / 2) + ${checkTweak})`,
-			...(val => (flip ? { right: `calc(${val})` } : { left: `calc(${val})` }))(
-				`(${controlWidth} - ${checkWidth}) / 2`
-			),
-			width: type === 'radio' ? 0 : checkWidth,
-			height: type === 'radio' ? 0 : checkHeight,
-			borderWidth:
-				type === 'radio' ? `calc(${checkWidth} / 2)` : `0 0 ${checkboxStroke} ${checkboxStroke}`,
-			borderRadius: type === 'radio' ? '50%' : null,
-			background: type === 'radio' ? formCheck.control.check.radio.backgroundColor : null,
-			transform: type === 'radio' ? null : 'rotate(-54deg)',
+			// Control outline
+			'::before': {
+				content: '""',
+				boxSizing: 'border-box',
+				position: 'absolute',
+				top: 0,
+				[flip ? 'right' : 'left']: 0,
+				width: controlWidth,
+				height: controlHeight,
+				border: `${formCheck.control.borderWidth} solid ${formCheck.control.default.borderColor}`,
+				background: formCheck.control.default.backgroundColor,
+				borderRadius: formCheck.control.check[type].borderRadius,
 
-			// Fix bug in IE11 caused by transform rotate (-54deg)
-			borderTopColor: type === 'radio' ? null : 'transparent',
+				// Focus state
+				'input:focus + &': {
+					...(isKeyboardUser && typography.link.focus),
+				},
 
-			// Selected state
-			'input:checked + &': {
-				opacity: 1, //show
+				// Disabled state
+				'input:disabled + &, fieldset:disabled &': {
+					...formCheck.control.disabled,
+				},
+			},
+
+			// Control 'check' (tick or dot)
+			'::after': {
+				content: '""',
+				position: 'absolute',
+				border: `solid ${formCheck.control.default.borderColor}`,
+				opacity: 0, //hide
+				top: `calc(((${controlHeight} - ${checkHeight}) / 2) + ${checkTweak})`,
+				[flip ? 'right' : 'left']: `calc((${controlWidth} - ${checkWidth}) / 2)`,
+				width: type === 'radio' ? 0 : checkWidth,
+				height: type === 'radio' ? 0 : checkHeight,
+				borderWidth:
+					type === 'radio' ? `calc(${checkWidth} / 2)` : `0 0 ${checkboxStroke} ${checkboxStroke}`,
+				borderRadius: type === 'radio' && '50%',
+				background: type === 'radio' && formCheck.control.check.radio.backgroundColor,
+				transform: type === 'checkbox' && 'rotate(-54deg)',
+
+				// Fix bug in IE11 caused by transform rotate (-54deg)
+				borderTopColor: type === 'checkbox' && 'transparent',
+
+				// Selected state
+				'input:checked + &': {
+					opacity: 1, //show
+				},
 			},
 		},
 	};
 
 	return (
-		<div css={styleCommon} {...props}>
+		<div css={style.common} {...props}>
 			<input
 				type={type}
-				css={styleInput}
+				css={style.input}
 				name={name}
 				id={formCheckId}
 				value={value}
 				checked={isChecked}
 				onChange={toggle}
 			/>
-			<label htmlFor={formCheckId} css={styleLabel}>
+			<label htmlFor={formCheckId} css={style.label}>
 				{children}
 			</label>
 		</div>
