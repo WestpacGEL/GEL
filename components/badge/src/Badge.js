c8e16390d3f5109bde2657291042aa4eed0ea978
Jonny Stening <info@jonathanstening.com>
2019-08-29 12:44:46 +1000

    refactor(badge): import via package name, update examples, tidy up

--- components/badge/src/Badge.js
+++ components/badge/src/Badge.js
@@ -5,38 +5,38 @@ import PropTypes from 'prop-types';
 import { jsx, useTheme } from '@westpac/core';
 
 // ==============================
-// Utils
-// ==============================
-
-// ==============================
 // Component
 // ==============================
 
 export const Badge = ({ appearance, children, ...props }) => {
 	const { colors, badge } = useTheme();
 
-	const common = {
-		border: `${badge.borderWidth} solid transparent`,
-		borderRadius: badge.borderRadius,
-		display: 'inline-block',
-		fontSize: badge.fontSize,
-		fontWeight: badge.fontWeight,
-		lineHeight: badge.lineHeight,
-		minWidth: badge.minWidth,
-		padding: badge.padding,
-		textAlign: 'center',
-		verticalAlign: 'baseline',
-		whiteSpace: 'nowrap',
-	};
+	const style = {
+		// Common styling
+		common: {
+			border: `${badge.borderWidth} solid transparent`,
+			borderRadius: badge.borderRadius,
+			display: 'inline-block',
+			fontSize: badge.fontSize,
+			fontWeight: badge.fontWeight,
+			lineHeight: badge.lineHeight,
+			minWidth: badge.minWidth,
+			padding: badge.padding,
+			textAlign: 'center',
+			verticalAlign: 'baseline',
+			whiteSpace: 'nowrap',
+		},
 
-	const styleAppearance = {
-		color: colors[appearance].foreground,
-		backgroundColor: colors[appearance].default,
-		borderColor: appearance === 'faint' ? colors.border : colors[appearance].default,
+		// Appearance styling
+		appearance: {
+			color: colors[appearance].foreground,
+			backgroundColor: colors[appearance].default,
+			borderColor: appearance === 'faint' ? colors.border : colors[appearance].default,
+		},
 	};
 
 	return (
-		<span css={{ ...common, ...styleAppearance }} {...props}>
+		<span css={{ ...style.common, ...style.appearance }} {...props}>
 			{children}
 		</span>
 	);
