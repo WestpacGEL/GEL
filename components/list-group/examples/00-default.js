e5c8d6958643c8fcec4987498cde035982769cae
Jonny Stening <info@jonathanstening.com>
2019-08-30 12:25:35 +1000

    chore(list-group): add missing package.json dependency, tidy up

--- components/list-group/examples/00-default.js
+++ components/list-group/examples/00-default.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from '../src';
-import { ButtonGroup, ButtonGroupButton } from '../../button-group/src';
+import { ButtonGroup, ButtonGroupButton } from '@westpac/button-group';
 
 const styleExample = {
-	paddingLeft: 12,
+	marginLeft: 12,
 };
 
 export default () => (
@@ -11,29 +11,29 @@ export default () => (
 		<ListGroupItem>
 			Send me sms reminders
 			<ButtonGroup size="small" name="example-default-1" css={styleExample}>
-				<ButtonGroupButton>Yes</ButtonGroupButton>
-				<ButtonGroupButton>No</ButtonGroupButton>
+				<ButtonGroupButton value="yes">Yes</ButtonGroupButton>
+				<ButtonGroupButton value="no">No</ButtonGroupButton>
 			</ButtonGroup>
 		</ListGroupItem>
 		<ListGroupItem>
 			Give me free money
 			<ButtonGroup size="small" name="example-default-2" css={styleExample}>
-				<ButtonGroupButton>Yes</ButtonGroupButton>
-				<ButtonGroupButton>No</ButtonGroupButton>
+				<ButtonGroupButton value="yes">Yes</ButtonGroupButton>
+				<ButtonGroupButton value="no">No</ButtonGroupButton>
 			</ButtonGroup>
 		</ListGroupItem>
 		<ListGroupItem>
 			Call me all the time
 			<ButtonGroup size="small" name="example-default-3" css={styleExample}>
-				<ButtonGroupButton>Yes</ButtonGroupButton>
-				<ButtonGroupButton>No</ButtonGroupButton>
+				<ButtonGroupButton value="yes">Yes</ButtonGroupButton>
+				<ButtonGroupButton value="no">No</ButtonGroupButton>
 			</ButtonGroup>
 		</ListGroupItem>
 		<ListGroupItem>
 			Deleted messages
 			<ButtonGroup size="small" name="example-default-4" css={styleExample}>
-				<ButtonGroupButton>Yes</ButtonGroupButton>
-				<ButtonGroupButton>No</ButtonGroupButton>
+				<ButtonGroupButton value="yes">Yes</ButtonGroupButton>
+				<ButtonGroupButton value="no">No</ButtonGroupButton>
 			</ButtonGroup>
 		</ListGroupItem>
 	</ListGroup>
