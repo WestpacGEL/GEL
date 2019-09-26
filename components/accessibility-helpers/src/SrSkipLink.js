11ed7ad35d25ca1635366eea6ea2f5dae057a2da
Jonny Stening <info@jonathanstening.com>
2019-09-26 23:36:12 +1000

    style(accessibility-helpers): move style object inline

--- components/accessibility-helpers/src/SrSkipLink.js
+++ components/accessibility-helpers/src/SrSkipLink.js
@@ -8,41 +8,42 @@ import { jsx } from '@westpac/core';
 // Component
 // ==============================
 
-export const SrSkipLink = ({ ...props }) => {
-	const common = {
-		position: 'absolute',
-		width: 1,
-		height: 1,
-		margin: -1,
-		padding: 0,
-		overflow: 'hidden',
-		clip: 'rect(0,0,0,0)',
-		border: 0,
-		fontSize: '1.3125rem',
+export const SrSkipLink = props => (
+	<a
+		css={{
+			position: 'absolute',
+			width: 1,
+			height: 1,
+			margin: -1,
+			padding: 0,
+			overflow: 'hidden',
+			clip: 'rect(0,0,0,0)',
+			border: 0,
+			fontSize: '1.3125rem',
 
-		':active, :focus': {
-			position: 'fixed',
-			top: 0,
-			right: 0,
-			left: 0,
-			height: 'auto',
-			width: 'auto',
-			margin: 0,
-			padding: '1em',
-			overflow: 'visible',
-			clip: 'auto',
-			textAlign: 'center',
-			zIndex: 2000,
-			backgroundColor: '#fff',
-		},
+			':active, :focus': {
+				position: 'fixed',
+				top: 0,
+				right: 0,
+				left: 0,
+				height: 'auto',
+				width: 'auto',
+				margin: 0,
+				padding: '1em',
+				overflow: 'visible',
+				clip: 'auto',
+				textAlign: 'center',
+				zIndex: 2000,
+				backgroundColor: '#fff',
+			},
 
-		':focus': {
-			outlineOffset: -2, //override to be inside
-		},
-	};
-
-	return <a css={common} {...props} />;
-};
+			':focus': {
+				outlineOffset: -2, //override to be inside
+			},
+		}}
+		{...props}
+	/>
+);
 
 // ==============================
 // Types
