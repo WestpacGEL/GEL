bbb770783c8a2315a6cc17cf35097c9ce50d06b6
Simon Vrachliotis <simon@simonswiss.com>
2019-09-30 13:04:16 +1000

    running prettier

--- helpers/transformers/web.js
+++ helpers/transformers/web.js
@@ -21,9 +21,7 @@ function convertFonts(fonts) {
 			output[''].push({
 				'@font-face': {
 					fontFamily: font.name,
-					src: `url("${font.files.woff2}") format("woff2"), url("${
-						font.files.woff
-					}") format("woff")`,
+					src: `url("${font.files.woff2}") format("woff2"), url("${font.files.woff}") format("woff")`,
 					weight: font.weight,
 					style: font.style,
 				},
