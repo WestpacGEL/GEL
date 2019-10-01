f67f60e766c5ff7a90a6101b255d84cdff0ec2a0
Simon Vrachliotis <simon@simonswiss.com>
2019-10-01 17:16:04 +1000

    Upgrade to React 16.10, addition of eslint config (#153)
    * WIP
    
    * fixing eslint errors
    
    * running prettier
    
    * format code
    
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
    
    * fixed lock file
    
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
    
    * fixed packages, removed linter for now
    
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- helpers/example/index.js
+++ helpers/example/index.js
@@ -39,7 +39,7 @@ const App = ({ components, packageName }) => {
 	// update doc title
 	useEffect(() => {
 		document.title = `${packageName} Example - GEL`;
-	}, packageName);
+	}, [packageName]);
 
 	// filter components for search
 	const navItems = inputValue.length
