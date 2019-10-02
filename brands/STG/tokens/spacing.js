f717575058cbb6b33c7bd3e1164e3c029083217a
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-10-02 14:21:18 +1000

    made code more DRY
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- brands/STG/tokens/spacing.js
+++ brands/STG/tokens/spacing.js
@@ -1,11 +1,6 @@
-const major = Array.from({ length: 21 }, (_, i) => i * 6);
-const minor = Array.from({ length: 40 }).reduce(
-	(acc, _, i) => {
-		if (i % 2) acc.push(i * 3);
-		return acc;
-	},
-	[0]
-);
+const { createUnits } = require('../../../helpers/transformers/utils');
+
+const { major, minor } = createUnits(6, 3, 20);
 
 module.exports = {
 	SPACING: {
