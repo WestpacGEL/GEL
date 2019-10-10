422a4a59ba3947967a2b83986e154da53b9b1ae9
Jonny Stening <info@jonathanstening.com>
2019-10-10 20:11:17 +1100

    style(label): inline styles

--- components/label/src/Label.js
+++ components/label/src/Label.js
@@ -11,51 +11,48 @@ import { jsx, useTheme } from '@westpac/core';
 export const Label = ({ appearance, tag: Tag, ...props }) => {
 	const { COLORS } = useTheme();
 
-	// Common styling
-	const styleCommon = {
-		border: '1px solid',
-		borderRadius: '0.125rem',
-		display: 'inline',
-		fontSize: '0.75rem',
-		fontWeight: 400,
-		lineHeight: 1,
-		padding: '0.0625rem 0.375rem',
-		textAlign: 'center',
-		verticalAlign: 'baseline',
-		whiteSpace: 'nowrap',
-
-		':empty': {
-			display: 'none',
-		},
-		'a&': {
-			textDecoration: 'none',
-		},
-
-		'@media print': {
-			color: '#000',
-			backgroundColor: '#fff',
-			border: '1px solid #000',
-		},
-	};
-
-	// Label appearance styling
-	const styleAppearance = {
-		color: appearance === 'faint' ? COLORS.muted : '#fff', //TODO: STG uses `COLORS.text`
-		backgroundColor: appearance === 'faint' ? COLORS.light : COLORS[appearance],
-		borderColor: appearance === 'faint' ? COLORS.border : COLORS[appearance],
-
-		'a&:hover, a&:focus': {
-			cursor: 'pointer',
-			backgroundColor: appearance === 'faint' ? '#fff' : COLORS.tints[`${appearance}50`],
-			borderColor: appearance !== 'faint' && COLORS.tints[`${appearance}50`],
-		},
-	};
-
 	if (props.href && Tag === 'span') {
 		Tag = 'a';
 	}
 
-	return <Tag css={{ ...styleCommon, ...styleAppearance }} {...props} />;
+	return (
+		<Tag
+			css={{
+				border: '1px solid',
+				borderRadius: '0.125rem',
+				display: 'inline',
+				fontSize: '0.75rem',
+				fontWeight: 400,
+				lineHeight: 1,
+				padding: '0.0625rem 0.375rem',
+				textAlign: 'center',
+				verticalAlign: 'baseline',
+				whiteSpace: 'nowrap',
+				color: appearance === 'faint' ? COLORS.muted : '#fff', //TODO: STG uses `COLORS.text`
+				backgroundColor: appearance === 'faint' ? COLORS.light : COLORS[appearance],
+				borderColor: appearance === 'faint' ? COLORS.border : COLORS[appearance],
+
+				':empty': {
+					display: 'none',
+				},
+				'a&': {
+					textDecoration: 'none',
+				},
+				'a&:hover, a&:focus': {
+					cursor: 'pointer',
+					backgroundColor: appearance === 'faint' ? '#fff' : COLORS.tints[`${appearance}50`],
+					borderColor: appearance !== 'faint' && COLORS.tints[`${appearance}50`],
+				},
+
+				'@media print': {
+					color: '#000',
+					backgroundColor: '#fff',
+					border: '1px solid #000',
+				},
+			}}
+			{...props}
+		/>
+	);
 };
 
 // ==============================
