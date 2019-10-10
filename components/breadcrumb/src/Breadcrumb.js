09f3646c53749c51fdd6b5afeab203411e940a86
Flore <37676464+florettewest@users.noreply.github.com>
2019-10-10 15:27:43 +1100

    Update Breadcrumb (#148)
    * refactor(breadcrumb): use new tokens, update rem values, move style objects inline
    
    * refactor(breadcrumb): PR amends
    
    * temporary fix so the preview works
    
    * adding a safeguard check for the right type of React.Children, to respect the API contract set with cloneElement
    
    * static prop instead of relying on type name which changes in production build
    
    * style(package.json): add package.json contributors
    
    * refactor(breadcrumb): add defaultProps

--- components/breadcrumb/src/Breadcrumb.js
+++ components/breadcrumb/src/Breadcrumb.js
@@ -2,7 +2,7 @@
 
 import React, { Children, cloneElement } from 'react';
 import PropTypes from 'prop-types';
-import { jsx, useTheme } from '@westpac/core';
+import { jsx } from '@westpac/core';
 import { SrOnly } from '@westpac/accessibility-helpers';
 
 // ==============================
@@ -13,23 +13,27 @@ import { SrOnly } from '@westpac/accessibility-helpers';
  * Breadcrumb: Breadcrumbs are styled navigational links used to indicate a user journey or path. They are a simple, effective and proven method to aid orientation.
  */
 export const Breadcrumb = ({ children, ...props }) => {
-	const { breadcrumb } = useTheme();
-
-	const common = {
-		padding: breadcrumb.padding,
-		marginBottom: breadcrumb.marginBottom,
-		fontSize: breadcrumb.fontSize,
-		listStyle: 'none',
-	};
-
-	const childrenWithProps = Children.map(children, (child, index) =>
-		index === Children.count(children) - 1 ? cloneElement(child, { last: 'true' }) : child
-	);
+	const childrenWithProps = Children.map(children, (child, index) => {
+		// Make sure we don't break the expected implementation contract of using `cloneElement`.
+		if (!child.type.isCrumb) {
+			throw new Error('<Breadcrumb /> only accepts <Crumb /> as direct children.');
+		}
+		return index === Children.count(children) - 1 ? cloneElement(child, { last: true }) : child;
+	});
 
 	return (
 		<div {...props}>
 			<SrOnly>Page navigation:</SrOnly>
-			<ol css={common}>{childrenWithProps}</ol>
+			<ol
+				css={{
+					padding: '0.375rem 1.125rem',
+					marginBottom: '1.3125rem',
+					fontSize: '0.8125rem',
+					listStyle: 'none',
+				}}
+			>
+				{childrenWithProps}
+			</ol>
 		</div>
 	);
 };
