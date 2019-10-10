55cc92346316892daacf1d09fc29138a11baffe6
Jeremy Ortiz <jeremy@jeremyortiz.io>
2019-10-10 12:54:52 +1100

    fix: prettier

--- components/modal/src/Modal.js
+++ components/modal/src/Modal.js
@@ -30,12 +30,9 @@ export const useModalContext = () => {
 export const Modal = ({ open: isOpen, onClose, size, children, ...props }) => {
 	const [open, setOpen] = useState(isOpen);
 
-	useEffect(
-		() => {
-			setOpen(isOpen);
-		},
-		[isOpen]
-	);
+	useEffect(() => {
+		setOpen(isOpen);
+	}, [isOpen]);
 
 	const handleClose = () => {
 		if (onClose) {
