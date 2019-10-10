4939090af40193076a59a833fa1122645f7a3f86
Jonny <info@jonathanstening.com>
2019-10-10 21:23:31 +1100

    Update FormCheck (#164)

--- components/form-check/examples/00-default.js
+++ components/form-check/examples/00-default.js
@@ -1,40 +1,40 @@
 import React from 'react';
-import { FormCheck, FormCheckItem } from '../src';
+import { FormCheck, Option } from '../src';
 
 export default () => (
 	<>
 		<h2>Default instance (no styling props)</h2>
 		<FormCheck name="example-default">
-			<FormCheckItem value="1">Option 1</FormCheckItem>
-			<FormCheckItem value="2">Option 2</FormCheckItem>
+			<Option value="1">Option 1</Option>
+			<Option value="2">Option 2</Option>
 		</FormCheck>
 
 		<hr />
 
 		<h2>Checkbox</h2>
 		<FormCheck type="checkbox" name="example-checkbox">
-			<FormCheckItem value="1">Option 1</FormCheckItem>
-			<FormCheckItem value="2">Option 2</FormCheckItem>
-			<FormCheckItem value="3">
+			<Option value="1">Option 1</Option>
+			<Option value="2">Option 2</Option>
+			<Option value="3">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et odit labore illo sint tempora
 				magnam modi nesciunt consectetur vitae maiores itaque reiciendis sunt nisi ullam officiis,
 				provident fugiat, esse iste adipisci repellat! Incidunt delectus, pariatur quaerat vitae
 				aspernatur eveniet libero.
-			</FormCheckItem>
+			</Option>
 		</FormCheck>
 
 		<hr />
 
 		<h2>Radio</h2>
 		<FormCheck type="radio" name="example-radio">
-			<FormCheckItem value="1">Option 1</FormCheckItem>
-			<FormCheckItem value="2">Option 2</FormCheckItem>
-			<FormCheckItem value="3">
+			<Option value="1">Option 1</Option>
+			<Option value="2">Option 2</Option>
+			<Option value="3">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et odit labore illo sint tempora
 				magnam modi nesciunt consectetur vitae maiores itaque reiciendis sunt nisi ullam officiis,
 				provident fugiat, esse iste adipisci repellat! Incidunt delectus, pariatur quaerat vitae
 				aspernatur eveniet libero.
-			</FormCheckItem>
+			</Option>
 		</FormCheck>
 	</>
 );
