a751725616af59c1bbc3a6a1e8ee629aca5c9416
Jeremy Ortiz <jeremy@jeremyortiz.io>
2019-10-11 13:53:10 +1100

    pretty please

--- components/list/src/ListItem.js
+++ components/list/src/ListItem.js
@@ -13,13 +13,9 @@ export const ListItem = ({ icon, children, ...props }) => {
 	const { COLORS } = useTheme();
 	const { appearance, type, spacing, icon: contextIcon } = useListContext();
 
-	const linkSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><polygon fill="${
-		COLORS.primary
-	}" fillRule="evenodd" points="14.588 12 8 18.588 9.412 20 17.412 12 9.412 4 8 5.412"/></svg>`;
+	const linkSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><polygon fill="${COLORS.primary}" fillRule="evenodd" points="14.588 12 8 18.588 9.412 20 17.412 12 9.412 4 8 5.412"/></svg>`;
 
-	const tickSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><polygon fill="${
-		COLORS.primary
-	}" points="8.6 15.6 4.4 11.4 3 12.8 8.6 18.4 20.6 6.4 19.2 5"/></svg>`;
+	const tickSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><polygon fill="${COLORS.primary}" points="8.6 15.6 4.4 11.4 3 12.8 8.6 18.4 20.6 6.4 19.2 5"/></svg>`;
 
 	const style = {
 		bullet: {
