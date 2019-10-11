e48c40c97ac4e919d2272c08b47e0894942349fe
Jeremy Ortiz <jeremy@jeremyortiz.io>
2019-10-11 12:20:32 +1100

    Modal (#138)

--- components/modal/src/ModalFooter.js
+++ components/modal/src/ModalFooter.js
@@ -4,17 +4,18 @@ import React from 'react';
 import { jsx, useTheme } from '@westpac/core';
 
 export const ModalFooter = props => {
-	const { modal } = useTheme();
+	const { COLORS } = useTheme();
 
 	return (
 		<div
 			css={{
-				backgroundColor: modal.footer.backgroundColor,
-				borderTop: `${modal.footer.borderWidth} solid ${modal.footer.borderColor}`,
+				backgroundColor: COLORS.background,
+				borderTop: `1px solid ${COLORS.border}`,
 				textAlign: 'right',
-				padding: modal.footer.padding,
+				padding: '0.75rem 1.125rem',
+
 				'button + button': {
-					marginLeft: '0.6rem',
+					marginLeft: '0.375rem',
 				},
 			}}
 			{...props}
