95afb21d760461ba23f97d834ed3a48ac5fd549c
Jonny <info@jonathanstening.com>
2019-10-17 13:45:03 +1100

    Remove neutral option from Button and ButtonGroup (#168)

--- components/button/examples/30-block.js
+++ components/button/examples/30-block.js
@@ -4,49 +4,37 @@ import { Button } from '../src';
 export default () => (
 	<>
 		<h2>Default</h2>
-		<p>
-			<Button appearance="primary" size="xlarge" block>
-				Primary extra large block button
-			</Button>
-		</p>
-		<p>
-			<Button appearance="hero" size="large" block>
-				Hero large block button
-			</Button>
-		</p>
-		<p>
-			<Button appearance="neutral" size="medium" block>
-				Neutral medium block button
-			</Button>
-		</p>
-		<p>
-			<Button appearance="faint" size="small" block>
-				Faint small block button
-			</Button>
-		</p>
+
+		<Button appearance="primary" size="xlarge" block>
+			Primary extra large block button
+		</Button>
+		<br />
+
+		<Button appearance="hero" size="large" block>
+			Hero large block button
+		</Button>
+		<br />
+
+		<Button appearance="faint" size="small" block>
+			Faint small block button
+		</Button>
 
 		<hr />
 
 		<h2>Soft</h2>
-		<p>
-			<Button appearance="primary" size="xlarge" soft block>
-				Primary extra large soft block button
-			</Button>
-		</p>
-		<p>
-			<Button appearance="hero" size="large" soft block>
-				Hero large soft block button
-			</Button>
-		</p>
-		<p>
-			<Button appearance="neutral" size="medium" soft block>
-				Neutral medium soft block button
-			</Button>
-		</p>
-		<p>
-			<Button appearance="faint" size="small" soft block>
-				Faint small soft block button
-			</Button>
-		</p>
+
+		<Button appearance="primary" size="xlarge" soft block>
+			Primary extra large soft block button
+		</Button>
+		<br />
+
+		<Button appearance="hero" size="large" soft block>
+			Hero large soft block button
+		</Button>
+		<br />
+
+		<Button appearance="faint" size="small" soft block>
+			Faint small soft block button
+		</Button>
 	</>
 );
