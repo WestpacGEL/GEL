5be6aeee1f7184e272a869328ac618ee8018a532
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-10-22 11:18:55 +1100

    added headings component
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

---
+++ components/heading/examples/00-default.js
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+
+import { GEL, jsx } from '@westpac/core';
+import { Heading } from '@westpac/heading';
+
+function Example({ brand }) {
+	return (
+		<GEL brand={brand}>
+			<h2>Without <code>tag</code> prop</h2>
+			<Heading size='1'>Size: 1</Heading>
+			<Heading size={2}>Size: 2</Heading>
+			<hr/>
+			<h2>With <code>tag</code> prop</h2>
+			<Heading tag='h2' size='1'>Tag: h2 size: 1</Heading>
+			<Heading tag='h1' size='2'>Tag: h1 size: 2</Heading>
+			<hr/>
+			<h2>Invalid props</h2>
+			<Heading size='0'>Size too small</Heading>
+			<Heading size='10'>Size too large</Heading>
+			<Heading tag='span' size='5'>Tag not headline tag</Heading>
+		</GEL>
+	);
+}
+
+export default Example;
