7fe1e04c2160a516e4870e9dfc64b181a959d0e8
Jeremy Ortiz <jeremy@jeremyortiz.io>
2019-11-04 13:22:46 +1100

    add useOutsideClick

---
+++ components/core/examples/65-useOutsideClick.js
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+
+import { useRef } from 'react';
+import { GEL, jsx, useOutsideClick } from '@westpac/core';
+import { Button } from '@westpac/button';
+import { Code } from './_utils';
+
+function Example({ brand }) {
+	const ref = useRef();
+
+	useOutsideClick(ref, () => alert('Clicked outside'));
+
+	return (
+		<GEL brand={brand}>
+			<Code>{`useOutsideClick(ref, () => alert(Clicked outside))`}</Code>
+			<br />
+			<div css={{ display: 'inline-block' }} ref={ref}>
+				<Button>Click outside me</Button>
+			</div>
+		</GEL>
+	);
+}
+
+export default Example;
--- components/core/src/index.js
+++ components/core/src/index.js
@@ -1,5 +1,6 @@
 export { jsx, css, Global, ClassNames } from '@emotion/core';
 export { BrandContext, useBrand } from './Brand'; // We need to export the context object for class components
+export { useOutsideClick } from './useOutsideClick';
 export { useMediaQuery } from './useMediaQuery';
 export { wrapHandlers } from './wrapHandlers';
 export { devWarning } from './devWarning';
