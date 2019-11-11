713db36c7f0b6ecce9f9fb6d6a745c032dd9e25d
Jeremy Ortiz <jeremy@jeremyortiz.io>
2019-11-12 08:34:50 +1100

    fix docs build

--- helpers/docz/wrapper.js
+++ helpers/docz/wrapper.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { GEL } from '@westpac/core';
 import defaultBrand from '@westpac/wbc';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function({ children }) {
 	return (
-		<GEL brand={defaultBrand}>
-			<style>{`
+		<Router>
+			<GEL brand={defaultBrand}>
+				<style>{`
 				.react-live-preview {
 					height: 100%;
 				}
 			`}</style>
-			{children}
-		</GEL>
+				{children}
+			</GEL>
+		</Router>
 	);
 }
