1b8f5c2566d9b806365eeb6e7011013ede4814bf
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-11-27 14:46:04 +1100

    fixed up deployment of docs site
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- helpers/example/docs.webpack.config.js
+++ helpers/example/docs.webpack.config.js
@@ -55,7 +55,7 @@ module.exports = () => ({
 
 	output: {
 		filename: '[name]-[hash].js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, '../../docs'),
 		publicPath: '/',
 	},
 
