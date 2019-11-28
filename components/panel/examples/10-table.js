b8ea14f01a2956ed5bdce24cf4ba6348b4b8bba5
Flore Laforge <flore.laforge@westpac.com.au>
2019-11-28 15:52:10 +1100

    refactor(panel): add new markup for table

--- components/panel/examples/10-table.js
+++ components/panel/examples/10-table.js
@@ -2,7 +2,7 @@
 
 import { GEL, jsx } from '@westpac/core';
 import { Panel, Header, Body } from '@westpac/panel';
-import { PanelTable } from '@westpac/table';
+import { Table, Caption, Thead, Tr, Th, Tbody, Td, Tfoot } from '@westpac/table';
 
 function Example({ brand }) {
 	return (
@@ -15,43 +15,50 @@ function Example({ brand }) {
 					bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam
 					venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit.
 				</Body>
-				<PanelTable striped responsive>
-					<caption>
+				<Table bordered>
+					<Caption>
 						Table caption this table width is: <em>(100%)</em>
-					</caption>
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Username</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Justin</td>
-							<td>Spencer</td>
-							<td>
-								<i>Justin has no twitter</i>
-							</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Dominik</td>
-							<td>Wilkowski</td>
-							<td>@wilkowskidom</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Peter</td>
-							<td>Clancy</td>
-							<td>
-								<i>Pete has no twitter either... :(</i>
-							</td>
-						</tr>
-					</tbody>
-				</PanelTable>
+					</Caption>
+					<Thead>
+						<Tr>
+							<Th scope="col">Column 1</Th>
+							<Th scope="col">Column 2</Th>
+							<Th scope="col">Column 3</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						<Tr>
+							<Td>Cell 1</Td>
+							<Td>Cell 7</Td>
+							<Td>Cell 13</Td>
+						</Tr>
+						<Tr>
+							<Td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Td>
+							<Td>Odit eligendi quasi quo nihil quia reiciendis obcaecati nosTrum.</Td>
+							<Td>Rem saepe eos, modi perferendis.</Td>
+						</Tr>
+						<Tr>
+							<Td>Cell 3</Td>
+							<Td>Cell 9</Td>
+							<Td>Cell 15</Td>
+						</Tr>
+						<Tr>
+							<Td>Cell 4</Td>
+							<Td>Cell 10</Td>
+							<Td>Cell 16</Td>
+						</Tr>
+						<Tr>
+							<Td>Cell 5</Td>
+							<Td>Cell 11</Td>
+							<Td>Cell 17</Td>
+						</Tr>
+					</Tbody>
+					<Tfoot>
+						<Tr>
+							<Td colSpan="3">Footer goes here and should colSpan all columns</Td>
+						</Tr>
+					</Tfoot>
+				</Table>
 			</Panel>
 
 			<hr />
@@ -59,45 +66,50 @@ function Example({ brand }) {
 			<h2>Responsive panel with table only</h2>
 			<Panel>
 				<Header>Panel title</Header>
-				<PanelTable striped responsive>
-					<caption>
+				<Table>
+					<Caption>
 						Table caption this table width is: <em>(100%)</em>
-					</caption>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Justin</td>
-							<td>Spencer</td>
-							<td>
-								<i>Justin has no twitter</i>
-							</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Dominik</td>
-							<td>Wilkowski</td>
-							<td>@wilkowskidom</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Peter</td>
-							<td>Clancy</td>
-							<td>
-								<i>Pete has no twitter either... :(</i>
-							</td>
-						</tr>
-						<tr>
-							<td>4</td>
-							<td>Someone else</td>
-							<td>[not disclosed]</td>
-							<td>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta repudiandae,
-								dolores, quam sapiente esse maxime dolor provident impedit similique facilis
-								reprehenderit. Provident voluptatem eum, ipsum illum quisquam quam beatae iste.
-							</td>
-						</tr>
-					</tbody>
-				</PanelTable>
+					</Caption>
+					<Thead>
+						<Tr>
+							<Th scope="col">Column 1</Th>
+							<Th scope="col">Column 2</Th>
+							<Th scope="col">Column 3</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						<Tr>
+							<Td>Cell 1</Td>
+							<Td>Cell 7</Td>
+							<Td>Cell 13</Td>
+						</Tr>
+						<Tr>
+							<Td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Td>
+							<Td>Odit eligendi quasi quo nihil quia reiciendis obcaecati nosTrum.</Td>
+							<Td>Rem saepe eos, modi perferendis.</Td>
+						</Tr>
+						<Tr>
+							<Td>Cell 3</Td>
+							<Td>Cell 9</Td>
+							<Td>Cell 15</Td>
+						</Tr>
+						<Tr>
+							<Td>Cell 4</Td>
+							<Td>Cell 10</Td>
+							<Td>Cell 16</Td>
+						</Tr>
+						<Tr>
+							<Td>Cell 5</Td>
+							<Td>Cell 11</Td>
+							<Td>Cell 17</Td>
+						</Tr>
+					</Tbody>
+					<Tfoot>
+						<Tr>
+							<Td colSpan="3">Footer goes here and should colSpan all columns</Td>
+						</Tr>
+					</Tfoot>
+				</Table>
 			</Panel>
 		</GEL>
 	);
