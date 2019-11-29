baea8dcce234b78180d445e0491b04391ffc7fc2
Jonny <info@jonathanstening.com>
2019-11-29 14:57:17 +1100

    Update Alert (#203)

--- components/alert/examples/30-dismissible.js
+++ components/alert/examples/30-dismissible.js
@@ -10,23 +10,23 @@ function Example({ brand }) {
 		<GEL brand={brand}>
 			<Intopia />
 
-			<Alert look="success" closable>
+			<Alert look="success" dismissible>
 				<strong>Well done!</strong> You successfully read this important alert message. Hey neato, I
 				can be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="info" closable>
+			<Alert look="info" dismissible>
 				<strong>Heads up!</strong> This alert needs your attention, but it’s not super important.
 				Hey neato, I can be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="warning" closable>
+			<Alert look="warning" dismissible>
 				<strong>Warning!</strong> Better check yourself, you’re not looking too good. Hey neato, I
 				can be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="danger" closable>
+			<Alert look="danger" dismissible>
 				<strong>Oh snap!</strong> Change a few things up and try submitting again. Hey neato, I can
 				be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="system" closable>
+			<Alert look="system" dismissible>
 				<strong>System Error 8942:</strong> The server is no responding. Please try again later.
 				Sorry for the inconvenience. Hey neato, I can be closed. <a href="#">Link</a>
 			</Alert>
