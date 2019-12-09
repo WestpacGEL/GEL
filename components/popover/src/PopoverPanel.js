734ec26bb975aa054f207135d9d8357dadd8f627
Jonny <info@jonathanstening.com>
2019-12-09 13:02:15 +1100

    Dismissible popover (#210)
    * feat(popover): add dismissible popover option, use Body to wrap popover content (popover should be able to accept html type content)
    
    * be prettier

--- components/popover/src/PopoverPanel.js
+++ components/popover/src/PopoverPanel.js
@@ -6,13 +6,14 @@ import { jsx, useBrand } from '@westpac/core';
 import ScrollLock from 'react-scrolllock';
 
 export const PopoverPanel = forwardRef(
-	({ open, title, content, position, Panel, ...props }, ref) => {
+	({ open, title, content, position, Panel, handleClose, ...props }, ref) => {
 		return (
 			open &&
 			ReactDOM.createPortal(
 				<Fragment>
 					<Panel
 						position={position}
+						handleClose={handleClose}
 						title={title}
 						content={content}
 						ref={ref}
