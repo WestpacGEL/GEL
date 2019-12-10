1916ad61bc87c25e50e91db542b05dcc071208ae
Jonny Stening <info@jonathanstening.com>
2019-12-10 12:22:16 +1100

    refactor(tooltip): add tag prop, add Body wrap in example, remove borderBottom reset

--- components/tooltip/examples/00-default.js
+++ components/tooltip/examples/00-default.js
@@ -4,6 +4,7 @@ import { GEL, jsx } from '@westpac/core';
 import { Tooltip } from '@westpac/tooltip';
 import { Button } from '@westpac/button';
 import { InfoIcon } from '@westpac/icon';
+import { Body } from '@westpac/body';
 
 import { Intopia } from '../../../helpers/example/components/Intopia.js';
 
@@ -12,28 +13,34 @@ function Example({ brand }) {
 		<GEL brand={brand}>
 			<Intopia />
 
-			<h3>In text</h3>
-			<p>
-				This is a <Tooltip text="This is a tooltip">random</Tooltip> sentence.
-			</p>
-			<Tooltip text="This is another tooltip">
+			<Body>
+				<h3>In text</h3>
 				<p>
-					Tooltip as a div block elements... Lorem ipsum dolor sit amet, consectetur adipisicing
-					elit. Enim adipisci laboriosam unde dolore, maxime quae amet praesentium minus sit! Cumque
-					repudiandae laboriosam sit ipsum eaque cupiditate temporibus, necessitatibus consectetur
-					debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam itaque sunt hic
-					enim earum explicabo.
+					This is a{' '}
+					<Tooltip tag="abbr" text="This is a tooltip">
+						random
+					</Tooltip>{' '}
+					sentence.
 				</p>
-			</Tooltip>
-			<h3>On icons</h3>
-			<Tooltip text="Some text to explain this icon">
-				<InfoIcon />
-			</Tooltip>
-			<h3>On Buttons</h3>
-			<Tooltip text="This is another very long tooltip to see what happens with long tooltips">
-				<Button appearance="hero">Tooltip as a button</Button>
-			</Tooltip>
-			<div css={{ height: '100vh' }} />
+				<Tooltip text="This is another tooltip">
+					<p>
+						Tooltip as a div block elements... Lorem ipsum dolor sit amet, consectetur adipisicing
+						elit. Enim adipisci laboriosam unde dolore, maxime quae amet praesentium minus sit!
+						Cumque repudiandae laboriosam sit ipsum eaque cupiditate temporibus, necessitatibus
+						consectetur debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+						itaque sunt hic enim earum explicabo.
+					</p>
+				</Tooltip>
+				<h3>On icons</h3>
+				<Tooltip text="Some text to explain this icon">
+					<InfoIcon />
+				</Tooltip>
+				<h3>On Buttons</h3>
+				<Tooltip text="This is another very long tooltip to see what happens with long tooltips">
+					<Button appearance="hero">Tooltip as a button</Button>
+				</Tooltip>
+				<div css={{ height: '100vh' }} />
+			</Body>
 		</GEL>
 	);
 }
