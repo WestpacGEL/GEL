448c4108eb1eba00979db265baf35982e3178e33
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-12-13 10:40:35 +1100

    moved polyfills above react
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- helpers/example/docs.js
+++ helpers/example/docs.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import 'core-js/es6/string';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'raf/polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { AppDocs } from './components/AppDocs';
 
