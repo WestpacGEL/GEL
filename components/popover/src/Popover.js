0d5d481225618ba81d88a93a1294949f3f00cd98
Jeremy Ortiz <jeremy@jeremyortiz.io>
2019-12-12 11:15:19 +1100

    Update Popover (#211)
    * feat(popover): initial commit
    
    * feat(popover): working component
    
    * fix(popover): fix spelling mistake
    
    * feat(popover): update deps, add conditional render of popover
    
    * refactor(popover): udpate to useBrand
    
    * add scroll lock
    
    * update dev sandbox implementation;
    
    return scroll to the body
    
    * update to position fixed
    
    * add overrides
    
    * make prettier
    
    * refactor overrides
    
    * move override components
    
    * remove scroll lock and refactor
    
    * fix errors

--- components/popover/src/Popover.js
+++ components/popover/src/Popover.js
@@ -1,13 +1,10 @@
 /** @jsx jsx */
 
 import { useState, useEffect, useRef, forwardRef, Fragment } from 'react';
-import PropTypes from 'prop-types';
 import { jsx, useBrand, merge } from '@westpac/core';
-import { Body } from '@westpac/body';
-import { CloseIcon } from '@westpac/icon';
-import { Button } from '@westpac/button';
-import pkg from '../package.json';
 import { PopoverPanel } from './PopoverPanel';
+import PropTypes from 'prop-types';
+import pkg from '../package.json';
 
 // ==============================
 // Component
@@ -20,10 +17,8 @@ export const Popover = ({ open: isOpen, title, content, dismissible, children, .
 	const popoverRef = useRef();
 
 	const overrides = {
-		CSS: {},
-		Wrapper,
-		Panel,
-		CloseBtn,
+		css: {},
+		Wrapper: forwardRef((props, ref) => <div ref={ref} {...props} />),
 	};
 
 	merge(overrides, overridesWithTokens);
@@ -31,10 +26,6 @@ export const Popover = ({ open: isOpen, title, content, dismissible, children, .
 	useEffect(() => {
 		setOpen(isOpen);
 	}, [isOpen]);
-
-	const handleClose = () => {
-		setOpen(false);
-	};
 
 	useEffect(() => {
 		if (open) {
@@ -46,18 +37,18 @@ export const Popover = ({ open: isOpen, title, content, dismissible, children, .
 			const left = (trigger.left - popover.width / 2 + trigger.width / 2) / remSize;
 
 			if (popover.height > trigger.top) {
-				const top = (trigger.top + trigger.height + remSize) / remSize;
+				const top = (trigger.top + window.scrollY + trigger.height + remSize) / remSize;
 				setPosition({ placement: 'bottom', top, left });
 			} else {
-				const top = (trigger.top - popover.height - remSize) / remSize;
+				const top = (trigger.top + window.scrollY - popover.height - remSize) / remSize;
 				setPosition({ placement: 'top', top, left });
 			}
 		}
 	}, [open]);
 
 	const handleOutsideClick = e => {
-		if (dismissible && open && !popoverRef.current.contains(e.target)) {
-			handleClose();
+		if (dismissible && open && popoverRef.current && !popoverRef.current.contains(e.target)) {
+			handleOpen();
 		}
 	};
 
@@ -69,10 +60,6 @@ export const Popover = ({ open: isOpen, title, content, dismissible, children, .
 			document.removeEventListener('click', handleOutsideClick);
 		};
 	}, [open]);
-
-	const handleClick = () => {
-		setOpen(!open);
-	};
 
 	// on escape close modal
 	const keyHandler = event => {
@@ -87,6 +74,10 @@ export const Popover = ({ open: isOpen, title, content, dismissible, children, .
 		};
 	});
 
+	const handleOpen = () => {
+		setOpen(!open);
+	};
+
 	return (
 		<Fragment>
 			<PopoverPanel
@@ -94,14 +85,13 @@ export const Popover = ({ open: isOpen, title, content, dismissible, children, .
 				content={content}
 				open={open}
 				position={position}
-				handleClose={handleClose}
+				handleOpen={handleOpen}
 				ref={popoverRef}
-				Panel={overrides.Panel}
 			/>
 			<overrides.Wrapper
 				css={{ display: 'inline-block', ...overrides.CSS }}
 				ref={triggerRef}
-				onClick={handleClick}
+				onClick={handleOpen}
 				{...props}
 			>
 				{children}
@@ -136,91 +126,3 @@ Popover.defaultProps = {
 	open: false,
 	dismissible: false,
 };
-
-// ==============================
-// Override Component
-// ==============================
-const Wrapper = forwardRef((props, ref) => <div ref={ref} {...props} />);
-
-const Panel = forwardRef(({ title, content, position, handleClose, ...props }, ref) => {
-	const { SPACING, COLORS } = useBrand();
-	return (
-		<div
-			ref={ref}
-			tabIndex="-1"
-			aria-label="Use the ESC key to close"
-			css={{
-				boxShadow: '0 5px 10px rgba(0, 0, 0, 0.2)',
-				border: `1px solid ${COLORS.muted}`,
-				borderRadius: 3,
-				width: '17.625rem',
-				backgroundColor: '#fff',
-				pointerEvents: 'all',
-				textAlign: 'left',
-
-				'::before': {
-					content: '""',
-					position: 'absolute',
-					[position.placement === 'top' ? 'bottom' : 'top']: '-0.8125rem',
-					left: '50%',
-					marginLeft: '-0.5rem',
-					width: 0,
-					[position.placement === 'top'
-						? 'borderTop'
-						: 'borderBottom']: `0.75rem solid ${COLORS.muted}`,
-					borderRight: '0.5rem solid transparent',
-					borderLeft: '0.5rem solid transparent',
-					fontSize: 0,
-					lineHeight: 0,
-				},
-
-				...(position.placement === 'top' && {
-					'::after': {
-						content: '""',
-						position: 'absolute',
-						bottom: '-0.6875rem',
-						left: '50%',
-						marginLeft: '-0.5rem',
-						width: 0,
-						borderTop: '0.75rem solid #fff',
-						borderRight: '0.5rem solid transparent',
-						borderLeft: '0.5rem solid transparent',
-						fontSize: 0,
-						lineHeight: 0,
-					},
-				}),
-			}}
-			{...props}
-		>
-			<p
-				css={{
-					margin: 0,
-					padding: '0.625rem 0.75rem',
-					backgroundColor: COLORS.muted,
-					color: '#fff',
-					fontSize: '1rem',
-				}}
-			>
-				{title}
-			</p>
-			<Body css={{ margin: 0, padding: '0.625rem 0.75rem', color: COLORS.neutral }}>{content}</Body>
-			<CloseBtn
-				onClose={handleClose}
-				icon={CloseIcon}
-				css={{
-					position: 'absolute',
-					top: 0,
-					right: SPACING(1),
-					color: '#fff',
-					':hover svg': {
-						opacity: 0.5,
-					},
-				}}
-			/>
-		</div>
-	);
-});
-
-const CloseBtn = ({ onClose, icon, ...props }) => (
-	<Button onClick={() => onClose()} iconAfter={icon} look="link" {...props} />
-);
