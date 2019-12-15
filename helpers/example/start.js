3aa3235af8077150e3ce8284303f2b5cc64652a1
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-12-16 09:11:20 +1100

    added core-js latest
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- helpers/example/start.js
+++ helpers/example/start.js
@@ -1,9 +1,5 @@
-import 'core-js/es6/string';
-import 'core-js/es6/object';
-import 'core-js/es6/array';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
 import 'raf/polyfill';
+import 'core-js/es';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
