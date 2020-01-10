a281f88c4c75d3753c1bda817b2680d632755e1f
Simon Vrachliotis <simon@simonswiss.com>
2020-01-10 10:05:34 +0100

    moving the code example stuff to the code-tab component

--- website/site/components/component-view/code-tab.js
+++ website/site/components/component-view/code-tab.js
@@ -2,7 +2,11 @@
 import { jsx, useBrand } from '@westpac/core';
 import { Grid, Cell } from '@westpac/grid';
 import { Heading } from '@westpac/heading';
+
+import ChangelogWrapper from '../../components/changelog';
 import { PageLinks } from './page-links';
+import { Playground } from '../../components/playground';
+// import ReactLive from '../../components/react-live';
 
 export const CodeTab = ({ dataComponent: DataComponent }) => {
 	const { SPACING, PACKS } = useBrand();
@@ -35,3 +39,67 @@ export const CodeTab = ({ dataComponent: DataComponent }) => {
 		</div>
 	);
 };
+
+// ==============================
+// Examples
+// ==============================
+
+const Examples = ({ examples, name }) => {
+	// Bail out if there are no examples in the snippets folder
+	if (!Object.keys(examples).length > 0 || Object.keys(examples)[0] === 'default') {
+		return (
+			<Alert look="warning">
+				No live code examples for this component. They can be added in{' '}
+				<code>
+					@westpac/{name}
+					/website/snippets
+				</code>
+				.
+			</Alert>
+		);
+	}
+
+	// If we have examples, display each group (file) in a Tab
+	return (
+		<Tabcordion mode="tabs" appearance="soft">
+			{Object.entries(examples).map(([name, snippets]) => (
+				<Tab key={name} label={name.charAt(0).toUpperCase() + name.slice(1).replace('_', ' ')}>
+					<ExamplesGroup key={name} snippets={snippets} />
+				</Tab>
+			))}
+		</Tabcordion>
+	);
+};
+
+const Nest = () => {
+	return <p>Hello</p>;
+};
+
+const Test = () => {
+	return (
+		<p>
+			<Nest />
+		</p>
+	);
+};
+
+const myTest = elm => {
+	const c = elm.props.children;
+	console.log(React.Children.count(c));
+	console.log(React.Children.toArray(c));
+};
+
+const ExamplesGroup = ({ snippets }) => {
+	return Object.entries(snippets).map(([name, code]) => {
+		if (name !== 'scope') {
+			return (
+				<Playground scope={{ Test }}>
+					<Test prop="string" />
+				</Playground>
+			);
+			//<Example key={name} name={name} code={code} scope={snippets.scope} />;
+		}
+	});
+};
+
+// const Example = ({ code, scope }) => <ReactLive code={code} scope={scope}></ReactLive>;
--- website/site/pages/components/[component].js
+++ website/site/pages/components/[component].js
@@ -4,14 +4,10 @@ import { useQuery } from '@apollo/react-hooks';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 
-import { Alert } from '@westpac/alert';
 import { jsx, useBrand } from '@westpac/core';
 import { Tab, Tabcordion } from '@westpac/tabcordion';
 
-// import ReactLive from '../../components/react-live';
 import { AccessibilityTab, CodeTab, DesignTab, PageHeader } from '../../components/component-view';
-import { Playground } from '../../components/playground';
-import ChangelogWrapper from '../../components/changelog';
 import { ALL_COMPONENTS } from '../../../graphql';
 
 const ComponentWrapper = () => {
@@ -67,69 +63,5 @@ const Component = ({ component }) => {
 		</Fragment>
 	);
 };
-
-// ==============================
-// Examples
-// ==============================
-
-const Examples = ({ examples, name }) => {
-	// Bail out if there are no examples in the snippets folder
-	if (!Object.keys(examples).length > 0 || Object.keys(examples)[0] === 'default') {
-		return (
-			<Alert look="warning">
-				No live code examples for this component. They can be added in{' '}
-				<code>
-					@westpac/{name}
-					/website/snippets
-				</code>
-				.
-			</Alert>
-		);
-	}
-
-	// If we have examples, display each group (file) in a Tab
-	return (
-		<Tabcordion mode="tabs" appearance="soft">
-			{Object.entries(examples).map(([name, snippets]) => (
-				<Tab key={name} label={name.charAt(0).toUpperCase() + name.slice(1).replace('_', ' ')}>
-					<ExamplesGroup key={name} snippets={snippets} />
-				</Tab>
-			))}
-		</Tabcordion>
-	);
-};
-
-const Nest = () => {
-	return <p>Hello</p>;
-};
-
-const Test = () => {
-	return (
-		<p>
-			<Nest />
-		</p>
-	);
-};
-
-const myTest = elm => {
-	const c = elm.props.children;
-	console.log(React.Children.count(c));
-	console.log(React.Children.toArray(c));
-};
-
-const ExamplesGroup = ({ snippets }) => {
-	return Object.entries(snippets).map(([name, code]) => {
-		if (name !== 'scope') {
-			return (
-				<Playground scope={{ Test }}>
-					<Test prop="string" />
-				</Playground>
-			);
-			//<Example key={name} name={name} code={code} scope={snippets.scope} />;
-		}
-	});
-};
-
-// const Example = ({ code, scope }) => <ReactLive code={code} scope={scope}></ReactLive>;
 
 export default ComponentWrapper;
