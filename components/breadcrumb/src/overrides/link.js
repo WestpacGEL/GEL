aeb15bbe44a1d58a8d1ed933be0fbfac51f9bfc0
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-02-03 14:59:32 +1100

    fixed breadcrumbs anchor link
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- components/breadcrumb/src/overrides/link.js
+++ components/breadcrumb/src/overrides/link.js
@@ -3,16 +3,29 @@
 import { jsx, useBrand } from '@westpac/core';
 import React from 'react';
 
-export const Link = ({ current, assistiveText, ...props }) => <a {...props} />;
+export const Link = ({ current, assistiveText, href, ...props }) => {
+	const Tag = href ? 'a' : 'button';
+
+	return (
+		<Tag href={href} {...props} />
+	);
+};
 
 export const linkStyles = (_, { current }) => {
 	const { COLORS } = useBrand();
 
 	return {
 		color: COLORS.text,
+		boxSizing: 'border-box',
 		textDecoration: 'none',
 		verticalAlign: 'middle',
 		cursor: current ? 'auto' : 'pointer',
+		appearance: 'none',
+		background: 'none',
+		border: 'none',
+		padding: 0,
+		margin: 0,
+		display: 'inline',
 
 		':focus, :hover': {
 			textDecoration: current ? 'none' : 'underline',
