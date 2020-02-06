46b1bf833de1b5079183797988965a1916564421
Mike <mike@madebymike.com.au>
2020-02-06 15:11:57 +1100

    Playground + babel plugin

--- website/index.js
+++ website/index.js
@@ -20,7 +20,7 @@ const apps = [
 		adminPath: '/admin',
 		hooks: require.resolve('./admin'),
 	}),
-	// new NextApp({ dir: 'site' }),
+	new NextApp({ dir: 'site' }),
 ];
 
 const options = resolveComponents();
@@ -40,7 +40,6 @@ const prepareKeystone = async () => {
 		.prepare({ apps, dev: process.env.NODE_ENV !== 'production' })
 		.then(async ({ middlewares }) => {
 			await keystone.connect();
-
 			const app = express();
 			app.use(middlewares).listen(3000);
 		});
--- website/site/components/playground/index.js
+++ website/site/components/playground/index.js
@@ -1,28 +1,15 @@
 /** @jsx jsx */
 import { jsx } from '@westpac/core';
 import { Fragment } from 'react';
-import jsxString from 'react-element-to-jsx-string';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
-function renderCode(code) {
-	return jsxString(code, { maxInlineAttributesLineLength: 100, showFunctions: true });
-}
-
-export const Playground = ({ editor = false, children, scope, theme = {} }) => {
-	let code = '';
-	let inline = false;
-
-	if (typeof children === 'string') {
-		code = children;
-		inline = true;
-	} else {
-		if (children.length > 1) {
-			code = renderCode(<Fragment>{children}</Fragment>);
-		} else {
-			code = renderCode(children);
-		}
+export const Playground = ({ editor = false, code, children, scope, inline, theme = {} }) => {
+	console.log(code);
+	if (!code && children.length > 1) {
+		console.warning(
+			'Playground code has not been compiled by the babel plugin. Please check configuration.'
+		);
 	}
-
 	if (editor) {
 		return (
 			<LiveProvider code={code} scope={scope} noInline={inline}>
