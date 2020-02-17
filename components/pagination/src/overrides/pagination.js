1445bce652db5c3908940c6003021ab04dbf3178
Jonny Stening <info@jonathanstening.com>
2020-02-17 14:08:19 +1100

    fix(pagination): revert wrapping element update

--- components/pagination/src/overrides/pagination.js
+++ components/pagination/src/overrides/pagination.js
@@ -2,6 +2,6 @@
 
 import { jsx } from '@westpac/core';
 
-export const Pagination = ({ current, infinite, back, next, data, ...rest }) => <ul {...rest} />;
+export const Pagination = ({ current, infinite, back, next, data, ...rest }) => <nav {...rest} />;
 
 export const paginationStyles = () => ({});
