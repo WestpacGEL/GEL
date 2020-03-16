733a083e589d084f1d0b1ea6e5d3e45822a85f1b
Mike <mike@madebymike.com.au>
2020-03-17 08:55:19 +1100

    Update deps

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-react'],
 	plugins: [
@@ -5,5 +7,27 @@ module.exports = {
 		'babel-plugin-codegen',
 		'@babel/plugin-transform-runtime',
 		'macros',
+	],
+
+	overrides: [
+		{
+			test: x => x && x.includes('field-content'),
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						targets: {
+							node: 10,
+							browsers: [
+								'last 2 chrome versions',
+								'last 2 firefox versions',
+								'last 2 safari versions',
+								'last 2 edge versions',
+							],
+						},
+					},
+				],
+			],
+		},
 	],
 };
--- field-content/dist/field-content.cjs.js
+++ field-content/dist/field-content.cjs.js
@@ -1,7 +1,19 @@
-'use strict';
+"use strict";
+// this file might look strange and you might be wondering what it's for
+// it's lets you import your source files by importing this entrypoint
+// as you would import it if it was built with preconstruct build
+// this file is slightly different to some others though
+// it has a require hook which compiles your code with Babel
+// this means that you don't have to set up @babel/register or anything like that
+// but you can still require this module and it'll be compiled
 
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./field-content.cjs.prod.js");
-} else {
-  module.exports = require("./field-content.cjs.dev.js");
-}
+// this bit of code imports the require hook and registers it
+let unregister = require("/Users/mike/Development/GEL/node_modules/@preconstruct/hook/dist/hook.cjs.js").___internalHook("/Users/mike/Development/GEL");
+
+// this re-exports the source file
+module.exports = require("/Users/mike/Development/GEL/field-content/src/index.js");
+
+// this unregisters the require hook so that any modules required after this one
+// aren't compiled with the require hook in case you have some other require hook
+// or something that should be used on other modules
+unregister();
