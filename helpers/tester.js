6596f20dc7238bde9668bb667fb9c49d75ca15cb
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-03-16 16:06:09 +1100

    fixed tests
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- helpers/tester.js
+++ helpers/tester.js
@@ -7,26 +7,28 @@ const chalk = require('chalk');
 
 	const longestName = [...workspaces].sort((a, b) => a.name.length < b.name.length)[0].name.length;
 
-	workspaces.map(workspace => {
-		process.stdout.write(
-			`${chalk.bold.yellow(workspace.name.split('/')[1].padEnd(longestName, ' '))}  `
-		);
+	workspaces
+		.filter(pkg => pkg.name !== '@westpac/website')
+		.map(workspace => {
+			process.stdout.write(
+				`${chalk.bold.yellow(workspace.name.split('/')[1].padEnd(longestName, ' '))}  `
+			);
 
-		const command = Spawn.spawnSync('npm', ['test'], {
-			cwd: workspace.dir,
-			stdio: process.env.DEBUG ? [process.stdin, process.stdout, process.stderr] : [],
-			encoding: 'utf-8',
+			const command = Spawn.spawnSync('npm', ['test'], {
+				cwd: workspace.dir,
+				stdio: process.env.DEBUG ? [process.stdin, process.stdout, process.stderr] : [],
+				encoding: 'utf-8',
+			});
+
+			if (command.status) {
+				process.stdout.write(chalk.bgRed.black('🛑  failed\n'));
+				console.error(command.stderr.toString());
+				console.log(command.stdout);
+				process.exit(1);
+			} else {
+				process.stdout.write(chalk.bgGreen.black('✅  success\n'));
+			}
 		});
-
-		if (command.status) {
-			process.stdout.write(chalk.bgRed.black('🛑  failed\n'));
-			console.error(command.stderr.toString());
-			console.log(command.stdout);
-			process.exit(1);
-		} else {
-			process.stdout.write(chalk.bgGreen.black('✅  success\n'));
-		}
-	});
 
 	console.log('Tests done 🎉');
 	process.exit(0);
