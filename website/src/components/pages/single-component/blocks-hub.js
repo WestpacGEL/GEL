374a81fcef9f32d5815a215eda5296c20b011146
maryam-mv <mv.mostajir@gmail.com>
2020-03-18 23:07:17 +1100

    use LAYOUT tokens for media queries

--- website/src/components/pages/single-component/blocks-hub.js
+++ website/src/components/pages/single-component/blocks-hub.js
@@ -48,10 +48,12 @@ const slateRenderer = item =>
 
 		// serialiser for all the blocks
 		({ node, path, serializeChildren, value }) => {
-			const { SPACING } = useBrand();
+			const { SPACING, LAYOUT } = useBrand();
+			console.log(LAYOUT.breakpoints.sm);
+
 			const leftIndent = {
 				paddingLeft: SPACING(11),
-				'@media (max-width: 568px)': {
+				[`@media (max-width: ${LAYOUT.breakpoints.sm}px)`]: {
 					paddingLeft: 0,
 				},
 			};
