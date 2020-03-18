e029c05b75f78ae8aba914ab6fb72f9644a44846
Mike <mike@madebymike.com.au>
2020-03-18 13:03:50 +1000

    Fix top link in footer (#242)

--- website/src/components/layout/footer.js
+++ website/src/components/layout/footer.js
@@ -12,7 +12,27 @@ export const Footer = () => {
 				<FooterIcon icon={SlackIcon} href="//westpac.slack.com" />
 				<FooterIcon icon={GithubIcon} href="//github.com/westpacgel" />
 			</div>
-			<div>Top &uarr;</div>
+
+			<button
+				css={{
+					display: 'block',
+					border: 0,
+					background: 'transparent',
+					cursor: 'pointer',
+					textAlign: 'right',
+				}}
+				onClick={e => {
+					e.preventDefault();
+					const el = document.querySelector('main') || window;
+					el.scroll({
+						top: 0,
+						left: 0,
+						behavior: 'smooth',
+					});
+				}}
+			>
+				Top <span css={{ color: COLORS.primary }}>&uarr;</span>
+			</button>
 		</div>
 	);
 };
