778a6a6c076e9c96aac3173dc71b5e64f8fac16d
maryam-mv <mv.mostajir@gmail.com>
2020-03-24 10:17:27 +1100

    remove added space in blocks hub file

--- website/src/components/pages/single-component/blocks-hub.js
+++ website/src/components/pages/single-component/blocks-hub.js
@@ -30,6 +30,7 @@ const slateRenderer = item =>
 			if (node.object !== 'mark') {
 				return;
 			}
+
 			switch (node.type) {
 				case 'bold':
 					return <Bold key={path}>{serializeChildren(node.nodes)}</Bold>;
