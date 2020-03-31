ca90e119b4181832772ad706ce29814a215efe6a
maryam-mv <mv.mostajir@gmail.com>
2020-03-31 14:23:36 +1100

    remove duplicate import

--- website/src/components/header/component-page-header-image.js
+++ website/src/components/header/component-page-header-image.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 
-import { Fragment } from 'react';
 import { jsx } from '@westpac/core';
 import React, { Fragment } from 'react';
 import WbcBackgroundRightSvg from './symbols/WbcBackgroundRightSvg';
