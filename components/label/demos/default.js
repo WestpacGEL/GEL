0e0220265edf3331b45bdb7882e216af168455df
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-04-01 21:23:18 +1100

    add label demos

---
+++ components/label/demos/default.js
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+
+import { jsx } from '@westpac/core';
+import { Label } from '@westpac/label';
+import { Playground } from '../../../website/src/components/playground/macro';
+
+export default ({ context, showCode, showDemo }) => {
+	return (
+		<Playground context={context} showCode={showCode} showDemo={showDemo}>
+			<p>
+				<Label value="Default" />
+			</p>
+			<p>
+				<Label look="primary" value="Primary" /> <Label look="hero" value="Hero" />{' '}
+				<Label look="neutral" value="Neutral" /> <Label look="faint" value="Faint" />
+			</p>
+			<p>
+				<Label look="success" value="Success" /> <Label look="info" value="Information" />{' '}
+				<Label look="warning" value="Warning" /> <Label look="danger" value="Danger" />
+			</p>
+		</Playground>
+	);
+};
