d225801dcc923c48fc0912a870a289c08ef79465
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-04-02 16:32:51 +1100

    add typography demos

--- components/body/demos/headings.js
+++ components/body/demos/headings.js
@@ -8,8 +8,15 @@ export default ({ context, showCode, showDemo }) => {
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
 			<Body>
-				<h2>Body</h2>
-				<p>Body with default tag</p>
+				<p>
+					<strong>Headings</strong>
+				</p>
+				<h1>Heading 1</h1>
+				<h2>Heading 2</h2>
+				<h3>Heading 3</h3>
+				<h4>Heading 4</h4>
+				<h5>Heading 5</h5>
+				<h6>Heading 6</h6>
 			</Body>
 		</Playground>
 	);
