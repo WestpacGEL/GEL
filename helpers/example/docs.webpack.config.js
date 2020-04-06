93ac1fe7cfca0d32027ff5817efeecb6d2d9efe7
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-04-06 12:40:49 +1000

    show demos in docs

--- helpers/example/docs.webpack.config.js
+++ helpers/example/docs.webpack.config.js
@@ -1,5 +1,11 @@
 const babelLoaderExcludeNodeModulesExcept = require('babel-loader-exclude-node-modules-except');
-const { slugFromFilename, labelFromSlug, findExampleFiles, makeCode } = require('./_utils.js');
+const {
+	slugFromFilename,
+	labelFromSlug,
+	findExampleFiles,
+	findDemoFiles,
+	makeCode,
+} = require('./_utils.js');
 const HtmlWebpackRootPlugin = require('html-webpack-root-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
@@ -26,7 +32,11 @@ const findComponents = () => {
 				filename: component,
 				landing: true,
 			});
-			components = [...components, ...findExampleFiles(component, slug)];
+			components = [
+				...components,
+				...findExampleFiles(component, slug),
+				...findDemoFiles(component, slug),
+			];
 		});
 
 	return components;
