bad340a77096424b0e65a1c6d27d24a1d2fb70da
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-04-14 13:24:49 +1000

    fixed core blender file
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- components/core/blender/recipe.js
+++ components/core/blender/recipe.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react';
 
-import { Core } from '../src/index.js';
+import { GEL } from '../src/index.js';
 
 export function AllStyles({ brand, children }) {
-	return <Core brand={brand}>{children}</Core>;
+	return <GEL brand={brand}>{children}</GEL>;
 }
 
 export function Docs({ brand }) {
 	return [
 		{
 			heading: 'The Core Component',
-			component: () => <Core>Add your GEL components inside the Core component</Core>,
+			component: () => <GEL brand={brand}>Add your GEL components inside the Core component</GEL>,
 		},
 	];
 }
