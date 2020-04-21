9041ec67ab048514f74c088bf5a513ca999b163f
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-04-22 08:24:50 +1000

    added print colors to the swatches
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- website/dynamic-blocks/color-swatch.js
+++ website/dynamic-blocks/color-swatch.js
@@ -42,9 +42,53 @@ const Component = ({ colors }) => {
 // Separator
 export const ColorSwatch = {
 	editor: ({ value, onChange }) => {
-		const { COLORS } = useBrand();
+		const { COLORS, BRAND } = useBrand();
+
+		const sillyColors = {
+			WBC: {
+				Violet: '#9f4585',
+				'Energy Slide Dark': '#b6000b',
+				'Energy Slide Mid': '#c30019',
+				'Energy Slide Light': '#dd3a46',
+			},
+			BOM: {
+				'Dark Purple': '#20024e',
+				'Mid Purple': '#685ac0',
+				'Light Purple': '#a094fc',
+			},
+			STG: {
+				Sky: '#3fc3eb',
+				Plum: '#502d79',
+				Amber: '#ff7f29',
+				'St.George Yellow': '#ffcd00',
+				'Access Green': '#048938',
+			},
+			BTFG: {
+				'BT Blue': '#00afd7',
+				'BT Black': '#2c2a29',
+				'BT Steel': '#80a1b6',
+			},
+			BSA: {
+				Deep: '#00204f',
+				Bight: '#00adbd',
+				Gum: '#5cbb3e',
+				Grape: '#a22269',
+				Sky: '#abe2ec',
+				Outback: '#f7921e',
+			},
+			WBG: {
+				Westpac: '#d5002b',
+				WIB: '#9f0029',
+				BankSA: '#002f6c',
+				'St.George': '#78be20',
+				BTFG: '#7998ac',
+				Rams: '#00adef',
+				'Bank of Melbourne': '#1f252c',
+			},
+		};
+
 		const swatches = [];
-		for (const key in COLORS) {
+		for (const key in { ...COLORS, ...sillyColors[BRAND] }) {
 			if (typeof COLORS[key] === 'string') {
 				swatches.push({ value: key, label: key });
 			}
