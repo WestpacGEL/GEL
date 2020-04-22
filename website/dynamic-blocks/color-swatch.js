c27a32b4679a01f86e361735f0eb5b5fada3343a
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-04-22 10:00:14 +1000

    added config into config
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- website/dynamic-blocks/color-swatch.js
+++ website/dynamic-blocks/color-swatch.js
@@ -1,8 +1,10 @@
 /** @jsx jsx */
 import React, { Fragment } from 'react'; // Needed for within Keystone
 import { jsx, useBrand } from '@westpac/core';
-import chroma from 'chroma-js';
 import Select from '@arch-ui/select';
+import chroma from 'chroma-js';
+
+import { secondaryColors } from '../src/secondary-colors.js';
 
 // Recursively render swatches
 const Swatch = ({ color, name }) => {
@@ -50,54 +52,13 @@ export const ColorSwatch = {
 	editor: ({ value, onChange }) => {
 		const { COLORS, BRAND } = useBrand();
 
-		const sillyColors = {
-			WBC: {
-				Violet: '#9f4585',
-				'Energy Slide Dark': '#b6000b',
-				'Energy Slide Mid': '#c30019',
-				'Energy Slide Light': '#dd3a46',
-			},
-			BOM: {
-				'Dark Purple': '#20024e',
-				'Mid Purple': '#685ac0',
-				'Light Purple': '#a094fc',
-			},
-			STG: {
-				Sky: '#3fc3eb',
-				Plum: '#502d79',
-				Amber: '#ff7f29',
-				'St.George Yellow': '#ffcd00',
-				'Access Green': '#048938',
-			},
-			BTFG: {
-				'BT Blue': '#00afd7',
-				'BT Black': '#2c2a29',
-				'BT Steel': '#80a1b6',
-			},
-			BSA: {
-				Deep: '#00204f',
-				Bight: '#00adbd',
-				Gum: '#5cbb3e',
-				Grape: '#a22269',
-				Sky: '#abe2ec',
-				Outback: '#f7921e',
-			},
-			WBG: {
-				Westpac: '#d5002b',
-				WIB: '#9f0029',
-				BankSA: '#002f6c',
-				'St.George': '#78be20',
-				BTFG: '#7998ac',
-				Rams: '#00adef',
-				'Bank of Melbourne': '#1f252c',
-			},
-		};
-
-		const swatches = Object.entries({ ...COLORS, ...sillyColors[BRAND] }).map(([key, value]) => {
-			if (typeof value === 'string') {
-				return { value: value, label: key.charAt(0).toUpperCase() + key.slice(1) };
+		const swatches = Object.entries({ ...COLORS, ...secondaryColors[BRAND] }).map(
+			([key, value]) => {
+				if (typeof value === 'string') {
+					return { value: value, label: key.charAt(0).toUpperCase() + key.slice(1) };
+				}
 			}
-		});
+		);
 
 		return (
 			<Select
