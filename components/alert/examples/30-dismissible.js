a82e6cacc8fff41406bd3b6c23fd8f8baa822a6b
Thomas Walker <bladey@me.com>
2020-04-23 12:36:56 +1000

    Cypress testing dismissible alerts.

--- components/alert/examples/30-dismissible.js
+++ components/alert/examples/30-dismissible.js
@@ -14,34 +14,33 @@ function Example({ brand }) {
 		<GEL brand={brand}>
 			<Intopia />
 
-			<Alert look="success" dismissible>
+			<Alert look="success" dismissible data-testing="alert0">
 				<strong>Well done!</strong> You successfully read this important alert message. Hey neato, I
 				can be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="info" dismissible>
+			<Alert look="info" dismissible data-testing="alert1">
 				<strong>Heads up!</strong> This alert needs your attention, but it’s not super important.
 				Hey neato, I can be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="warning" dismissible>
+			<Alert look="warning" dismissible data-testing="alert2">
 				<strong>Warning!</strong> Better check yourself, you’re not looking too good. Hey neato, I
 				can be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="danger" dismissible>
+			<Alert look="danger" dismissible data-testing="alert3">
 				<strong>Oh snap!</strong> Change a few things up and try submitting again. Hey neato, I can
 				be closed. <a href="#">Link</a>
 			</Alert>
-			<Alert look="system" dismissible>
+			<Alert look="system" dismissible data-testing="alert4">
 				<strong>System Error 8942:</strong> The server is no responding. Please try again later.
 				Sorry for the inconvenience. Hey neato, I can be closed. <a href="#">Link</a>
 			</Alert>
 
 			<h2>Visibility Controlled via props and onClose</h2>
-			<Button onClick={() => setOpen(!open)}>{open ? 'Hide' : 'Show'}</Button>
+			<Button onClick={() => setOpen(!open)} data-testing="toggle">{open ? 'Hide' : 'Show'}</Button>
 			<br />
 			<br />
-			<Alert open={open} look="success" dismissible onClose={() => setOpen(false)}>
-				<strong>Well done!</strong> You successfully read this important alert message. Hey neato, I
-				can be closed. <a href="#">Link</a>
+			<Alert open={open} look="success" dismissible onClose={() => setOpen(false)} data-testing="alert-state">
+				<strong>Well done!</strong> This alert is controlled by state and has a <a href="#">Link</a>.
 			</Alert>
 		</GEL>
 	);
--- components/alert/tests/integration/alert.cypress.js
+++ components/alert/tests/integration/alert.cypress.js
@@ -1,9 +1,21 @@
-describe('Navigation', () => {
+describe('Alert', () => {
 	before(() => {
 		cy.visit(`http://localhost:8080/`);
+		cy.get('[data-test-nav-link]').contains('dismissible').click();
 	});
 
-	it('Click first item', () => {
-		// cy.get('[data-test-nav]:first-child [data-test-nav-link]').click();
+	it('Alert should be dismissable', () => {
+		['Well done!', 'Heads up!', 'Warning!', 'Oh snap!', 'System Error 8942:'].map((heading, i) => {
+			cy.get(`[data-testing="alert${i}"]`).contains(heading).should('be.visible').get(`[data-testing="alert${i}"] [data-testing="alert-closeBtn"]`).click();
+			cy.wait(500);
+			cy.get(`[data-testing="alert${i}"]`).should('not.be.visible');
+		});
+	});
+
+	it('Alert can be state controlled', () => {
+		cy.get('[data-testing="alert-state"]').should('not.be.visible');
+		cy.get('[data-testing="toggle"]').click();
+		cy.wait(500);
+		cy.get('[data-testing="alert-state"]').should('be.visible');
 	});
 });
