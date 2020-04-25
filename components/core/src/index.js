577618367e2a89b43fdace82ee3728010b1cbe1b
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-04-25 17:52:46 +1000

    added getLabel tests
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- components/core/src/index.js
+++ components/core/src/index.js
@@ -1,5 +1,6 @@
 export { jsx, css, keyframes, Global, ClassNames } from '@emotion/core';
 export { overrideReconciler } from './overrideReconciler';
+export { getLabel, cleanClassName } from './getLabel';
 export { BrandContext, useBrand } from './Brand'; // We need to export the context object for class components
 export { useMediaQuery } from './useMediaQuery';
 export { useInstanceId } from './useInstanceId';
@@ -7,6 +8,5 @@ export { wrapHandlers } from './wrapHandlers';
 export { devWarning } from './devWarning';
 export { mergeWith } from './mergeWith';
 export { useFonts } from './useFonts';
-export { getLabel } from './getLabel';
 export { asArray } from './asArray';
 export { GEL } from './GEL';
