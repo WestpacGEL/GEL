002b76f7bb864d1b458e1fcf9d386febf9624823
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-04-30 21:42:54 +1000

    added some niceties to the schemas
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- website/schema/user.js
+++ website/schema/user.js
@@ -1,10 +1,16 @@
 const { Text, Password } = require('@keystonejs/fields');
 
 const userSchema = {
+	adminDoc: 'This is a list of users who can log into the AdminUI of Keystone and change things',
 	fields: {
 		email: { type: Text },
 		password: { type: Password },
 	},
+	adminConfig: {
+		defaultColumns: '',
+		defaultSort: 'email',
+	},
+	labelField: 'email',
 };
 
 module.exports = { userSchema };
