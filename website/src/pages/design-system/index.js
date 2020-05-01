b51a80053d322e924458eee89f8285e50aabcfd0
maryam-mv <mv.mostajir@gmail.com>
2020-05-01 16:14:12 +1000

    put everything in home page inside grid and clean up ui and spacings

--- website/src/pages/design-system/index.js
+++ website/src/pages/design-system/index.js
@@ -5,6 +5,7 @@ import { Button } from '@westpac/button';
 import { Heading } from '@westpac/heading';
 import { TextInput } from '@westpac/text-input';
 import HomePageHeader from '../../components/header/home-page-header';
+import { Footer } from '../../components/layout/footer';
 import {
 	ReactLogo,
 	AtlassianLogo,
@@ -26,37 +27,39 @@ const Homepage = () => {
 	return (
 		<div css={{ textAlign: 'center', lineHeight: 2 }}>
 			<HomePageHeader />
-			<section css={{ padding: `${SPACING(14)} 0` }}>
+			<section>
 				<Container
 					css={{
 						maxWidth: '60rem',
 						margin: '0 auto',
 					}}
 				>
-					<Heading tag="h2" size={5}>
-						Accessibility is in everything we do
-					</Heading>
-					<p css={{ marginBottom: SPACING(3), marginTop: SPACING(4) }}>
-						Accessibility and inclusive design is a strong part of how we design in the Design
-						Quality team.
-					</p>
+					<Grid columns={12} css={{ padding: `${SPACING(15)} 0` }}>
+						<Cell width={[10, 12, 10, 10]} left={[2, 1, 2, 2]}>
+							<Heading tag="h2" size={5}>
+								Accessibility is in everything we do
+							</Heading>
+							<p css={{ marginBottom: SPACING(3), marginTop: SPACING(4) }}>
+								Accessibility and inclusive design is a strong part of how we design in the Design
+								Quality team.
+							</p>
 
-					<p css={{ margin: `${SPACING(3)} 0` }}>
-						We delve into the detail and pressure test solutions against the latest WCAG
-						requirements, delivering the most accessible components and patterns possible. We then
-						guide teams further along the path of accessibility. Read more about our commitment to
-						accessibility.
-					</p>
-					<a href="" css={{ margin: `${SPACING(3)} 0`, color: COLORS.primary }}>
-						Read more about our commitment to accessibility
-					</a>
+							<p css={{ margin: `${SPACING(3)} 0` }}>
+								We delve into the detail and pressure test solutions against the latest WCAG
+								requirements, delivering the most accessible components and patterns possible. We
+								then guide teams further along the path of accessibility. Read more about our
+								commitment to accessibility.
+							</p>
+							<a href="" css={{ margin: `${SPACING(3)} 0`, color: COLORS.primary }}>
+								Read more about our commitment to accessibility
+							</a>
+						</Cell>
+					</Grid>
 				</Container>
 			</section>
-
 			<section
 				css={{
 					background: COLORS.background,
-					padding: `${SPACING(14)} 0`,
 				}}
 			>
 				<Container
@@ -65,111 +68,125 @@ const Homepage = () => {
 						margin: '0 auto',
 					}}
 				>
-					<ReactLogo size={'100px'} />
-					<Heading tag="h2" size={5} css={{ padding: `${SPACING(3)} 0` }}>
-						Built on React
-					</Heading>
-					<p css={{ margin: `${SPACING(3)} 0` }}>
-						We're moving with the times. With React we can showcase so much more and deliver higher
-						quality, more accessible code.
-					</p>
+					<Grid columns={12} css={{ paddingTop: SPACING(10), paddingBottom: SPACING(15) }}>
+						<Cell width={[10, 12, 10, 10]} left={[2, 1, 2, 2]}>
+							<ReactLogo size={'60px'} />
+							<Heading tag="h2" size={5} css={{ padding: `${SPACING(3)} 0` }}>
+								Built on React
+							</Heading>
+							<p css={{ marginTop: SPACING(1), marginBottom: 0 }}>
+								We're moving with the times. With React we can showcase so much more and deliver
+								higher quality, more accessible code.
+							</p>
 
-					<Heading
-						tag="h3"
-						size={7}
-						css={{
-							borderBottom: `1px solid ${COLORS.text}`,
-							paddingBottom: SPACING(4),
-							paddingTop: SPACING(6),
-						}}
-					>
-						Who else is using React?
-					</Heading>
-
-					<Grid css={{ marginTop: SPACING(4) }}>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<GovLogo size={'113px'} />
+							<Heading
+								tag="h3"
+								size={7}
+								css={{
+									borderBottom: `1px solid ${COLORS.text}`,
+									paddingBottom: SPACING(3),
+									paddingTop: SPACING(7),
+								}}
+							>
+								Who else is using React?
+							</Heading>
 						</Cell>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<MicrosoftLogo size={'127px'} />
-						</Cell>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<MyobLogo size={'90px'} />
-						</Cell>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<IbmLogo size={'77px'} />
-						</Cell>
-					</Grid>
-					<Grid>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<AtlassianLogo size={'159px'} />
-						</Cell>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<FacebookLogo size={'39px'} />
-						</Cell>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<TwitterLogo size={'50px'} />
-						</Cell>
-						<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
-							<ShopifyLogo size={'138px'} />
+						<Cell width={[10, 12, 10, 10]} left={[2, 1, 2, 2]}>
+							<Grid columns={12}>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<GovLogo size={'113px'} />
+								</Cell>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<MicrosoftLogo size={'127px'} />
+								</Cell>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<MyobLogo size={'90px'} />
+								</Cell>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<IbmLogo size={'77px'} />
+								</Cell>
+							</Grid>
+							<Grid css={{ marginTop: SPACING(3) }} columns={12}>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<AtlassianLogo size={'159px'} />
+								</Cell>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<FacebookLogo size={'39px'} />
+								</Cell>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<TwitterLogo size={'50px'} />
+								</Cell>
+								<Cell css={{ paddingTop: SPACING(2) }} width={[12, 3]}>
+									<ShopifyLogo size={'138px'} />
+								</Cell>
+							</Grid>
 						</Cell>
 					</Grid>
 				</Container>
 			</section>
 
-			<section css={{ padding: `${SPACING(14)} 0` }}>
+			<section>
 				<Container
 					css={{
 						maxWidth: '60rem',
 						margin: '0 auto',
 					}}
 				>
-					<Heading tag="h2" size={5} css={{ padding: `${SPACING(3)} 0` }}>
-						Subscribe to our updates
-					</Heading>
-					<p>Get news about components, code chages, new resources, and helpful tips.</p>
-					<form
-						action="//westpac.us11.list-manage.com/subscribe/post?u=d3cf7e940bf311ace99e397b7&amp;id=c78955f1b4"
-						method="post"
-						id="mc-embedded-subscribe-form"
-						name="mc-embedded-subscribe-form"
-						target="_blank"
-						noValidate=""
-					>
-						<Grid
-							columns={12}
-							style={{
-								maxWidth: '30rem',
-								margin: '2rem auto',
-								textAlign: 'start',
-								padding: '0rem 2rem',
-							}}
-						>
-							<Cell width={[12, 8]} left={1}>
-								<label htmlFor="mce-EMAIL">Enter your e-mail address</label>
-								<TextInput type="text" name="EMAIL" id="mce-EMAIL" />
-							</Cell>
-							<Cell width={[12, 4]} style={{ display: 'flex', alignItems: 'flex-end' }}>
-								<Button
-									type="submit"
-									name="subscribe"
-									id="mc-embedded-subscribe"
+					<Grid columns={12} css={{ padding: `${SPACING(15)} 0` }}>
+						<Cell width={[10, 12, 10, 10]} left={[2, 1, 2, 2]}>
+							<Heading tag="h2" size={5}>
+								Subscribe to our updates
+							</Heading>
+							<p css={{ margin: `${SPACING(4)} 0` }}>
+								Get news about components, code chages, new resources, and helpful tips.
+							</p>
+						</Cell>
+						<Cell width={[10, 12, 10, 10]} left={[2, 1, 2, 2]}>
+							<form
+								action="//westpac.us11.list-manage.com/subscribe/post?u=d3cf7e940bf311ace99e397b7&amp;id=c78955f1b4"
+								method="post"
+								id="mc-embedded-subscribe-form"
+								name="mc-embedded-subscribe-form"
+								target="_blank"
+								noValidate=""
+							>
+								<Grid
+									columns={12}
 									style={{
-										width: '100%',
+										maxWidth: '30rem',
+										margin: '0 auto',
+										textAlign: 'start',
 									}}
 								>
-									Subscribe
-								</Button>
-							</Cell>
-						</Grid>
-					</form>
+									<Cell width={[12, 8]} left={1}>
+										<label css={{ marginTop: SPACING(1) }} htmlFor="mce-EMAIL">
+											Enter your e-mail address
+										</label>
+										<TextInput type="text" name="EMAIL" id="mce-EMAIL" />
+									</Cell>
+									<Cell width={[12, 4]} style={{ display: 'flex', alignItems: 'flex-end' }}>
+										<Button
+											type="submit"
+											look="primary"
+											name="subscribe"
+											id="mc-embedded-subscribe"
+											style={{
+												width: '100%',
+											}}
+										>
+											Subscribe
+										</Button>
+									</Cell>
+								</Grid>
+							</form>
+						</Cell>
+					</Grid>
 				</Container>
 			</section>
 
 			<section
 				css={{
 					background: COLORS.background,
-					padding: `${SPACING(12)} 0`,
 				}}
 			>
 				<Container
@@ -178,16 +195,21 @@ const Homepage = () => {
 						margin: '0 auto',
 					}}
 				>
-					<Heading tag="h2" size={5}>
-						Downloads & links
-					</Heading>
-					<p>
-						Get the Sketch UI or Axure kit to start designing. Visit Git to get the latest code and
-						development framework.
-					</p>
-					<DownloadAndLinksSection />
+					<Grid columns={12} css={{ padding: `${SPACING(15)} 0` }}>
+						<Cell width={[10, 12, 10, 10]} left={[2, 1, 2, 2]}>
+							<Heading tag="h2" size={5}>
+								Downloads & links
+							</Heading>
+							<p>
+								Get the Sketch UI or Axure kit to start designing. Visit Git to get the latest code
+								and development framework.
+							</p>
+							<DownloadAndLinksSection />
+						</Cell>
+					</Grid>
 				</Container>
 			</section>
+			<Footer />
 		</div>
 	);
 };
