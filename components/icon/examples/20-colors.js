418971fa5b4ef06f6ebd24794b8ec2a20f23b0ff
Dominik Wilkowski <hi@dominik-wilkowski.com>
2020-05-07 17:14:00 +1000

    Feature/icon (#386)
    * blender support for button-group
    
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
    
    * added token keys as possible color value in Icon
    
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- components/icon/examples/20-colors.js
+++ components/icon/examples/20-colors.js
@@ -9,7 +9,17 @@ import { Intopia } from '../../../helpers/example/components/Intopia.js';
 function Example({ brand }) {
 	return (
 		<GEL brand={brand}>
-			<Intopia />
+			<Intopia ignore />
+
+			<Row>
+				<FavouriteIcon assistiveText="Colored heart" color="primary" />
+				<FavouriteIcon assistiveText="Colored heart" color="hero" />
+				<FavouriteIcon assistiveText="Colored heart" color="neutral" />
+				<FavouriteIcon assistiveText="Colored heart" color="text" />
+				<FavouriteIcon assistiveText="Colored heart" color="system" />
+			</Row>
+
+			<hr />
 
 			<Row>
 				<FavouriteIcon assistiveText="Colored heart" color="#DE350B" />
