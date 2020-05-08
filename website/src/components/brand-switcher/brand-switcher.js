7a58268af7e1ae3c27b6c078506bc0bdf56e11a5
Jonny Stening <info@jonathanstening.com>
2020-05-08 14:46:02 +1000

    bold currently selected brand in brand switcher

--- website/src/components/brand-switcher/brand-switcher.js
+++ website/src/components/brand-switcher/brand-switcher.js
@@ -55,7 +55,7 @@ export const brandsMap = {
 	},
 };
 
-const Option = (props) => {
+const Option = props => {
 	const Logo = brandsMap[props.data.value].smallLogo;
 	return (
 		<components.Option {...props}>
@@ -65,7 +65,7 @@ const Option = (props) => {
 	);
 };
 
-const DropdownIndicator = (props) => {
+const DropdownIndicator = props => {
 	const { COLORS } = useBrand();
 	const {
 		selectProps: { menuIsOpen },
@@ -112,18 +112,18 @@ export const BrandSwitcher = () => {
 						...base,
 						...(isScrolled && !menuIsOpen && { boxShadow: '0 4px 4px rgba(0, 0, 0, 0.3)' }),
 					}),
-					menu: (base) => ({
+					menu: base => ({
 						...base,
 						margin: 0,
 						borderRadius: 0,
 						boxShadow: '0 4px 4px rgba(0, 0, 0, 0.3)',
 					}),
-					menuList: (base) => ({
+					menuList: base => ({
 						...base,
 						maxHeight: '100%',
 						padding: 0,
 					}),
-					option: (base, { isFocused }) => ({
+					option: (base, { value, isFocused }) => ({
 						...base,
 						display: 'flex',
 						alignItems: 'center',
@@ -133,9 +133,10 @@ export const BrandSwitcher = () => {
 						borderBottom: `solid 1px ${COLORS.border}`,
 						cursor: 'pointer',
 						fontSize: '0.875rem',
+						...(brandName === value && TYPE.bodyFont[700]),
 						...(isFocused && { backgroundColor: COLORS.background }),
 					}),
-					control: (base) => ({
+					control: base => ({
 						...base,
 						borderRadius: 0,
 						border: 0,
@@ -147,28 +148,28 @@ export const BrandSwitcher = () => {
 							borderBottom: `solid 1px ${COLORS.border}`,
 						},
 					}),
-					valueContainer: (base) => ({
+					valueContainer: base => ({
 						...base,
 						display: 'flex',
 						alignItems: 'center',
 						paddingLeft: SPACING(3),
 					}),
-					placeholder: (base) => ({
+					placeholder: base => ({
 						...base,
 						...TYPE.bodyFont[400],
 						color: COLORS.text,
 						fontSize: '0.875rem',
 					}),
-					indicatorsContainer: (base) => ({
+					indicatorsContainer: base => ({
 						...base,
 						width: 72,
 					}),
-					indicatorSeparator: (base) => ({
+					indicatorSeparator: base => ({
 						...base,
 						margin: 0,
 						backgroundColor: COLORS.border,
 					}),
-					dropdownIndicator: (base) => ({
+					dropdownIndicator: base => ({
 						...base,
 						margin: 'auto',
 						color: COLORS.primary,
@@ -178,10 +179,10 @@ export const BrandSwitcher = () => {
 						cursor: 'pointer',
 					}),
 				}}
-				onChange={(data) => {
+				onChange={data => {
 					setBrand(data.value);
 				}}
-				options={Object.keys(brandsMap).map((k) => ({ value: k, label: brandsMap[k].label }))}
+				options={Object.keys(brandsMap).map(k => ({ value: k, label: brandsMap[k].label }))}
 			/>
 		</div>
 	);
