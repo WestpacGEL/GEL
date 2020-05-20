be69c2580234e25f5995cd2710d1a454648788dd
Jonny <info@jonathanstening.com>
2020-05-20 11:28:59 +1000

    Refactor grid (#406)
    * fix(grid): rowGap propType declaration
    
    * fix(grid): Cell left prop should be able to take string value (default CSS value is 'auto')
    
    * chore(grid): tidy up
    
    * fix(core): update GEL brand prop propType

--- components/core/src/GEL.js
+++ components/core/src/GEL.js
@@ -19,5 +19,5 @@ export const GEL = ({ brand, noReset, children, ...props }) => {
 
 GEL.propTypes = {
 	noReset: PropTypes.bool,
-	brand: PropTypes.func,
+	brand: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 };
