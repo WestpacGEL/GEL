353d88006f6fd60b07f62e7f2e4156aeb37d0edb
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-05-26 12:29:09 +1000

    update icon and symbol spacing

--- website/dynamic-blocks/icons.js
+++ website/dynamic-blocks/icons.js
@@ -1,17 +1,17 @@
 /** @jsx jsx */
-import React, { useState } from 'react'; // Needed for within Keystone
+import React, { useState, Fragment } from 'react'; // Needed for within Keystone
 import { jsx, useBrand, useMediaQuery } from '@westpac/core';
 import { TextInput } from '@westpac/text-input';
+import { Grid, Cell } from '@westpac/grid';
 import * as icons from '@westpac/icon';
-import { Container, Grid, Cell } from '@westpac/grid';
-import { blocksContainerStyle, blocksGridStyle } from '../src/components/_utils';
 
 const renderIcons = (search) => {
 	const iconDetails = [];
 	for (let key in icons) {
 		iconDetails.push({ name: key, icon: icons[key] });
 	}
-	const { COLORS, SPACING } = useBrand();
+	const { COLORS } = useBrand();
+	const mq = useMediaQuery();
 
 	return iconDetails
 		.filter((icon) =>
@@ -26,22 +26,22 @@ const renderIcons = (search) => {
 				>
 					<div
 						key={icon.name}
-						css={{
+						css={mq({
 							alignItems: 'center',
 							justifyContent: 'center',
 							display: 'flex',
 							flexDirection: 'column',
 							background: '#fff',
-							border: `solid 1px ${COLORS.background}`,
-							padding: `${(SPACING(3), SPACING(2))}`,
-						}}
+							padding: '36px 12px 18px',
+							marginBottom: ['12px', '24px'],
+						})}
 					>
 						<Icon
 							css={{
-								padding: SPACING(6),
+								paddingBottom: '36px',
 							}}
 						/>
-						<span css={{ fontSize: '0.6875rem' }}>{icon.name}</span>
+						<span css={{ fontSize: '0.6875rem', color: COLORS.muted }}>{icon.name}</span>
 					</div>
 				</Cell>
 			);
@@ -54,29 +54,33 @@ const Icon = () => {
 	const mq = useMediaQuery();
 	return (
 		<Fragment>
-			<Cell width={[12, 12, 6]}>
-				<div
-					css={mq({
-						display: 'flex',
-						flexDirection: ['column', null, 'row'],
-						alignItems: ['start', null, 'center'],
-						margin: '1.5rem 0 0.75rem',
-					})}
-				>
-					<label
-						htmlFor={'filter-icons'}
-						value={search}
-						onChange={(e) => setSearch(e.target.value)}
-						css={mq({
-							marginRight: '1rem',
-							marginBottom: ['0.75rem', null, 0],
-							whiteSpace: 'nowrap',
-						})}
-					>
-						Filter by name
-					</label>
-					<TextInput />
-				</div>
+			<Cell width={12}>
+				<Grid>
+					<Cell width={[12, 12, 6]}>
+						<div
+							css={mq({
+								display: 'flex',
+								flexDirection: ['column', null, 'row'],
+								alignItems: ['start', null, 'center'],
+								margin: '1.5rem 0 0.75rem',
+							})}
+						>
+							<label
+								htmlFor={'filter-icons'}
+								value={search}
+								onChange={(e) => setSearch(e.target.value)}
+								css={mq({
+									marginRight: '1rem',
+									marginBottom: ['0.75rem', null, 0],
+									whiteSpace: 'nowrap',
+								})}
+							>
+								Filter by name
+							</label>
+							<TextInput />
+						</div>
+					</Cell>
+				</Grid>
 			</Cell>
 			{renderIcons(search)}
 		</Fragment>
--- website/dynamic-blocks/symbols.js
+++ website/dynamic-blocks/symbols.js
@@ -1,18 +1,17 @@
 /** @jsx jsx */
 
-import React, { useState } from 'react'; // Needed for within Keystone
+import React, { useState, Fragment } from 'react'; // Needed for within Keystone
 import { jsx, useBrand, useMediaQuery } from '@westpac/core';
 import { TextInput } from '@westpac/text-input';
+import { Grid, Cell } from '@westpac/grid';
 import * as symbols from '@westpac/symbol';
-import { Container, Grid, Cell } from '@westpac/grid';
-import { blocksContainerStyle } from '../src/components/_utils';
 
 const renderSymbols = (search) => {
 	const symbolDetails = [];
 	for (let key in symbols) {
 		symbolDetails.push({ name: key, symbol: symbols[key] });
 	}
-	const { COLORS, SPACING } = useBrand();
+	const { COLORS } = useBrand();
 
 	return symbolDetails
 		.filter((symbol) =>
@@ -31,17 +30,17 @@ const renderSymbols = (search) => {
 							display: 'flex',
 							flexDirection: 'column',
 							background: '#fff',
-							border: `solid 1px ${COLORS.background}`,
-							padding: `${(SPACING(3), SPACING(2))}`,
+							padding: '36px 0 18px',
+							marginBottom: ['12px', '24px'],
 						}}
 					>
 						<Symbol
 							css={{
 								flexGrow: 1,
-								padding: SPACING(4),
+								paddingBottom: '36px',
 							}}
 						/>
-						<span css={{ fontSize: '0.6875rem' }}>{symbol.name}</span>
+						<span css={{ fontSize: '0.6875rem', color: COLORS.muted }}>{symbol.name}</span>
 					</div>
 				</Cell>
 			);
@@ -52,9 +51,9 @@ const Symbol = () => {
 	const [search, setSearch] = useState('');
 	const mq = useMediaQuery();
 	return (
-		<div>
-			<Container css={blocksContainerStyle}>
-				<Grid columns={12}>
+		<Fragment>
+			<Cell width={12}>
+				<Grid>
 					<Cell width={[12, 12, 6]}>
 						<div
 							css={mq({
@@ -80,11 +79,9 @@ const Symbol = () => {
 						</div>
 					</Cell>
 				</Grid>
-				<Grid columns={12} css={{ gridAutoRows: '1fr' }}>
-					{renderSymbols(search)}
-				</Grid>
-			</Container>
-		</div>
+			</Cell>
+			{renderSymbols(search)}
+		</Fragment>
 	);
 };
 
