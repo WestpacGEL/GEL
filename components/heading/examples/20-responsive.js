76bc5d724fa7ab60a9fc542fa8d3ae5e02d95ef9
Jonny Stening <info@jonathanstening.com>
2020-05-26 20:54:08 +1000

    update examples

--- components/heading/examples/20-responsive.js
+++ components/heading/examples/20-responsive.js
@@ -13,15 +13,15 @@ function Example({ brand }) {
 			<h2>
 				Without <code>tag</code> prop
 			</h2>
-			<Heading size={[1, 6, 3, 7, 2]}>Heading size: [1, 6, 3, 7, 2]</Heading>
+			<Heading size={[10, 8, 6, 4, 2]}>Heading size: [10, 8, 6, 4, 2]</Heading>
 
 			<hr />
 
 			<h2>
 				With <code>tag</code> prop
 			</h2>
-			<Heading tag="h2" size={[1, 6, 3, 7, 2]}>
-				Heading tag: h2 size: [1, 6, 3, 7, 2]
+			<Heading tag="h2" size={[10, 8, 6, 4, 2]}>
+				Heading tag: h2 size: [10, 8, 6, 4, 2]
 			</Heading>
 		</GEL>
 	);
