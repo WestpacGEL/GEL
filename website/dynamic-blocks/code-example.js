5f56731ca7d5cdd236261e56ea3ddafa6712b583
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-06-01 22:12:22 +1000

    update headings block, remove heading from code block and some clean up

--- website/dynamic-blocks/code-example.js
+++ website/dynamic-blocks/code-example.js
@@ -3,19 +3,14 @@
 import React, { Suspense, Fragment } from 'react';
 
 import { jsx, useMediaQuery } from '@westpac/core';
-import { Heading } from '@westpac/heading';
-import { Body } from '@westpac/body';
 import { Cell } from '@westpac/grid';
 
-import { FieldContainer, FieldLabel, FieldInput } from '@arch-ui/fields';
 import { CheckboxPrimitive } from '@arch-ui/controls';
-import { Input } from '@arch-ui/input';
 import Select from '@arch-ui/select';
 
 import preval from 'preval.macro';
 
 import importCodeExamples from '../utils/babel-dynamic-code-block-import.macro';
-import { exampleLevelOptions, exampleSizeOptions } from './_utils';
 
 let data = preval`
 const fs = require('fs');
@@ -81,6 +76,7 @@ export const CodeExample = {
 			codeExample: null,
 			showCode: false,
 			showDemo: false,
+			// below are depracated
 			level: 'h2',
 			size: 6,
 			heading: '',
@@ -99,91 +95,6 @@ export const CodeExample = {
 
 		return (
 			<Fragment>
-				<FieldContainer>
-					<label css={{ display: 'flex', margin: '10px 20px 0 0' }}>
-						<CheckboxPrimitive
-							checked={currentValue.editHeader}
-							tabIndex="0"
-							onChange={({ target }) => update({ editHeader: target.checked })}
-						/>
-						<span>Edit header</span>
-					</label>
-				</FieldContainer>
-				{currentValue.editHeader && (
-					<Fragment>
-						<FieldContainer>
-							<FieldLabel htmlFor={'heading-text'} field={{ label: 'Heading Text', config: {} }} />
-							<FieldInput>
-								<Input
-									id="heading-text"
-									value={currentValue.heading}
-									onChange={(e) => update({ heading: e.target.value })}
-								/>
-							</FieldInput>
-						</FieldContainer>
-						<div css={{ display: 'flex' }}>
-							<FieldContainer css={{ flexGrow: 1, marginRight: 30 }}>
-								<FieldLabel
-									htmlFor={'heading-level'}
-									field={{ label: 'Heading Level', config: {} }}
-								/>
-								<Select
-									id="heading-level"
-									placeholder="Select a heading level"
-									options={exampleLevelOptions}
-									value={exampleLevelOptions.find((o) => o.value === currentValue.level)}
-									onChange={({ value }) => update({ level: value })}
-								/>
-							</FieldContainer>
-							<FieldContainer css={{ flexGrow: 1 }}>
-								<FieldLabel
-									htmlFor={'heading-size'}
-									field={{ label: 'Heading Size', config: {} }}
-								/>
-								<Select
-									id="heading-size"
-									placeholder="Select a heading size"
-									options={exampleSizeOptions}
-									value={exampleSizeOptions.find((o) => o.value === currentValue.size)}
-									onChange={({ value }) => update({ size: value })}
-								/>
-							</FieldContainer>
-						</div>
-						<div css={{ display: 'flex' }}>
-							<FieldContainer css={{ marginRight: 42 }}>
-								<label css={{ display: 'flex', margin: '10px 20px 0 0' }}>
-									<CheckboxPrimitive
-										checked={currentValue.addSubText}
-										tabIndex="0"
-										onChange={({ target }) => update({ addSubText: target.checked })}
-									/>
-									<span>Add sub text</span>
-								</label>
-							</FieldContainer>
-							<FieldContainer>
-								<label css={{ display: 'flex', margin: '10px 20px 0 0' }}>
-									<CheckboxPrimitive
-										checked={currentValue.addTableContent}
-										tabIndex="0"
-										onChange={({ target }) => update({ addTableContent: target.checked })}
-									/>
-									<span>Include in table of contents</span>
-								</label>
-							</FieldContainer>
-						</div>
-						{currentValue.addSubText && (
-							<FieldContainer>
-								<FieldLabel htmlFor={'heading-subtext'} field={{ label: 'Sub Text', config: {} }} />
-								<Input
-									id={'heading-subtext'}
-									isMultiline
-									value={currentValue.subText}
-									onChange={(e) => update({ subText: e.target.value })}
-								/>
-							</FieldContainer>
-						)}
-					</Fragment>
-				)}
 				<Select
 					isSearchable={true}
 					placeholder="Select a code example"
@@ -224,7 +135,6 @@ export const CodeExample = {
 		}
 
 		const mq = useMediaQuery();
-		const id = heading.replace(/ /g, '-').toLowerCase();
 		const loadCodeBlock = codeExamples[codeExample];
 
 		return (
@@ -237,34 +147,6 @@ export const CodeExample = {
 					':last-child': { marginBottom: 0 },
 				})}
 			>
-				<div css={mq({ marginBottom: ['18px', null, null, null, '24px'] })}>
-					{heading && (
-						<Heading
-							id={id}
-							tabIndex="-1"
-							tag={level}
-							size={[7, null, null, null, size]}
-							{...(addTableContent && { 'data-toc': true })}
-							overrides={{
-								Heading: {
-									styles: (styles) =>
-										mq({
-											...styles,
-											scrollMarginTop: '10.375rem',
-											marginBottom: ['12px', null, null, null, '18px'],
-										}),
-								},
-							}}
-						>
-							{heading}
-						</Heading>
-					)}
-					{subText && (
-						<Body css={{ p: { margin: 0 } }}>
-							<p>{subText}</p>
-						</Body>
-					)}
-				</div>
 				<Suspense fallback={<p>Loading...</p>}>
 					{loadCodeBlock && typeof window !== 'undefined' ? (
 						<ShowCodeBlock
