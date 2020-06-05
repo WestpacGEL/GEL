e3ab22992711c416bef0147711f2917674899335
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-06-05 14:53:32 +1000

    update side-nav behaviour

--- website/src/components/layout/sidebar.js
+++ website/src/components/layout/sidebar.js
@@ -8,41 +8,27 @@ import { BrandSwitcher } from '../brand-switcher';
 import { Navigation } from '.';
 
 export const Sidebar = ({ items }) => {
-	const { COLORS, LAYOUT } = useBrand();
-	const mq = useMediaQuery();
+	const { COLORS } = useBrand();
 	const { isOpen, setIsOpen } = useSidebar();
+	const mq = useMediaQuery();
 
 	return (
 		<Fragment>
 			<aside
-				css={{
+				css={mq({
 					display: 'flex',
 					flexDirection: 'column',
 					background: 'white',
-					gridColumnStart: 1,
-					gridColumnEnd: 2,
+					position: 'relative',
 					borderRight: `1px solid ${COLORS.border}`,
 					width: 300,
 					height: '100vh',
 					boxSizing: 'border-box',
 					position: 'absolute',
-					zIndex: 11,
-					top: 0,
-					bottom: 0,
-					left: 0,
+					zIndex: [11, null, null, null, 'auto'],
 					transition: 'transform 0.15s',
 					transform: isOpen ? 'translateX(0px)' : 'translateX(-300px)',
-
-					// refactor this to use mq above...
-					[`@media only screen and (min-width: ${LAYOUT.breakpoints.lg}px)`]: {
-						position: 'static',
-						zIndex: 'auto',
-						top: 'auto',
-						bottom: 'auto',
-						left: 'auto',
-						transform: 'none',
-					},
-				}}
+				})}
 			>
 				<CloseButton />
 				<BrandSwitcher />
