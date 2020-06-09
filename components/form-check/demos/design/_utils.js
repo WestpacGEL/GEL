2cc8169d2bca7ee106b97db37c6652c66a07287c
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-06-09 20:45:20 +1000

    form-check: update demos

--- components/form-check/demos/design/_utils.js
+++
@@ -1,8 +0,0 @@
-/** @jsx jsx */
-
-import { jsx, useBrand } from '@westpac/core';
-
-export const Title = (props) => {
-	const { COLORS } = useBrand();
-	return <p css={{ fontStyle: 'italic', color: COLORS.muted }} {...props} />;
-};
