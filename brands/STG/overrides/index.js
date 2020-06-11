def3059bddb32d28dd5913b69c163b0a32e1e683
Jonny Stening <info@jonathanstening.com>
2020-06-11 21:04:33 +1000

    and again

--- brands/STG/overrides/index.js
+++ brands/STG/overrides/index.js
@@ -4,7 +4,7 @@ import { formCheck } from './formCheck';
 import { label } from './label';
 import { pagination } from './pagination';
 import { panel } from './panel';
-import { progressBar } from './progress-bar';
+import { progressBar } from './progressBar';
 import { switchStyle } from './switch';
 
 export const OVERRIDES = (TOKENS) => ({
