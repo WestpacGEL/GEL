c068894e66cad9c948011a52ee152650deaa2cf9
Jonny Stening <info@jonathanstening.com>
2020-06-15 21:32:49 +1000

    label: recipe tidy up

--- components/label/blender/recipe.js
+++ components/label/blender/recipe.js
@@ -3,32 +3,27 @@ import React from 'react';
 
 import { Label } from '@westpac/label';
 
+const looks = ['primary', 'hero', 'neutral', 'faint', 'success', 'info', 'warning', 'danger'];
+
 export function AllStyles({ brand }) {
 	return (
 		<GEL brand={brand}>
-			<Label look="primary" value="Text" />
-			<Label look="hero" value="Text" />
-			<Label look="neutral" value="Text" />
-			<Label look="faint" value="Text" />
-			<Label look="success" value="Text" />
-			<Label look="info" value="Text" />
-			<Label look="warning" value="Text" />
-			<Label look="danger" value="Text" />
+			{looks.map((look) => (
+				<Label key={look} look={look} value="Text" />
+			))}
 		</GEL>
 	);
 }
 
 export function Docs({ brand }) {
 	return [
-		...['primary', 'hero', 'neutral', 'faint', 'success', 'info', 'warning', 'danger'].map(
-			(look) => ({
-				heading: `A ${look} label`,
-				component: () => (
-					<GEL brand={brand}>
-						<Label look={look} value={`Your ${look} label text`} />
-					</GEL>
-				),
-			})
-		),
+		...looks.map((look) => ({
+			heading: `${look === 'info' ? 'An' : 'A'} ${look} label`,
+			component: () => (
+				<GEL brand={brand}>
+					<Label look={look} value={`Your ${look} label text`} />
+				</GEL>
+			),
+		})),
 	];
 }
