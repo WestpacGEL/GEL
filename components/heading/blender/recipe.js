c7f4fe15581f15091ea705b9c93127aa5c3920c3
Jonny Stening <info@jonathanstening.com>
2020-06-15 21:27:31 +1000

    heading: recipe tidy up

--- components/heading/blender/recipe.js
+++ components/heading/blender/recipe.js
@@ -3,43 +3,39 @@ import React from 'react';
 
 import { Heading, BrandHeading } from '@westpac/heading';
 
+const headingSizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const brandHeadingSizes = [1, 2, 3, 4, 5, 6, 7];
+
 export function AllStyles({ brand }) {
 	return (
 		<GEL brand={brand}>
-			<Heading size={1}>Your heading</Heading>
-			<Heading size={2}>Your heading</Heading>
-			<Heading size={3}>Your heading</Heading>
-			<Heading size={4}>Your heading</Heading>
-			<Heading size={5}>Your heading</Heading>
-			<Heading size={6}>Your heading</Heading>
-			<Heading size={7}>Your heading</Heading>
-			<Heading size={8}>Your heading</Heading>
-			<Heading size={9}>Your heading</Heading>
-			<Heading size={10}>Your heading</Heading>
+			{headingSizes.map((size) => (
+				<Heading key={size} size={size}>
+					Text
+				</Heading>
+			))}
 
-			<BrandHeading size={1}>Your brand heading</BrandHeading>
-			<BrandHeading size={2}>Your brand heading</BrandHeading>
-			<BrandHeading size={3}>Your brand heading</BrandHeading>
-			<BrandHeading size={4}>Your brand heading</BrandHeading>
-			<BrandHeading size={5}>Your brand heading</BrandHeading>
-			<BrandHeading size={6}>Your brand heading</BrandHeading>
-			<BrandHeading size={7}>Your brand heading</BrandHeading>
+			{brandHeadingSizes.map((size) => (
+				<BrandHeading key={size} size={size}>
+					Text
+				</BrandHeading>
+			))}
 		</GEL>
 	);
 }
 
 export function Docs({ brand }) {
 	return [
-		...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((size) => ({
-			heading: `A size:${size} heading`,
+		...headingSizes.map((size) => ({
+			heading: `A heading - size:${size}`,
 			component: () => (
 				<GEL brand={brand}>
 					<Heading size={size}>Heading size {size}</Heading>
 				</GEL>
 			),
 		})),
-		...[1, 2, 3, 4, 5, 6, 7].map((size) => ({
-			heading: `A size:${size} brand heading`,
+		...brandHeadingSizes.map((size) => ({
+			heading: `A brand heading - size:${size}`,
 			component: () => (
 				<GEL brand={brand}>
 					<BrandHeading size={size}>Brand heading size {size}</BrandHeading>
