4ba78dead50e52ee4c118727a0ad44eb6c24c022
Jonny Stening <info@jonathanstening.com>
2020-06-26 15:05:10 +1000

    docs(grid): add blender grid docs

--- components/grid/blender/recipe.js
+++ components/grid/blender/recipe.js
@@ -35,8 +35,8 @@ export function AllStyles({ brand }) {
 export function Docs({ brand }) {
 	return [
 		{
-			heading: 'Container',
-			subheading: 'A default (fluid width) grid container',
+			heading: 'Containers',
+			subheading: 'Default (fluid width) container',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">Your container content</div>
@@ -44,7 +44,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'A fixed width grid container',
+			subheading: 'Fixed container',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container-fixed">Your fixed container content</div>
@@ -52,7 +52,8 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Grid system example',
+			heading: 'Grid system',
+			subheading: 'Example',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -66,7 +67,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Auto-layout columns - Equal widths',
+			subheading: 'Auto-layout columns - Equal widths',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -84,7 +85,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Auto-layout columns - Setting one column width',
+			subheading: 'Auto-layout columns - Setting one column width',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -103,7 +104,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Auto-layout columns - Variable width content',
+			subheading: 'Auto-layout columns - Variable width content',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -122,7 +123,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - All breakpoints',
+			subheading: 'Responsive classes - All breakpoints',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -141,7 +142,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Stacked to horizontal',
+			subheading: 'Responsive classes - Stacked to horizontal',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -159,7 +160,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Mix and match',
+			subheading: 'Responsive classes - Mix and match',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -186,7 +187,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Row columns example 1',
+			subheading: 'Responsive classes - Row columns example 1',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -201,7 +202,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Row columns example 2',
+			subheading: 'Responsive classes - Row columns example 2',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -216,7 +217,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Row columns example 3',
+			subheading: 'Responsive classes - Row columns example 3',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -231,7 +232,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Row columns example 4',
+			subheading: 'Responsive classes - Row columns example 4',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -246,7 +247,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Row columns example 5',
+			subheading: 'Responsive classes - Row columns example 5',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -261,7 +262,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Responsive classes - Row columns example 6',
+			subheading: 'Responsive classes - Row columns example 6',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -276,7 +277,7 @@ export function Docs({ brand }) {
 			),
 		},
 		{
-			heading: 'Nesting',
+			subheading: 'Nesting',
 			component: () => (
 				<GEL brand={brand}>
 					<div className="container">
@@ -289,6 +290,241 @@ export function Docs({ brand }) {
 								</div>
 							</div>
 						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			heading: 'Columns',
+			subheading: 'Alignment - Vertical alignment example 1',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row align-items-start">
+							<div className="col">One of three columns</div>
+							<div className="col">One of three columns</div>
+							<div className="col">One of three columns</div>
+						</div>
+						<div className="row align-items-center">
+							<div className="col">One of three columns</div>
+							<div className="col">One of three columns</div>
+							<div className="col">One of three columns</div>
+						</div>
+						<div className="row align-items-end">
+							<div className="col">One of three columns</div>
+							<div className="col">One of three columns</div>
+							<div className="col">One of three columns</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Alignment - Vertical alignment example 2',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col align-self-start">One of three columns</div>
+							<div className="col align-self-center">One of three columns</div>
+							<div className="col align-self-end">One of three columns</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Alignment - Horizontal alignment',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row justify-content-start">
+							<div className="col-4">One of two columns</div>
+							<div className="col-4">One of two columns</div>
+						</div>
+						<div className="row justify-content-center">
+							<div className="col-4">One of two columns</div>
+							<div className="col-4">One of two columns</div>
+						</div>
+						<div className="row justify-content-end">
+							<div className="col-4">One of two columns</div>
+							<div className="col-4">One of two columns</div>
+						</div>
+						<div className="row justify-content-around">
+							<div className="col-4">One of two columns</div>
+							<div className="col-4">One of two columns</div>
+						</div>
+						<div className="row justify-content-between">
+							<div className="col-4">One of two columns</div>
+							<div className="col-4">One of two columns</div>
+						</div>
+						<div className="row justify-content-evenly">
+							<div className="col-4">One of two columns</div>
+							<div className="col-4">One of two columns</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Alignment - Column wrapping',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col-9">.col-9</div>
+							<div className="col-4">
+								.col-4
+								<br />
+								Since 9 + 4 = 13 &gt; 12, this 4-column-wide div gets wrapped onto a new line as one
+								contiguous unit.
+							</div>
+							<div className="col-6">
+								.col-6
+								<br />
+								Subsequent columns continue along the new line.
+							</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Alignment - Column breaks example 1',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
+							<div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
+
+							{/* Force next columns to break to new line */}
+							<div className="w-100"></div>
+
+							<div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
+							<div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Alignment - Column breaks example 2',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+							<div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+
+							{/*Force next columns to break to new line at md breakpoint and up */}
+							<div className="w-100 d-none d-md-block"></div>
+
+							<div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+							<div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			heading: 'Reordering',
+			subheading: 'Order classes example 1',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col">First in DOM, no order applied</div>
+							<div className="col order-5">Second in DOM, with a larger order</div>
+							<div className="col order-1">Third in DOM, with an order of 1</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Order classes example 2',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col order-last">First in DOM, ordered last</div>
+							<div className="col">Second in DOM, unordered</div>
+							<div className="col order-first">Third in DOM, ordered first</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Offsetting columns - Offset classes example 1',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col-md-4">.col-md-4</div>
+							<div className="col-md-4 offset-md-4">.col-md-4 .offset-md-4</div>
+						</div>
+						<div className="row">
+							<div className="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+							<div className="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+						</div>
+						<div className="row">
+							<div className="col-md-6 offset-md-3">.col-md-6 .offset-md-3</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Offsetting columns - Offset classes example 2',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col-sm-5 col-md-6">.col-sm-5 .col-md-6</div>
+							<div className="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+								.col-sm-5 .offset-sm-2 .col-md-6 .offset-md-0
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-sm-6 col-md-5 col-lg-6">.col-sm-6 .col-md-5 .col-lg-6</div>
+							<div className="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
+								.col-sm-6 .col-md-5 .offset-md-2 .col-lg-6 .offset-lg-0
+							</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Offsetting columns - Margin utilities',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="container">
+						<div className="row">
+							<div className="col-md-4">.col-md-4</div>
+							<div className="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
+						</div>
+						<div className="row">
+							<div className="col-md-3 ml-md-auto">.col-md-3 .ml-md-auto</div>
+							<div className="col-md-3 ml-md-auto">.col-md-3 .ml-md-auto</div>
+						</div>
+						<div className="row">
+							<div className="col-auto mr-auto">.col-auto .mr-auto</div>
+							<div className="col-auto">.col-auto</div>
+						</div>
+					</div>
+				</GEL>
+			),
+		},
+		{
+			subheading: 'Standalone column classes',
+			component: () => (
+				<GEL brand={brand}>
+					<div className="col-3 bg-light p-3 border">.col-3: width of 25%</div>
+					<div className="col-sm-9 bg-light p-3 border">
+						.col-sm-9: width of 75% above sm breakpoint
 					</div>
 				</GEL>
 			),
