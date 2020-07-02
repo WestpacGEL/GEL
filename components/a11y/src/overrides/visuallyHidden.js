a540ba0addd786d4c28207de0ecfe5366e0623da
Jonny Stening <info@jonathanstening.com>
2020-07-02 16:18:05 +1000

    fix(a11y): ensure skipLink and visuallyHidden styling is !important, refactor skipLink css selector, update skiplink example

--- components/a11y/src/overrides/visuallyHidden.js
+++ components/a11y/src/overrides/visuallyHidden.js
@@ -5,15 +5,15 @@ import { jsx, getLabel } from '@westpac/core';
 const VisuallyHidden = ({ state: { tag: Tag }, ...rest }) => <Tag {...rest} />;
 
 const visuallyHiddenStyles = () => ({
-	label: getLabel('visuallyHidden'),
-	position: 'absolute',
-	width: 1,
-	height: 1,
-	padding: 0,
-	overflow: 'hidden',
-	clip: 'rect(0, 0, 0, 0)',
-	whiteSpace: 'nowrap',
-	border: 0,
+	position: 'absolute !important',
+	width: '1px !important',
+	height: '1px !important',
+	padding: '0 !important',
+	margin: '-1px !important',
+	overflow: 'hidden !important',
+	clip: 'rect(0,0,0,0) !important',
+	whiteSpace: 'nowrap !important',
+	border: '0 !important',
 });
 
 const visuallyHiddenAttributes = () => null;
