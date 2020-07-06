1ac01d72b87b474d59203a9b1972854a460ea4cd
Dominik Wilkowski <hi@dominik-wilkowski.com>
2020-07-06 12:41:37 +1000

    fixed icon state and showed how to override an icon entirely (#467)
    * fixed icon state and showed how to override an icon entirely
    
    * added changeset

--- components/icon/examples/60-overrides.js
+++ components/icon/examples/60-overrides.js
@@ -3,6 +3,7 @@
 import { GEL, jsx } from '@westpac/core';
 import {
 	AddIcon,
+	BankIcon,
 	CalendarIcon,
 	DeleteIcon,
 	GridIcon,
@@ -15,13 +16,26 @@ import { Fragment } from 'react';
 
 import { Intopia } from '../../../helpers/example/components/Intopia.js';
 
-const Wrapper = ({ children, icon, color, size, assistiveText, ...rest }) => (
+const Wrapper = ({ children, state: { icon }, ...rest }) => (
 	<Fragment>
 		<div {...rest}>{children}</div>
 		<div css={{ marginBottom: '1rem' }}>
 			name: <span css={{ fontWeight: 900 }}>{icon}</span>
 		</div>
 	</Fragment>
+);
+
+const Svg = ({ children, state: { icon, assistiveText }, ...rest }) => (
+	<svg
+		aria-label={assistiveText}
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		role="img"
+		focusable="false"
+		{...rest}
+	>
+		{icon === 'BankIcon' ? <circle fill="currentColor" cx="12" cy="12" r="12"></circle> : children}
+	</svg>
 );
 
 function Example({ brand }) {
@@ -33,6 +47,9 @@ function Example({ brand }) {
 				outline: '1px solid red',
 			}),
 			component: Wrapper,
+		},
+		Svg: {
+			component: Svg,
 		},
 	};
 
@@ -49,6 +66,9 @@ function Example({ brand }) {
 			<MessageIcon />
 			<StarIcon />
 			<WriteIcon />
+
+			<h2>Overriding the icon itself</h2>
+			<BankIcon />
 
 			<h2>With overrides and component overrides</h2>
 			<AddIcon
