0eb9c0bb744d9985efdff5fae0950c2cd0faa644
Jonny Stening <info@jonathanstening.com>
2020-07-23 18:11:25 +1000

    export Section in layout/index.js

--- website/src/components/layout/index.js
+++ website/src/components/layout/index.js
@@ -1,6 +1,7 @@
 export { Footer } from './footer';
+export { Gridly } from './gridly';
 export { Layout } from './layout';
 export { Navigation } from './navigation';
 export { Normalize } from './normalize';
+export { Section } from './section';
 export { Sidebar } from './sidebar';
-export { Gridly } from './gridly';
