c110d3736626be4c48573a0eee241b8e605c60c5
Jonny Stening <info@jonathanstening.com>
2020-08-07 15:36:32 +1000

    style(brands): update brand font for WBG

--- brands/WBG/tokens/type.js
+++ brands/WBG/tokens/type.js
@@ -1,6 +1,34 @@
 module.exports = {
 	TYPE: {
-		files: [],
+		files: [
+			{
+				name: '"brandFontWBG"',
+				files: {
+					woff2: 'montserrat-v14-latin-300.woff2',
+					woff: 'montserrat-v14-latin-300.woff',
+				},
+				weight: '300',
+				style: 'normal',
+			},
+			{
+				name: '"brandFontWBG"',
+				files: {
+					woff2: 'montserrat-v14-latin-regular.woff2',
+					woff: 'montserrat-v14-latin-regular.woff',
+				},
+				weight: '400',
+				style: 'normal',
+			},
+			{
+				name: '"brandFontWBG"',
+				files: {
+					woff2: 'montserrat-v14-latin-700.woff2',
+					woff: 'montserrat-v14-latin-700.woff',
+				},
+				weight: '700',
+				style: 'normal',
+			},
+		],
 		bodyFont: {
 			weights: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 			headingWeight: 700,
@@ -8,9 +36,9 @@ module.exports = {
 				'-apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Arial, sans-serif',
 		},
 		brandFont: {
-			weights: ['400', '400', '400', '400', '400', '400', '700', '700', '700'],
+			weights: ['300', '300', '300', '400', '400', '400', '700', '700', '700'],
 			headingWeight: 400,
-			fontFamily: '"Times New Roman", "Times", serif',
+			fontFamily: '"brandFontWBG"',
 		},
 	},
 };
