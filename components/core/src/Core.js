5dd514237617555ff2a1cb0d631159f107fe6837
Jonny Stening <info@jonathanstening.com>
2020-08-17 09:36:45 +1000

    rearchitect core specificity solution, prepend all classNames with .GEL class giving components specificity equal to Core resets

--- components/core/src/Core.js
+++ components/core/src/Core.js
@@ -1,17 +1,46 @@
 /** @jsx jsx */
 
-import { jsx } from '@emotion/core';
+import { jsx, CacheProvider } from '@emotion/core';
+import { useState } from 'react';
+import createCache from '@emotion/cache';
 
 import { useBrand } from './Brand';
 import { reset } from './reset';
+
+const wrapperClass = 'GEL';
+const emotionLabel = 'Core';
+
+const AddRootClass = ({ children }) => {
+	let [cache] = useState(() => {
+		return createCache({
+			stylisPlugins: [
+				(context, content, selectors, parent, line, column, length) => {
+					if (context === -1 && selectors.length && !selectors[0].includes(emotionLabel)) {
+						selectors[0] = `.${wrapperClass} ${selectors[0]}`;
+					}
+					return content;
+				},
+			],
+		});
+	});
+	return <CacheProvider value={cache}>{children}</CacheProvider>;
+};
+
+/* 
+Note:
+This works provided that there is only one GEL wrapper on the page. Our example docs have two... one at App level, one at example level. Override examples needs access to the brand to override it, hence why I'm having issues rearchitecting this. 
+
+Also note that we have a reset (used by `<Core />`) and website layout.js additinally uses a `<Normalize />`. I've commented out these for now. Need to understand why Normalize is needed in addition to our reset in Core.
+*/
 
 export const Core = ({ noReset, children }) => {
 	const { COLORS, TYPE, PACKS } = useBrand();
 
 	return (
 		<div
+			className={wrapperClass}
 			css={{
-				label: 'Core',
+				label: emotionLabel,
 				lineHeight: 1.428571429,
 				color: COLORS.text,
 				fontFeatureSettings: '"liga" 1', // Enable OpenType ligatures in IE
@@ -27,11 +56,11 @@ export const Core = ({ noReset, children }) => {
 					backgroundColor: COLORS.tints.primary20,
 				},
 				'&': {
-					...(noReset ? null : reset),
+					...(!noReset && reset),
 				},
 			}}
 		>
-			{children}
+			<AddRootClass>{children}</AddRootClass>
 		</div>
 	);
 };
