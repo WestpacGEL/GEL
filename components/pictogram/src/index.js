65b0aed493496333c83192c695129a7e98f5bd07
Jonny Stening <info@jonathanstening.com>
2020-08-19 21:41:52 +1000

    feat(pictogram): add pictogram package

---
+++ components/pictogram/src/index.js
@@ -0,0 +1,3 @@
+export { ChatPictogram } from './pictograms/ChatPictogram';
+export { PayPictogram } from './pictograms/PayPictogram';
+export { SecurePictogram } from './pictograms/SecurePictogram';
