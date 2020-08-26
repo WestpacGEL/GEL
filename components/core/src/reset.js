f4af7bc8fe661682ef519bc92c654d1ffcc73ba2
Jonny Stening <info@jonathanstening.com>
2020-08-26 13:03:07 +1000

    style(core): tidy up

--- components/core/src/reset.js
+++ components/core/src/reset.js
@@ -84,23 +84,23 @@ export const reset = css`
 	select {
 		text-transform: none;
 	}
+	button,
 	[type='button'],
 	[type='reset'],
-	[type='submit'],
-	button {
+	[type='submit'] {
 		-webkit-appearance: button;
 	}
+	button::-moz-focus-inner,
 	[type='button']::-moz-focus-inner,
 	[type='reset']::-moz-focus-inner,
-	[type='submit']::-moz-focus-inner,
-	button::-moz-focus-inner {
+	[type='submit']::-moz-focus-inner {
 		border-style: none;
 		padding: 0;
 	}
+	button:-moz-focusring,
 	[type='button']:-moz-focusring,
 	[type='reset']:-moz-focusring,
-	[type='submit']:-moz-focusring,
-	button:-moz-focusring {
+	[type='submit']:-moz-focusring {
 		outline: 1px dotted ButtonText;
 	}
 	fieldset {
