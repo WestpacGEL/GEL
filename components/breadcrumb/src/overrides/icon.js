7b8944d1f372cce74715eda493a71c0408f13819
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-08-26 17:12:58 +1000

    breadcrumb: blender re-architecture

--- components/breadcrumb/src/overrides/icon.js
+++ components/breadcrumb/src/overrides/icon.js
@@ -3,11 +3,34 @@
 import { jsx, useBrand, getLabel } from '@westpac/core';
 import { ArrowRightIcon } from '@westpac/icon';
 
+// ==============================
+// Component
+// ==============================
+
 const Icon = ({ state, ...rest }) => {
 	const { COLORS } = useBrand();
 
 	return <ArrowRightIcon size="small" color={COLORS.primary} assistiveText={null} {...rest} />;
 };
+
+const BlenderIcon = (props) => (
+	<Icon
+		overrides={{
+			Icon: {
+				styles: (styles) => {
+					const blenderStyles = { ...styles };
+					delete blenderStyles.label;
+					return blenderStyles;
+				},
+			},
+		}}
+		{...props}
+	/>
+);
+
+// ==============================
+// Styles
+// ==============================
 
 const iconStyles = () => ({
 	label: getLabel('breadcrumb-icon'),
@@ -16,12 +39,26 @@ const iconStyles = () => ({
 	verticalAlign: 'middle',
 });
 
+// ==============================
+// Attributes
+// ==============================
+
 const iconAttributes = () => ({
 	'aria-hidden': 'true',
 });
 
+// ==============================
+// Exports
+// ==============================
+
 export const defaultIcon = {
 	component: Icon,
+	styles: iconStyles,
+	attributes: iconAttributes,
+};
+
+export const blenderIcon = {
+	component: BlenderIcon,
 	styles: iconStyles,
 	attributes: iconAttributes,
 };
