b176c11db97c08a3d744268ee95f70d130e29876
Jonny Stening <info@jonathanstening.com>
2020-08-28 12:19:21 +1000

    nav group hide/show transition

--- website/src/components/layout/navigation/navigation-group.js
+++ website/src/components/layout/navigation/navigation-group.js
@@ -3,13 +3,22 @@
 import { jsx, useBrand, useInstanceId } from '@westpac/core';
 import { AddIcon, RemoveIcon } from '@westpac/icon';
 import { useState, useEffect } from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
+import { useSpring, animated } from 'react-spring';
+import useMeasure from 'react-use-measure';
 import { NavigationItem, StyledItem } from './navigation-item';
 
 export const NavigationGroup = ({ title, isBlockOpen, level, children }) => {
 	const { COLORS, PACKS } = useBrand();
 	const [isOpen, setIsOpen] = useState(isBlockOpen);
+	const [measureRef, { height }] = useMeasure({ polyfill: ResizeObserver });
+	const [initial, setInitial] = useState(true);
 	const [instanceId, setInstanceId] = useState();
-	const toggleBlock = () => {
+
+	const handleButtonClick = () => {
+		if (initial) {
+			setInitial(false);
+		}
 		setIsOpen((currentState) => !currentState);
 	};
 
@@ -17,12 +26,20 @@ export const NavigationGroup = ({ title, isBlockOpen, level, children }) => {
 		setInstanceId(`nav-group-${useInstanceId()}`);
 	}, []);
 
+	const animate = useSpring({
+		to: {
+			height: !isOpen ? 0 : height,
+			overflow: 'hidden',
+		},
+		immediate: initial,
+	});
+
 	return (
 		<NavigationItem>
 			<StyledItem
 				tag="button"
 				level={level}
-				onClick={toggleBlock}
+				onClick={handleButtonClick}
 				aria-expanded={isOpen}
 				aria-controls={instanceId}
 				css={{
@@ -44,9 +61,9 @@ export const NavigationGroup = ({ title, isBlockOpen, level, children }) => {
 					<AddIcon size="small" color={COLORS.muted} />
 				)}
 			</StyledItem>
-			<div id={instanceId} css={{ display: !isOpen && 'none' }} aria-hidden={!isOpen}>
-				{children}
-			</div>
+			<animated.div style={animate} id={instanceId} aria-hidden={!isOpen}>
+				<div ref={measureRef}>{children}</div>
+			</animated.div>
 		</NavigationItem>
 	);
 };
