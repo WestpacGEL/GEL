ff59a11f70826aaa3e61bcba230c52d844fc3459
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-09-04 16:53:48 +1000

    button-dropdown: blender re-architecture

--- components/button-dropdown/blender/recipe.js
+++ components/button-dropdown/blender/recipe.js
@@ -3,10 +3,32 @@ import React from 'react';
 
 import { ButtonDropdown, Heading } from '@westpac/button-dropdown';
 import { AndroidIcon, AppleIcon } from '@westpac/icon';
+import { blenderButton } from '@westpac/button';
+
+import { blenderButtonDropdown } from '../src/overrides/buttonDropdown';
+import { blenderHeading } from '../src/overrides/heading';
+import { blenderPanel } from '../src/overrides/panel';
 
 export function AllStyles({ brand }) {
+	const overridesWithTokens = { ...brand };
+	overridesWithTokens['@westpac/button-dropdown'] = {
+		ButtonDropdown: {
+			styles: blenderButtonDropdown.styles,
+		},
+		Heading: {
+			component: blenderHeading.component,
+		},
+		Panel: {
+			styles: blenderPanel.styles,
+		},
+	};
+	overridesWithTokens['@westpac/button'] = {
+		Button: {
+			styles: blenderButton.styles,
+		},
+	};
 	return (
-		<GEL brand={brand}>
+		<GEL brand={overridesWithTokens}>
 			<ButtonDropdown text="Text">Text</ButtonDropdown>
 			<ButtonDropdown text="Text">
 				<Heading>Text</Heading>
@@ -30,28 +52,19 @@ export function AllStyles({ brand }) {
 			<ButtonDropdown text="Text" look="faint" soft>
 				Text
 			</ButtonDropdown>
-			<ButtonDropdown text="Text" look="primary" block>
+			{/* <ButtonDropdown text="Text" block>
+				Text
+			</ButtonDropdown> */}
+			{/* <ButtonDropdown text="Text" size="small">
 				Text
 			</ButtonDropdown>
-			<ButtonDropdown text="Text" look="hero" block>
+			<ButtonDropdown text="Text" size="large">
 				Text
 			</ButtonDropdown>
-			<ButtonDropdown text="Text" look="faint" block>
+			<ButtonDropdown text="Text" size="xlarge">
 				Text
-			</ButtonDropdown>
-			<ButtonDropdown text="Text" look="primary" size="small">
-				Text
-			</ButtonDropdown>
-			<ButtonDropdown text="Text" look="primary" size="medium">
-				Text
-			</ButtonDropdown>
-			<ButtonDropdown text="Text" look="primary" size="large">
-				Text
-			</ButtonDropdown>
-			<ButtonDropdown text="Text" look="primary" size="xlarge">
-				Text
-			</ButtonDropdown>
-			<ButtonDropdown text="Text" dropdownSize="small">
+			</ButtonDropdown> */}
+			{/* <ButtonDropdown text="Text" dropdownSize="small">
 				Text
 			</ButtonDropdown>
 			<ButtonDropdown text="Text" dropdownSize="medium">
@@ -59,32 +72,40 @@ export function AllStyles({ brand }) {
 			</ButtonDropdown>
 			<ButtonDropdown text="Text" dropdownSize="large">
 				Text
-			</ButtonDropdown>
+			</ButtonDropdown> */}
 			<ButtonDropdown text="Text" open>
 				Text
 			</ButtonDropdown>
-			<ButtonDropdown text="Text" dropdownSize="small" open>
+			{/* <ButtonDropdown iconBefore={AndroidIcon} iconAfter={AppleIcon} text="Text">
 				Text
-			</ButtonDropdown>
-			<ButtonDropdown text="Text" dropdownSize="medium" open>
-				Text
-			</ButtonDropdown>
-			<ButtonDropdown text="Text" dropdownSize="large" open>
-				Text
-			</ButtonDropdown>
-			<ButtonDropdown iconBefore={AndroidIcon} iconAfter={AppleIcon} text="Text">
-				Text
-			</ButtonDropdown>
+			</ButtonDropdown> */}
 		</GEL>
 	);
 }
 
 export function Docs({ brand }) {
+	const overridesWithTokens = { ...brand };
+	overridesWithTokens['@westpac/button-dropdown'] = {
+		ButtonDropdown: {
+			attributes: blenderButtonDropdown.attributes,
+		},
+		Heading: {
+			component: blenderHeading.component,
+		},
+		Panel: {
+			attributes: blenderPanel.attributes,
+		},
+	};
+	overridesWithTokens['@westpac/button'] = {
+		Button: {
+			attributes: blenderButton.attributes,
+		},
+	};
 	return [
 		{
 			heading: `A button-dropdown`,
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<ButtonDropdown text="Your text">
 						<p>Your button-dropdown</p>
 					</ButtonDropdown>
@@ -94,7 +115,7 @@ export function Docs({ brand }) {
 		{
 			heading: `A button-dropdown with headings`,
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<ButtonDropdown text="Your text">
 						<Heading>First heading</Heading>
 						<p>Example dropdown content...</p>
@@ -113,7 +134,7 @@ export function Docs({ brand }) {
 		...['primary', 'hero', 'faint'].map((look) => ({
 			heading: `A ${look} button-dropdown`,
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<ButtonDropdown text="Your text" look={look}>
 						<p>Your {look} button-dropdown</p>
 					</ButtonDropdown>
@@ -123,37 +144,37 @@ export function Docs({ brand }) {
 		...['primary', 'hero', 'faint'].map((look) => ({
 			heading: `A ${look} soft button-dropdown`,
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<ButtonDropdown text="Your text" look={look} soft>
 						<p>Your {look} soft button-dropdown</p>
 					</ButtonDropdown>
 				</GEL>
 			),
 		})),
-		...['primary', 'hero', 'faint'].map((look) => ({
-			heading: `A ${look} block button-dropdown`,
-			component: () => (
-				<GEL brand={brand}>
-					<ButtonDropdown text="Your text" look={look} block>
-						<p>Your {look} block button-dropdown</p>
-					</ButtonDropdown>
-				</GEL>
-			),
-		})),
-		...['small', 'medium', 'large', 'xlarge'].map((size) => ({
-			heading: `A primary ${size} button-dropdown`,
-			component: () => (
-				<GEL brand={brand}>
-					<ButtonDropdown text="Your text" look="primary" size={size}>
-						<p>Your primary {size} button-dropdown</p>
-					</ButtonDropdown>
-				</GEL>
-			),
-		})),
+		// ...['primary', 'hero', 'faint'].map((look) => ({
+		// 	heading: `A ${look} block button-dropdown`,
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<ButtonDropdown text="Your text" look={look} block>
+		// 				<p>Your {look} block button-dropdown</p>
+		// 			</ButtonDropdown>
+		// 		</GEL>
+		// 	),
+		// })),
+		// ...['small', 'medium', 'large', 'xlarge'].map((size) => ({
+		// 	heading: `A primary ${size} button-dropdown`,
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<ButtonDropdown text="Your text" look="primary" size={size}>
+		// 				<p>Your primary {size} button-dropdown</p>
+		// 			</ButtonDropdown>
+		// 		</GEL>
+		// 	),
+		// })),
 		...['small', 'medium', 'large'].map((size) => ({
 			heading: `A button-dropdown with a ${size} dropdown`,
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<ButtonDropdown text="Your text" dropdownSize={size}>
 						<p>Your button-dropdown with a {size} dropdown?</p>
 					</ButtonDropdown>
--- components/button-dropdown/src/overrides/buttonDropdown.js
+++ components/button-dropdown/src/overrides/buttonDropdown.js
@@ -1,20 +1,75 @@
 /** @jsx jsx */
 
-import { jsx, getLabel } from '@westpac/core';
+import { jsx, getLabel, getModifier, styleReconciler, classNames } from '@westpac/core';
+import { defaultProps } from '../ButtonDropdown';
+
+// ==============================
+// Component
+// ==============================
 
 const ButtonDropdown = ({ state, ...rest }) => <div {...rest} />;
 
+// ==============================
+// Styles
+// ==============================
+
 const buttonDropdownStyles = (_, { block }) => ({
-	label: getLabel('buttonDropdown', { block }),
+	label: getLabel('buttonDropdown'),
 	position: 'relative',
 	display: block ? 'block' : 'inline-block',
 	verticalAlign: 'middle',
 });
 
+// ==============================
+// Blender Styles
+// ==============================
+
+const blenderStyles = (_, { block }) => {
+	const props = { block };
+	const baseStyles = buttonDropdownStyles(_, defaultProps);
+
+	let modifiers = getModifier(defaultProps, props);
+	if (!modifiers.length) return baseStyles;
+
+	const modifierStyles = buttonDropdownStyles(_, props);
+	const reconciledStyles = styleReconciler(baseStyles, modifierStyles);
+
+	let label = baseStyles.label;
+	const modifier = modifiers[0];
+
+	switch (modifier) {
+		default:
+			label = `${label}-${modifier}`;
+			break;
+	}
+
+	return { label, ...reconciledStyles };
+};
+
+// ==============================
+// Attributes
+// ==============================
+
 const buttonDropdownAttributes = () => null;
+
+const blenderAttributes = (_, { block }) => ({
+	className: classNames({
+		[`__convert__buttonDropdown-block`]: block,
+	}),
+});
+
+// ==============================
+// Exports
+// ==============================
 
 export const defaultButtonDropdown = {
 	component: ButtonDropdown,
 	styles: buttonDropdownStyles,
 	attributes: buttonDropdownAttributes,
+};
+
+export const blenderButtonDropdown = {
+	component: ButtonDropdown,
+	styles: blenderStyles,
+	attributes: blenderAttributes,
 };
