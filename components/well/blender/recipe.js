0ed5af662501385a6de310139d0ad47210c7fe06
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-09-08 11:29:58 +1000

    well: add blender support

---
+++ components/well/blender/recipe.js
@@ -0,0 +1,50 @@
+import { GEL } from '@westpac/core';
+import React from 'react';
+
+import { Well } from '@westpac/well';
+import { blenderWell } from '../src/overrides/well';
+
+export function AllStyles({ brand }) {
+	const overridesWithTokens = { ...brand };
+	overridesWithTokens['@westpac/well'] = {
+		Well: {
+			component: blenderWell.component,
+		},
+	};
+
+	return (
+		<GEL brand={overridesWithTokens}>
+			<Well>Text</Well>
+		</GEL>
+	);
+}
+
+export function Docs({ brand }) {
+	const overridesWithTokens = { ...brand };
+	overridesWithTokens['@westpac/well'] = {
+		Well: {
+			component: blenderWell.component,
+		},
+	};
+
+	return [
+		{
+			heading: 'A well',
+			component: () => (
+				<GEL brand={overridesWithTokens}>
+					<Well>Text</Well>
+				</GEL>
+			),
+		},
+		{
+			heading: "Nested well's",
+			component: () => (
+				<GEL brand={overridesWithTokens}>
+					<Well>
+						Text<Well>Nested</Well>
+					</Well>
+				</GEL>
+			),
+		},
+	];
+}
