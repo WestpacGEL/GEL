62673200a021776a8c28f4f674f7ba6f1b6b835a
Jeremy Ortiz <jeremy@jeremyortiz.io>
2020-09-10 17:17:27 +1000

    input-group: intial blender re-architecture

--- components/input-group/blender/recipe.js
+++ components/input-group/blender/recipe.js
@@ -1,232 +1,106 @@
 import { GEL } from '@westpac/core';
 import React, { Fragment } from 'react';
-
 import { InputGroup, Before, After } from '@westpac/input-group';
+import { blenderTextInput, blenderSelect } from '@westpac/text-input';
+
+import { blenderText } from '../src/overrides/text';
+import { blenderTextInput as blenderGroupTextInput } from '../src/overrides/textInput';
+import { blenderButton as blenderGroupButton } from '../src/overrides/button';
+import { blenderSelect as blenderGroupSelect } from '../src/overrides/select';
 
 const sizes = ['small', 'medium', 'large', 'xlarge'];
 
 export function AllStyles({ brand }) {
+	const overridesWithTokens = { ...brand };
+	overridesWithTokens['@westpac/input-group'] = {
+		Text: {
+			styles: blenderText.styles,
+		},
+		TextInput: {
+			component: ({ invalid, ...props }) => <input {...props} />,
+			styles: blenderGroupTextInput.styles,
+		},
+		// Button: {
+		// 	component: (props) => <button {...props} />,
+		// 	styles: blenderGroupButton.styles,
+		// },
+		Select: {
+			component: (props) => <select {...props} />,
+			styles: blenderGroupSelect.styles,
+		},
+	};
+
 	return (
-		<GEL brand={brand}>
-			{sizes.map((size, i) => (
-				<Fragment key={i}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							after: { inputType: 'text', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'text', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'text', data: 'TEXT' },
-							after: { inputType: 'text', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="hero"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="hero"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="hero"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="primary"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="primary"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="primary"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="faint"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="faint"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						look="faint"
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: { inputType: 'button', data: 'TEXT' },
-							after: { inputType: 'button', data: 'TEXT' },
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							after: {
-								inputType: 'select',
-								label: 'TEXT',
-								data: [
-									{ text: 'TEXT', value: '' },
-									{ text: 'TEXT', value: '' },
-								],
-							},
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: {
-								inputType: 'select',
-								label: 'TEXT',
-								data: [
-									{ text: 'TEXT', value: '' },
-									{ text: 'TEXT', value: '' },
-								],
-							},
-						}}
-					/>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						size={size}
-						name="TEXT"
-						label="TEXT"
-						data={{
-							before: {
-								inputType: 'select',
-								label: 'TEXT',
-								data: [
-									{ text: 'TEXT', value: '' },
-									{ text: 'TEXT', value: '' },
-								],
-							},
-							after: {
-								inputType: 'select',
-								label: 'TEXT',
-								data: [
-									{ text: 'TEXT', value: '' },
-									{ text: 'TEXT', value: '' },
-								],
-							},
-						}}
-					/>
-				</Fragment>
+		<GEL brand={overridesWithTokens}>
+			{/* This is a special case so we can genereate a base style for the text component */}
+			<InputGroup name="TEXT" label="TEXT">
+				<Before inputType="text" data="TEXT" position={'none'} />
+			</InputGroup>
+			<InputGroup name="TEXT" label="TEXT">
+				<Before inputType="text" data="TEXT" />
+			</InputGroup>
+			<InputGroup name="TEXT" label="TEXT">
+				<After inputType="text" data="TEXT" />
+			</InputGroup>
+			<InputGroup name="TEXT" label="TEXT">
+				<Before inputType="text" data="TEXT" />
+				<After inputType="text" data="TEXT" />
+			</InputGroup>
+			{sizes.map((size) => (
+				<InputGroup name="TEXT" label="TEXT" size={size} key={size}>
+					<After inputType="text" data="TEXT" />
+				</InputGroup>
 			))}
+			<InputGroup name="TEXT" label="TEXT">
+				<Before
+					inputType="select"
+					name="TEXT"
+					label="TEXT"
+					data={[
+						{ text: 'TEXT', value: '' },
+						{ text: 'TEXT', value: '' },
+					]}
+				/>
+				<After
+					inputType="select"
+					name="TEXT"
+					label="TEXT"
+					data={[
+						{ text: 'TEXT', value: '' },
+						{ text: 'TEXT', value: '' },
+					]}
+				/>
+			</InputGroup>
 		</GEL>
 	);
 }
 
 export function Docs({ brand }) {
+	const overridesWithTokens = { ...brand };
+	overridesWithTokens['@westpac/input-group'] = {
+		Text: {
+			attributes: blenderText.attributes,
+		},
+		TextInput: {
+			component: blenderGroupTextInput.component,
+		},
+		Select: {
+			component: blenderGroupSelect.component,
+		},
+	};
+	overridesWithTokens['@westpac/text-input'] = {
+		TextInput: {
+			attributes: blenderTextInput.attributes,
+		},
+		Select: {
+			attributes: blenderSelect.attributes,
+		},
+	};
 	return [
 		{
 			heading: 'Input group with text left',
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<InputGroup
 						instanceIdPrefix="GEL"
 						name="inputgroup-left-text"
@@ -238,25 +112,25 @@ export function Docs({ brand }) {
 				</GEL>
 			),
 		},
-		{
-			heading: 'Input group with button left',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						name="inputgroup-left-button"
-						label="inputgroup-left-button"
-						data={{
-							before: { inputType: 'button', data: 'Search' },
-						}}
-					/>
-				</GEL>
-			),
-		},
+		// {
+		// 	heading: 'Input group with button left',
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				name="inputgroup-left-button"
+		// 				label="inputgroup-left-button"
+		// 				data={{
+		// 					before: { inputType: 'button', data: 'Search' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
 		{
 			heading: 'Input group with select left',
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<InputGroup
 						instanceIdPrefix="GEL"
 						name="inputgroup-left-select"
@@ -280,7 +154,7 @@ export function Docs({ brand }) {
 		{
 			heading: 'Input group with text right',
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<InputGroup
 						instanceIdPrefix="GEL"
 						name="inputgroup-right-text"
@@ -292,25 +166,25 @@ export function Docs({ brand }) {
 				</GEL>
 			),
 		},
-		{
-			heading: 'Input group with button right',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						name="inputgroup-right-button"
-						label="inputgroup-right-button"
-						data={{
-							after: { inputType: 'button', data: 'Submit' },
-						}}
-					/>
-				</GEL>
-			),
-		},
+		// {
+		// 	heading: 'Input group with button right',
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				name="inputgroup-right-button"
+		// 				label="inputgroup-right-button"
+		// 				data={{
+		// 					after: { inputType: 'button', data: 'Submit' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
 		{
 			heading: 'Input group with select right',
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<InputGroup
 						instanceIdPrefix="GEL"
 						name="inputgroup-right-select"
@@ -330,106 +204,106 @@ export function Docs({ brand }) {
 				</GEL>
 			),
 		},
-		{
-			heading: 'Input group with text left and button right',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						name="inputgroup-left-text-right-button"
-						label="inputgroup-left-text-right-button"
-						data={{
-							before: { inputType: 'text', data: 'AUS $' },
-							after: { inputType: 'button', data: 'Submit' },
-						}}
-					/>
-				</GEL>
-			),
-		},
-		{
-			heading: 'Input group with text left and text right',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						name="inputgroup-left-text-right-text"
-						label="inputgroup-left-text-right-text"
-						data={{
-							before: { inputType: 'text', data: 'AUS $' },
-							after: { inputType: 'text', data: '.00' },
-						}}
-					/>
-				</GEL>
-			),
-		},
-		{
-			heading: 'Input group with button left and button right',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						name="inputgroup-left-button-right-button"
-						label="inputgroup-left-button-right-button"
-						data={{
-							before: { inputType: 'button', data: '-' },
-							after: { inputType: 'button', data: '+' },
-						}}
-					/>
-				</GEL>
-			),
-		},
-		{
-			heading: 'Input group with hero button',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						look="hero"
-						name="inputgroup-button-hero"
-						label="inputgroup-button-hero"
-						data={{
-							after: { inputType: 'button', data: 'Submit' },
-						}}
-					/>
-				</GEL>
-			),
-		},
-		{
-			heading: 'Input group with primary button',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						look="primary"
-						name="inputgroup-button-primary"
-						label="inputgroup-button-primary"
-						data={{
-							after: { inputType: 'button', data: 'Submit' },
-						}}
-					/>
-				</GEL>
-			),
-		},
-		{
-			heading: 'Input group with faint button',
-			component: () => (
-				<GEL brand={brand}>
-					<InputGroup
-						instanceIdPrefix="GEL"
-						look="faint"
-						name="inputgroup-button-faint"
-						label="inputgroup-button-faint"
-						data={{
-							after: { inputType: 'button', data: 'Submit' },
-						}}
-					/>
-				</GEL>
-			),
-		},
+		// {
+		// 	heading: 'Input group with text left and button right',
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				name="inputgroup-left-text-right-button"
+		// 				label="inputgroup-left-text-right-button"
+		// 				data={{
+		// 					before: { inputType: 'text', data: 'AUS $' },
+		// 					after: { inputType: 'button', data: 'Submit' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
+		// {
+		// 	heading: 'Input group with text left and text right',
+		// 	component: () => (
+		// 		<GEL brand={overridesWithTokens}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				name="inputgroup-left-text-right-text"
+		// 				label="inputgroup-left-text-right-text"
+		// 				data={{
+		// 					before: { inputType: 'text', data: 'AUS $' },
+		// 					after: { inputType: 'text', data: '.00' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
+		// {
+		// 	heading: 'Input group with button left and button right',
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				name="inputgroup-left-button-right-button"
+		// 				label="inputgroup-left-button-right-button"
+		// 				data={{
+		// 					before: { inputType: 'button', data: '-' },
+		// 					after: { inputType: 'button', data: '+' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
+		// {
+		// 	heading: 'Input group with hero button',
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				look="hero"
+		// 				name="inputgroup-button-hero"
+		// 				label="inputgroup-button-hero"
+		// 				data={{
+		// 					after: { inputType: 'button', data: 'Submit' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
+		// {
+		// 	heading: 'Input group with primary button',
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				look="primary"
+		// 				name="inputgroup-button-primary"
+		// 				label="inputgroup-button-primary"
+		// 				data={{
+		// 					after: { inputType: 'button', data: 'Submit' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
+		// {
+		// 	heading: 'Input group with faint button',
+		// 	component: () => (
+		// 		<GEL brand={brand}>
+		// 			<InputGroup
+		// 				instanceIdPrefix="GEL"
+		// 				look="faint"
+		// 				name="inputgroup-button-faint"
+		// 				label="inputgroup-button-faint"
+		// 				data={{
+		// 					after: { inputType: 'button', data: 'Submit' },
+		// 				}}
+		// 			/>
+		// 		</GEL>
+		// 	),
+		// },
 		...sizes.map((size) => ({
 			heading: `Input group size ${size}`,
 			component: () => (
-				<GEL brand={brand}>
+				<GEL brand={overridesWithTokens}>
 					<InputGroup
 						instanceIdPrefix="GEL"
 						size={size}
@@ -446,7 +320,7 @@ export function Docs({ brand }) {
 									{ text: 'GBP £', value: 'GBP £' },
 								],
 							},
-							after: { inputType: 'button', data: 'Go' },
+							after: { inputType: 'text', data: '.00' },
 						}}
 					/>
 				</GEL>
--- components/input-group/src/overrides/textInput.js
+++ components/input-group/src/overrides/textInput.js
@@ -1,29 +1,101 @@
 /** @jsx jsx */
 
-import { jsx, getLabel } from '@westpac/core';
+import { jsx, getLabel, classNames } from '@westpac/core';
 import { TextInput as Input } from '@westpac/text-input';
+
+// ==============================
+// Component
+// ==============================
 
 const TextInput = ({ state: { size }, ...rest }) => <Input size={size} {...rest} />;
 
-const textInputStyles = (_, { before, after, size }) => ({
-	label: getLabel('inputGroup-textInput', { before, after, size }),
-	boxSizing: 'border-box',
-	...(before && {
-		borderTopLeftRadius: 0,
-		borderBottomLeftRadius: 0,
-	}),
-	...(after && {
-		borderTopRightRadius: 0,
-		borderBottomRightRadius: 0,
-	}),
-});
+const BlenderTextInput = (props) => {
+	const {
+		state: { before, after },
+	} = props;
+	return (
+		<TextInput
+			overrides={{
+				TextInput: {
+					attributes: (attributes) => {
+						if (attributes) {
+							attributes.className = attributes.className.concat(
+								' ',
+								classNames({
+									[`__convert__inputGroup-textInput-before`]: before && !after,
+									[`__convert__inputGroup-textInput-middle`]: before && after,
+									[`__convert__inputGroup-textInput-after`]: after && !before,
+								})
+							); // unable to use attributes below since there is already a className being passed as part of the blender text input
+						}
+						return attributes;
+					},
+				},
+			}}
+			{...props}
+		/>
+	);
+};
+
+// ==============================
+// Styles
+// ==============================
+
+const textInputStyles = (_, { before, after }) => {
+	const modifier = before && after ? 'middle' : before ? 'before' : 'after'; // for blender
+
+	return {
+		label: getLabel(`inputGroup-textInput-${modifier}`),
+		boxSizing: 'border-box',
+		...(before && {
+			borderTopLeftRadius: 0,
+			borderBottomLeftRadius: 0,
+		}),
+		...(after && {
+			borderTopRightRadius: 0,
+			borderBottomRightRadius: 0,
+		}),
+	};
+};
+
+// ==============================
+// Blender Styles
+// ==============================
+// button and select only take a position prop which so we only need button/select-before/after
+// textInput is similar but takes before and after as props directly but still only needs to generate textInput-before/after
+// for these components we have to pass className of before/after only we dont need the base one since there is no base one
+
+// text takes size and position prop is most like other components
+// classes should be .text, text-before, text-after, text-size
+// I can't generate .text
+// actually I can, Im just going to pass position: null to override the one already there
+// problem here is generating before/after classes? on the component nah I can just use regular attributes here
+
+const blenderStyles = (_, { before, after }) => {
+	const { label, ...styles } = textInputStyles(_, { before, after });
+	return { label, 'input&': styles }; // need to increase specificity
+};
+
+// ==============================
+// Attributes
+// ==============================
 
 const textInputAttributes = (_, { instanceId }) => ({
 	id: instanceId,
 });
 
+// ==============================
+// Exports
+// ==============================
+
 export const defaultTextInput = {
 	component: TextInput,
 	styles: textInputStyles,
+	attributes: textInputAttributes,
+};
+
+export const blenderTextInput = {
+	component: BlenderTextInput,
+	styles: blenderStyles,
 	attributes: textInputAttributes,
 };
