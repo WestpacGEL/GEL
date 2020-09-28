c0b664d2c09786ead0798588a9c846035bbe2a2e
Jonny Stening <info@jonathanstening.com>
2020-09-28 15:39:06 +1000

    refactor(form-check): move sizeMap into _utils

--- components/form-check/src/overrides/option.js
+++ components/form-check/src/overrides/option.js
@@ -1,36 +1,20 @@
 /** @jsx jsx */
 
 import { jsx, getLabel } from '@westpac/core';
+import { sizeMap } from '../_utils';
 
 const Option = ({ state: _, ...rest }) => <div {...rest} />;
 
-const optionStyles = (_, { size, inline }) => {
-	const sizeMap = {
-		medium: {
-			marginRight: '1.125rem',
-			marginBottom: '0.375rem',
-			width: '1.5rem',
-			height: '1.5rem',
-		},
-		large: {
-			marginRight: '1.125rem',
-			marginBottom: '0.75rem',
-			width: '1.875rem',
-			height: '1.875rem',
-		},
-	};
-
-	return {
-		label: getLabel('formCheck-option', { size, inline }),
-		position: 'relative',
-		display: inline ? 'inline-block' : 'block',
-		verticalAlign: inline && 'top',
-		marginRight: inline && sizeMap[size].marginRight,
-		marginBottom: sizeMap[size].marginBottom,
-		minHeight: sizeMap[size].height,
-		paddingLeft: sizeMap[size].width,
-	};
-};
+const optionStyles = (_, { size, inline }) => ({
+	label: getLabel('formCheck-option', { size, inline }),
+	position: 'relative',
+	display: inline ? 'inline-block' : 'block',
+	verticalAlign: inline && 'top',
+	marginRight: inline && sizeMap[size]['option'].marginRight,
+	marginBottom: sizeMap[size]['option'].marginBottom,
+	minHeight: sizeMap[size]['option'].height,
+	paddingLeft: sizeMap[size]['option'].width,
+});
 
 const optionAttributes = () => null;
 
