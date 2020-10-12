8dc8b5b452e4bfdd76c87329b53f0f15b1b95b59
Jonny Stening <info@jonathanstening.com>
2020-10-12 20:31:53 +1100

    docs(button): update test examples

--- components/button/examples/00-look.js
+++ components/button/examples/00-look.js
@@ -20,66 +20,8 @@ function Example({ brand }) {
 			<Button>Default standard</Button> <Button soft>Default soft</Button>
 			<hr />
 			<h2>Standard</h2>
-			<Button look="primary">Primary standard</Button>{' '}
-			<Button look="primary" soft>
-				Primary soft
-			</Button>{' '}
-			<Button look="primary" size="small">
-				Primary small
-			</Button>{' '}
-			<Button look="primary" size="medium">
-				Primary medium
-			</Button>{' '}
-			<Button look="primary" size="large">
-				Primary large
-			</Button>{' '}
-			<div css={{ marginTop: '0.5rem' }}>
-				<Button look="primary" block iconAfter={ArrowRightIcon} justify>
-					Primary block
-				</Button>
-			</div>{' '}
-			<br />
-			<br />
-			<Button look="hero">Primary standard</Button>{' '}
-			<Button look="hero" soft>
-				Primary soft
-			</Button>{' '}
-			<Button look="hero" size="small">
-				Primary small
-			</Button>{' '}
-			<Button look="hero" size="medium">
-				Primary medium
-			</Button>{' '}
-			<Button look="hero" size="large">
-				Primary large
-			</Button>{' '}
-			<div css={{ marginTop: '0.5rem' }}>
-				<Button look="hero" block iconAfter={ArrowRightIcon} justify>
-					Primary block
-				</Button>
-			</div>{' '}
-			<br />
-			<br />
-			<Button look="faint">Primary standard</Button>{' '}
-			<Button look="faint" soft>
-				Primary soft
-			</Button>{' '}
-			<Button look="faint" size="small">
-				Primary small
-			</Button>{' '}
-			<Button look="faint" size="medium">
-				Primary medium
-			</Button>{' '}
-			<Button look="faint" size="large">
-				Primary large
-			</Button>{' '}
-			<div css={{ marginTop: '0.5rem' }}>
-				<Button look="faint" block iconAfter={ArrowRightIcon} justify>
-					Primary block
-				</Button>
-			</div>{' '}
-			<br />
-			<br />
+			<Button look="primary">Primary standard</Button> <Button look="hero">Hero standard</Button>{' '}
+			<Button look="faint">Faint standard</Button> <Button look="link">Link</Button>
 			<hr />
 			<h2>Soft</h2>
 			<Button look="primary" soft>
