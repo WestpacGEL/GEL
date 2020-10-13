50143046c151e016ea601c0e429a9a6c7d1d2857
Jonny Stening <info@jonathanstening.com>
2020-10-13 12:20:46 +1100

    feat(form-check): add blender support and update recipe

--- components/form-check/src/overrides/hint.js
+++ components/form-check/src/overrides/hint.js
@@ -1,24 +1,67 @@
 /** @jsx jsx */
 
-import { jsx, useBrand } from '@westpac/core';
+import { jsx, useBrand, getLabel, getModifier, styleReconciler } from '@westpac/core';
 import { Body } from '@westpac/body';
 import { sizeMap } from '../_utils';
 
+import { defaultProps } from '../FormCheck';
+
+// ==============================
+// Component
+// ==============================
+
 const Hint = ({ state: _, ...rest }) => <Body {...rest} />;
 
+// ==============================
+// Styles
+// ==============================
+
 const hintStyles = (_, { size }) => {
-	const { COLORS, PACKS } = useBrand();
+	const { COLORS } = useBrand();
 
 	return {
+		label: getLabel('formCheck-hint'),
 		paddingLeft: sizeMap[size]['label'].gap,
 		color: COLORS.muted,
 	};
 };
 
+// ==============================
+// Blender Styles
+// ==============================
+
+const blenderStyles = () => hintStyles(null, defaultProps);
+
+export const nestedHintStyles = ({ size }) => {
+	const props = { size };
+	let modifiers = getModifier(defaultProps, props);
+	if (!modifiers.length) return {};
+
+	const baseStyles = hintStyles(null, defaultProps);
+	const modifierStyles = hintStyles(null, props);
+	const reconciledStyles = styleReconciler(baseStyles, modifierStyles);
+
+	return { [`.__convert__${baseStyles.label}`]: reconciledStyles };
+};
+
+// ==============================
+// Attributes
+// ==============================
+
 const hintAttributes = (_, { hintId }) => ({ id: hintId });
+
+// ==============================
+// Exports
+// ==============================
 
 export const defaultHint = {
 	component: Hint,
 	styles: hintStyles,
+	attributes: hintAttributes,
+};
+
+export const blenderHint = {
+	component: Hint,
+	styles: blenderStyles,
 	attributes: hintAttributes,
 };
