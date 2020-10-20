8647703319a330821ebd353832d507b10bed6e55
Jonny Stening <info@jonathanstening.com>
2020-10-20 12:12:13 +1100

    docs(badge): update blender recipe

--- components/badge/blender/recipe.js
+++ components/badge/blender/recipe.js
@@ -1,8 +1,10 @@
-import { GEL } from '@westpac/core';
+import { GEL, titleCase } from '@westpac/core';
 import React from 'react';
 
 import { Badge } from '@westpac/badge';
 import { blenderBadge } from '../src/overrides/badge';
+
+const looks = ['primary', 'hero', 'faint', 'info', 'success', 'warning', 'danger'];
 
 export function AllStyles({ brand }) {
 	const overridesWithTokens = { ...brand };
@@ -37,76 +39,21 @@ export function Docs({ brand }) {
 
 	return [
 		{
-			heading: 'A default badge',
+			heading: 'Badge looks',
+			subheading: 'Default',
 			component: () => (
 				<GEL brand={overridesWithTokens} noPrefix>
 					<Badge value="Default" />
 				</GEL>
 			),
 		},
-		{
-			heading: 'A primary badge',
+		...looks.map((look) => ({
+			subheading: titleCase(look),
 			component: () => (
 				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="primary" value="Primary" />
+					<Badge look={look} value={titleCase(look)} />
 				</GEL>
 			),
-		},
-		{
-			heading: 'A hero badge',
-			component: () => (
-				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="hero" value="Hero" />
-				</GEL>
-			),
-		},
-		{
-			heading: 'A neutral badge',
-			component: () => (
-				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="neutral" value="Neutral" />
-				</GEL>
-			),
-		},
-		{
-			heading: 'A faint badge',
-			component: () => (
-				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="faint" value="Faint" />
-				</GEL>
-			),
-		},
-		{
-			heading: 'A success badge',
-			component: () => (
-				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="success" value="Success" />
-				</GEL>
-			),
-		},
-		{
-			heading: 'An info badge',
-			component: () => (
-				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="info" value="Info" />
-				</GEL>
-			),
-		},
-		{
-			heading: 'A warning badge',
-			component: () => (
-				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="warning" value="Warning" />
-				</GEL>
-			),
-		},
-		{
-			heading: 'A danger badge',
-			component: () => (
-				<GEL brand={overridesWithTokens} noPrefix>
-					<Badge look="danger" value="Danger" />
-				</GEL>
-			),
-		},
+		})),
 	];
 }
