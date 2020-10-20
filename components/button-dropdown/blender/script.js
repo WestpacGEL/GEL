37d6a06e68fb8b19071bcf0287f1d1ce68571792
Jonny Stening <info@jonathanstening.com>
2020-10-20 16:36:13 +1100

    refactor(button-dropdown): blender script update, remove unsupported JS, reduce code duplication, add aria-expanded toggling

--- components/button-dropdown/blender/script.js
+++ components/button-dropdown/blender/script.js
@@ -1,17 +1,24 @@
 $(function () {
-	$('[data-js="buttonDropdown-btn__version__"]')
-		.on('click', function () {
-			var $panel = $(this).next('[data-js="buttonDropdown-panel__version__"]');
+	$('[data-js="buttonDropdown-btn__version__"]').on('click keyup', function (e) {
+		var event = e.type === 'keyup' && e.keyCode === 27 ? 'esc' : 'click';
+		var $this = $(this);
+
+		if (event === 'click' || event === 'esc') {
+			var $panel = $this.next('[data-js="buttonDropdown-panel__version__"]');
 			var classes = $panel.attr('class').split(/\s+/);
-			var baseClass = classes.find((el) => /panel$/.test(el));
+			var baseClass = classes.filter(function (el) {
+				return /panel$/.test(el);
+			})[0];
+		}
+		if (event === 'click') {
+			$this.attr('aria-expanded', function (_, val) {
+				return val === 'true' ? 'false' : 'true';
+			});
 			$panel.toggleClass(`${baseClass}-open`);
-		})
-		.keyup(function (event) {
-			if (event.keyCode === 27) {
-				var $panel = $(this).next('[data-js="buttonDropdown-panel__version__"]');
-				var classes = $panel.attr('class').split(/\s+/);
-				var baseClass = classes.find((el) => /panel$/.test(el));
-				$panel.removeClass(`${baseClass}-open`);
-			}
-		});
+		}
+		if (event === 'esc') {
+			$this.attr('aria-expanded', false);
+			$panel.removeClass(`${baseClass}-open`);
+		}
+	});
 });
