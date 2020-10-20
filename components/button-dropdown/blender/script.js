737f4f0dd3a595ce91b00b147e94ca798784f5e9
Jonny Stening <info@jonathanstening.com>
2020-10-20 16:43:02 +1100

    fix(button-dropdown): blender script update, fix keyboard tab behaviour

--- components/button-dropdown/blender/script.js
+++ components/button-dropdown/blender/script.js
@@ -1,22 +1,21 @@
 $(function () {
 	$('[data-js="buttonDropdown-btn__version__"]').on('click keyup', function (e) {
-		var event = e.type === 'keyup' && e.keyCode === 27 ? 'esc' : 'click';
 		var $this = $(this);
 
-		if (event === 'click' || event === 'esc') {
+		if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 27)) {
 			var $panel = $this.next('[data-js="buttonDropdown-panel__version__"]');
 			var classes = $panel.attr('class').split(/\s+/);
 			var baseClass = classes.filter(function (el) {
 				return /panel$/.test(el);
 			})[0];
 		}
-		if (event === 'click') {
+		if (e.type === 'click') {
 			$this.attr('aria-expanded', function (_, val) {
 				return val === 'true' ? 'false' : 'true';
 			});
 			$panel.toggleClass(`${baseClass}-open`);
 		}
-		if (event === 'esc') {
+		if (e.type === 'keyup' && e.keyCode === 27) {
 			$this.attr('aria-expanded', false);
 			$panel.removeClass(`${baseClass}-open`);
 		}
