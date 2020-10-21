44c806d30091feb01e9ad129d7e8f1204a97696b
Jonny Stening <info@jonathanstening.com>
2020-10-21 12:15:13 +1100

    feat: additional a11y content shortcodes

--- website/src/shortcodes.js
+++ website/src/shortcodes.js
@@ -2,4 +2,8 @@ import { brandsMap } from './components/brand-switcher/brand-switcher';
 // Shortcodes can contain characters a-z and numbers 0-9 but no other characters
 export const getShortCodes = (brand) => ({
 	brandName: brandsMap[brand].label,
+	perceivable: `This is generic <em>perceivable</em> content.`,
+	operable: `This is generic <em>operable</em> content.`,
+	understandable: `This is generic <em>understandable</em> content.`,
+	robust: `This is generic <em>robust</em> content.`,
 });
