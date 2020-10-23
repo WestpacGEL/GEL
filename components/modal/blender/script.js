39808dbfcacab8a0afebd9b62dbe02efac8d1e24
Jonny Stening <info@jonathanstening.com>
2020-10-23 14:14:16 +1100

    fix(modal): update blender script, move toggle function outside of document.ready()

--- components/modal/blender/script.js
+++ components/modal/blender/script.js
@@ -1,30 +1,30 @@
-$(function () {
-	function toggleModalWrapper($modalWrapper) {
-		const classes = $modalWrapper.attr('class').split(/\s+/);
-		const baseClass = classes.find((el) => /wrapper$/.test(el));
-		$modalWrapper.toggleClass(baseClass.concat('-open'));
-	}
+function GELToggleModalWrapper($modalWrapper) {
+	const classes = $modalWrapper.attr('class').split(/\s+/);
+	const baseClass = classes.find((el) => /wrapper$/.test(el));
+	$modalWrapper.toggleClass(baseClass.concat('-open'));
+}
 
-	$('[data-modal]').click(function (e) {
+$(function () {
+	$('[data-modal]').on('click', function (e) {
 		e.stopPropagation();
 		const modalID = $(this).data('modal');
 		const $modal = $(`#${modalID}`);
 
 		const $modalWrapper = $modal.closest('[data-js="modal-wrapper__version__"]');
-		toggleModalWrapper($modalWrapper);
+		GELToggleModalWrapper($modalWrapper);
 	});
 
-	$('[data-js="modal-closeBtn__version__"]').click(function (e) {
+	$('[data-js="modal-closeBtn__version__"]').on('click', function (e) {
 		e.stopPropagation();
 		const $modalWrapper = $(this).closest('[data-js="modal-wrapper__version__"]');
-		toggleModalWrapper($modalWrapper);
+		GELToggleModalWrapper($modalWrapper);
 	});
 
-	$('[data-js="modal-wrapper__version__"]').click(function (e) {
+	$('[data-js="modal-wrapper__version__"]').on('click', function (e) {
 		if (e.target != this) {
 			return false;
 		}
 		const $modalWrapper = $(this);
-		toggleModalWrapper($modalWrapper);
+		GELToggleModalWrapper($modalWrapper);
 	});
 });
