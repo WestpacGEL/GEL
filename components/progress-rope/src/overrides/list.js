8fd27da29c9a1e18d7523304f56bef1a3f26aafc
Jonny Stening <info@jonathanstening.com>
2020-10-28 09:18:14 +1100

    fix(progress-rope): add list role to enhance UX in VoiceOver

--- components/progress-rope/src/overrides/list.js
+++ components/progress-rope/src/overrides/list.js
@@ -40,7 +40,9 @@ const listStyles = () => {
 // Attributes
 // ==============================
 
-const listAttributes = () => null;
+const listAttributes = () => ({
+	role: 'list',
+});
 
 // ==============================
 // Exports
