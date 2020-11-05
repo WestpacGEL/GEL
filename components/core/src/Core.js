6c527d189db36b55a4a246f06af2b144a8d75992
Jonny Stening <info@jonathanstening.com>
2020-11-05 17:33:08 +1100

    fix(core): parsing of stylis CSS, ensure child selectors are .GEL prepended

--- components/core/src/Core.js
+++ components/core/src/Core.js
@@ -7,21 +7,27 @@ import createCache from '@emotion/cache';
 import { useBrand } from './Brand';
 import { reset } from './reset';
 
-const label = 'Core'; // for use in stylis plugin and the css label
+const coreLabel = 'Core'; // for use in stylis plugin and the css label
 
 const AddRootClass = ({ children }) => {
 	let [cache] = useState(() => {
 		return createCache({
 			stylisPlugins: [
-				(context, content, selectors, parent, line, column, length) => {
+				(context, content, selectors) => {
+					if (context === -2 && selectors.length && selectors[0] !== '') {
+						console.log(`Original: ${content}`);
+					}
 					if (
 						context === -2 &&
 						selectors.length &&
 						selectors[0] !== '' && // exclude <Global /> styles
-						!selectors[0].includes(`-${label}`) // exclude nested <GEL /> (Core) styles
+						!selectors[0].includes(`-${coreLabel}`) // exclude nested <GEL /> (Core) styles
 					) {
+						/* 1. Strip any parsed CSS comments */
+						content = content.replace(/\/\*.*?\*\//g, '');
+
 						/**
-						 * Add to beginning of `content` string, if not beginning with `@`
+						 * 2. Add to beginning of `content` string, if not beginning with `@`
 						 * (catches `@media` and `@font-face` queries etc)
 						 *
 						 * Regex explanation:
@@ -33,14 +39,29 @@ const AddRootClass = ({ children }) => {
 						content = content.replace(/^[^@].*/g, (s) => `.GEL ${s}`);
 
 						/**
-						 * Additionally, insert within @media queries
+						 * 3. Additionally, consider selectors found within the same string (e.g. child selectors)
 						 *
 						 * Regex explanation:
-						 * - (\){) Match `){`
+						 * - (;}) Capture group #1 matching `;}`
+						 * - ([^@/}]) Capture group #2 matching negated set (not `@`, `/` or `}` symbol)
 						 * - /g Global search
 						 */
-						content = content.replace(/(\){)/g, (s) => `${s}.GEL `);
+						content = content.replace(/(;})([^@/}])/g, '$1.GEL $2');
+
+						/**
+						 * 4. Additionally, insert within all @media queries
+						 *
+						 * Regex explanation:
+						 * - @ Match `@` character
+						 * - .+ Any character except new line, match 1 or more
+						 * - ? Makes the preceding quantifier lazy, causing it to match as few characters as possible
+						 * - /g Global search
+						 *
+						 * - i.e. match `@media (min-width:576px){` and `@print {`
+						 */
+						content = content.replace(/@.+?\{/g, (s) => `${s}.GEL `);
 					}
+					console.log(`Edited: ${content}`);
 					return content;
 				},
 			],
@@ -56,7 +77,7 @@ export const Core = ({ noReset, noPrefix, children }) => {
 		<div
 			className="GEL"
 			css={{
-				label,
+				label: coreLabel,
 				lineHeight: 1.428571429,
 				color: COLORS.text,
 				fontFeatureSettings: '"liga" 1', // Enable OpenType ligatures in IE
