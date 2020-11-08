3c56ca2275d42d53c38468b3b1baf8b535030ff0
Jonny Stening <info@jonathanstening.com>
2020-11-09 08:56:18 +1100

    chore(icon): update example tests

--- components/icon/examples/20-colors.js
+++ components/icon/examples/20-colors.js
@@ -2,7 +2,7 @@
 
 import { GEL, jsx } from '@westpac/core';
 import { FavouriteIcon } from '@westpac/icon';
-import { Row } from './_util';
+import { Grid, Cell, Name, Row } from './_util';
 
 const brandColors = [
 	'primary',
@@ -16,46 +16,63 @@ const brandColors = [
 	'light',
 ];
 const reservedColors = ['info', 'success', 'warning', 'danger', 'system'];
+const customColors = ['#DE350B', '#FF991F', '#00875A', '#00A3BF', '#0052CC', '#5243AA'];
 
 function Example({ brand }) {
 	return (
 		<GEL brand={brand}>
 			<h2>Default</h2>
-			<FavouriteIcon assistiveText="Colored heart" />
+			<Grid css={{ justifyItems: 'start' }}>
+				<Cell>
+					<FavouriteIcon assistiveText="Coloured heart" />
+					<Name>{`<FavouriteIcon\u00A0/>`}</Name>
+				</Cell>
+			</Grid>
 
 			<hr />
 
 			<h2>System colours</h2>
 
 			<h3>Brand colours</h3>
-			<Row>
+			<Grid>
 				{brandColors.map((color) => (
-					<FavouriteIcon key={color} assistiveText="Colored heart" color={color} />
+					<Cell>
+						<FavouriteIcon key={color} assistiveText="Coloured heart" color={color} />
+						<Name>{`<FavouriteIcon color="${color}"\u00A0/>`}</Name>
+					</Cell>
 				))}
-			</Row>
+			</Grid>
 
 			<h3>Reserved colours</h3>
-			<Row>
+			<Grid>
 				{reservedColors.map((color) => (
-					<FavouriteIcon key={color} assistiveText="Colored heart" color={color} />
+					<Cell>
+						<FavouriteIcon key={color} assistiveText="Coloured heart" color={color} />
+						<Name>{`<FavouriteIcon color="${color}"\u00A0/>`}</Name>
+					</Cell>
 				))}
-			</Row>
+			</Grid>
 
 			<hr />
 
 			<h2>Custom colours</h2>
 			<h3>Inherit</h3>
-			<FavouriteIcon assistiveText="Colored heart" color="inherit" />
+			<Grid css={{ justifyItems: 'start' }}>
+				<Cell>
+					<FavouriteIcon assistiveText="Coloured heart" color="inherit" />
+					<Name>{`<FavouriteIcon color="inherit"\u00A0/>`}</Name>
+				</Cell>
+			</Grid>
 
 			<h3>Hex values</h3>
-			<Row>
-				<FavouriteIcon assistiveText="Colored heart" color="#DE350B" />
-				<FavouriteIcon assistiveText="Colored heart" color="#FF991F" />
-				<FavouriteIcon assistiveText="Colored heart" color="#00875A" />
-				<FavouriteIcon assistiveText="Colored heart" color="#00A3BF" />
-				<FavouriteIcon assistiveText="Colored heart" color="#0052CC" />
-				<FavouriteIcon assistiveText="Colored heart" color="#5243AA" />
-			</Row>
+			<Grid>
+				{customColors.map((color) => (
+					<Cell key={color}>
+						<FavouriteIcon assistiveText="Coloured heart" color={color} />
+						<Name>{`<FavouriteIcon color="${color}"\u00A0/>`}</Name>
+					</Cell>
+				))}
+			</Grid>
 		</GEL>
 	);
 }
