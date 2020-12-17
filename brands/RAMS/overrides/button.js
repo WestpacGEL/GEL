6dfe63eae58b13259631416775a605d427728517
Jonny Stening <info@jonathanstening.com>
2020-12-17 20:17:34 +1100

    feat(rams): add RAMS brand button override

--- brands/RAMS/overrides/button.js
+++ brands/RAMS/overrides/button.js
@@ -1,21 +1,37 @@
+const sizeMap = {
+	small: { borderBottomWidth: '0.125rem' },
+	medium: { borderBottomWidth: '0.1875rem' },
+	large: { borderBottomWidth: '0.25rem' },
+	xlarge: { borderBottomWidth: '0.3125rem' },
+};
+
 export const button = ({ COLORS }) => ({
 	Button: {
-		styles: (styles, { look, soft }) => {
-			if (soft) {
-				if (look === 'primary') {
-					return {
-						...styles,
-						color: COLORS.primary,
-					};
-				}
-				if (look === 'hero') {
-					return {
-						...styles,
-						color: COLORS.hero,
-					};
-				}
-			}
-			return styles;
+		styles: (styles, { look, size, soft }) => {
+			return {
+				...styles,
+
+				...((look === 'primary' || look === 'hero') && {
+					...(soft && { color: COLORS[look] }),
+
+					// Bottom bar
+					position: 'relative',
+					'::after': {
+						content: '""',
+						display: 'block',
+						position: 'absolute',
+						zIndex: 0,
+						bottom: '-1px',
+						left: '-1px',
+						right: '-1px',
+						height: 0,
+						borderBottom: 'solid #78C339', //RAMS Green (secondary palette)
+						borderBottomLeftRadius: '3px',
+						borderBottomRightRadius: '3px',
+						...sizeMap[size],
+					},
+				}),
+			};
 		},
 	},
 });
