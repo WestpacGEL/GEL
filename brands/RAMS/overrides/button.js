7eabbbfea225cbe8843566ab3fc4f1cc9adbe198
Jonny Stening <info@jonathanstening.com>
2020-12-19 15:47:09 +1100

    fix(rams): fix primary/hero button bottom bar styling with responsive button size

--- brands/RAMS/overrides/button.js
+++ brands/RAMS/overrides/button.js
@@ -1,26 +1,31 @@
-const sizeMap = {
-	small: { offsetY: '0.125rem' },
-	medium: { offsetY: '0.1875rem' },
-	large: { offsetY: '0.25rem' },
-	xlarge: { offsetY: '0.3125rem' },
-};
+export const button = ({ COLORS }) => {
+	const asArray = (val) => (Array.isArray(val) ? val : [val]);	
+	const sizeMap = {
+		small: { offsetY: '0.125rem' },
+		medium: { offsetY: '0.1875rem' },
+		large: { offsetY: '0.25rem' },
+		xlarge: { offsetY: '0.3125rem' },
+	};
 
-export const button = ({ COLORS }) => ({
-	Button: {
-		styles: (styles, { look, size, soft }) => {
-			return {
-				...styles,
+	return {
+		Button: {
+			styles: (styles, { look, size, soft }) => {
+				const sizeArr = asArray(size);
 
-				...((look === 'primary' || look === 'hero') && {
-					...(soft && { color: COLORS[look] }),
+				return {
+					...styles,
 
-					// Bottom bar
-					...(!soft && {
-						border: 0, //reset
-						boxShadow: `inset 0 -${sizeMap[size].offsetY} #78C339`,
+					...((look === 'primary' || look === 'hero') && {
+						...(soft && { color: COLORS[look] }),
+
+						// Bottom bar
+						...(!soft && {
+							border: 0, //reset
+							boxShadow: sizeArr.map(s => s && `inset 0 -${sizeMap[s].offsetY} #78C339`),
+						}),
 					}),
-				}),
-			};
-		},
-	},
-});
+				};
+			},
+		}
+	};
+};
