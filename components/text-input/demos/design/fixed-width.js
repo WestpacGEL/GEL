b4e35a57164a1c8789213c6b34dde767a7bd8359
Jonny Stening <info@jonathanstening.com>
2021-04-06 10:57:53 +1000

    chore(text-input): update fixed width website demo

--- components/text-input/demos/design/fixed-width.js
+++ components/text-input/demos/design/fixed-width.js
@@ -12,24 +12,12 @@ export default ({ context, showCode, showDemo }) => {
 			<TextInput name="example-width-2" width={2} />
 			<br />
 			<br />
-			<Title>3 characters</Title>
-			<TextInput name="example-width-3" width={3} />
-			<br />
-			<br />
 			<Title>4 characters</Title>
 			<TextInput name="example-width-4" width={4} />
 			<br />
 			<br />
-			<Title>5 characters</Title>
-			<TextInput name="example-width-5" width={5} />
-			<br />
-			<br />
 			<Title>10 characters</Title>
 			<TextInput name="example-width-10" width={10} />
-			<br />
-			<br />
-			<Title>20 characters</Title>
-			<TextInput name="example-width-20" width={20} />
 			<br />
 			<br />
 			<Title>30 characters</Title>
