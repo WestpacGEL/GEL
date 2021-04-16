076e713b13d03e7acf63ca8206adbfb5d6138a51
Jonny Stening <info@jonathanstening.com>
2021-04-16 13:50:46 +1000

    style(selector): use input:checked for indicator

--- components/selector/src/overrides/indicator.js
+++ components/selector/src/overrides/indicator.js
@@ -46,20 +46,18 @@ const indicatorStyles = (_, { type, checked, nextIndicator }) => {
 			width: '36px', //xlarge icon size
 			height: '36px', //xlarge icon size
 
-			...(checked && {
-				'::after': {
-					content: '""',
-					display: 'block',
-					boxSizing: 'border-box',
-					position: 'relative',
-					top: '-0.125rem', //tweak
-					width: '1.25rem',
-					height: '0.625rem',
-					border: `solid ${COLORS.hero}`,
-					borderWidth: '0 0 0.1875rem 0.1875rem',
-					transform: 'rotate(-54deg)',
-				},
-			}),
+			'input:checked + div &::after': {
+				content: '""',
+				display: 'block',
+				boxSizing: 'border-box',
+				position: 'relative',
+				top: '-0.125rem', //tweak
+				width: '1.25rem',
+				height: '0.625rem',
+				border: `solid ${COLORS.hero}`,
+				borderWidth: '0 0 0.1875rem 0.1875rem',
+				transform: 'rotate(-54deg)',
+			},
 		}),
 
 		// Note: Temporary fix... mq() array values must come last if `&` selectors are used (as above). This is an issue with our reset specificity solution (`.GEL ` class prepend)
