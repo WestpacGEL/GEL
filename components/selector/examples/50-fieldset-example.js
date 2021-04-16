1b8a4f4449992eb0d7c2b955a2cbeb8b2b3a26f3
Jonny Stening <info@jonathanstening.com>
2021-04-16 13:34:46 +1000

    docs(selector): add fieldset markup example

---
+++ components/selector/examples/50-fieldset-example.js
@@ -0,0 +1,42 @@
+/** @jsx jsx */
+
+import { GEL, jsx, useBrand } from '@westpac/core';
+import { Selector, Option } from '@westpac/selector';
+
+const Fieldset = (props) => <fieldset css={{ border: 'none', margin: 0, padding: 0 }} {...props} />;
+
+const Legend = (props) => {
+	const { PACKS, TYPE, SPACING } = useBrand();
+
+	return (
+		<legend
+			css={{ ...PACKS.typeScale.bodyFont[8], ...TYPE.bodyFont[600], marginBottom: SPACING(4) }}
+			{...props}
+		/>
+	);
+};
+function Example({ brand }) {
+	const { COLORS } = useBrand();
+
+	return (
+		<GEL brand={brand}>
+			<h2>Default</h2>
+			<p css={{ color: COLORS.muted, fontStyle: 'italic' }}>
+				Note: The following examples illustrate the HTML markup required for this component to be
+				accessible. Fieldset and legend styling is not provided by this component.
+			</p>
+
+			<Fieldset>
+				<Legend>Choose a thing</Legend>
+
+				<Selector name="example-default">
+					<Option value="1">Here is a label</Option>
+					<Option value="2">Here is a label</Option>
+					<Option value="3">Here is a label</Option>
+				</Selector>
+			</Fieldset>
+		</GEL>
+	);
+}
+
+export default Example;
