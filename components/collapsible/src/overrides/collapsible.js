418dd8d96fe7757258c457120dca577af2cdbb30
Jonny Stening <info@jonathanstening.com>
2021-05-03 11:03:51 +1000

    chore(collapsible): tidy up

--- components/collapsible/src/overrides/collapsible.js
+++ components/collapsible/src/overrides/collapsible.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import { jsx, getLabel, useBrand } from '@westpac/core';
+import { jsx, getLabel } from '@westpac/core';
 
 // ==============================
 // Component
@@ -12,15 +12,11 @@ const Collapsible = ({ state: _, ...rest }) => <div {...rest} />;
 // Styles
 // ==============================
 
-const collapsibleStyles = () => {
-	const { SPACING } = useBrand();
-
-	return {
-		label: getLabel('collapsible'),
-		position: 'relative',
-		display: 'inline-block',
-	};
-};
+const collapsibleStyles = () => ({
+	label: getLabel('collapsible'),
+	position: 'relative',
+	display: 'inline-block',
+});
 
 // ==============================
 // Attributes
