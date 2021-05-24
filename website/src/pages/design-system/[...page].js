bdba52c11c36e08320760cf422496e2d253f6ba3
Jonny Stening <info@jonathanstening.com>
2021-05-24 14:27:16 +1000

    fix(website): fix tab nav height

--- website/src/pages/design-system/[...page].js
+++ website/src/pages/design-system/[...page].js
@@ -92,22 +92,14 @@ const Tabs = ({ component, tabName }) => {
 					zIndex: 5,
 					height: [66, null, 90],
 					transition: 'box-shadow 0.2s ease',
-				},
 
-				// Intentionally separated from above so `height` array values are not overridden
-				{
-					[`@media (max-width: ${LAYOUT.breakpoints.sm - 1}px)`]: {
-						'body.hasScrolledSmall &': {
-							boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
-						},
+					'body.hasScrolledSmall &': {
+						boxShadow: ['0 2px 5px rgba(0,0,0,0.3)', null, 'none'],
 					},
-					[`@media (min-width: ${LAYOUT.breakpoints.sm}px)`]: {
-						'body.hasScrolledLarge &': {
-							boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
-						},
-					}
-				}
-				)[0],
+					'body.hasScrolledLarge &': {
+						boxShadow: [null, null, '0 2px 5px rgba(0,0,0,0.3)'],
+					},
+				})[0],
 		},
 		TabBtn: {
 			styles: (styles, { selected }) =>
