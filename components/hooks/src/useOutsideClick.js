efacd90d600da56bf4f26791f1a981f38a2a109e
Jonny Stening <info@jonathanstening.com>
2021-06-04 10:54:39 +1000

    feat(hooks): update useOutsideClick hook, new function API, take multiple refs, use useLayoutEffect and listenWhen

--- components/hooks/src/useOutsideClick.js
+++ components/hooks/src/useOutsideClick.js
@@ -1,15 +1,41 @@
-import { useEffect } from 'react';
+import { RefObject, useCallback, useLayoutEffect } from 'react';
 
-export const useOutsideClick = (ref, callback) => {
-	const handleOutsideClick = (e) => {
-		if (ref.current && !ref.current.contains(e.target)) {
-			callback();
+/**
+ * @callback MouseHandler
+ * @param {MouseEvent} event - The native event object.
+ * @return {void}
+ */
+
+/**
+ * Watch for click events outside your target refs and handle them.
+ * @param {Object} options - The options object.
+ * @param {MouseHandler} options.handler - Handle a click outside the target refs
+ * @param {RefObject<HTMLElement>[]} options.refs - An array of refs to ignore click events on
+ * @param {boolean} options.listenWhen - When to add/remove event listeners
+ */
+export const useOutsideClick = ({ handler, refs, listenWhen }) => {
+	const handleMouseDown = useCallback(
+		(event) => {
+			// bail on mouse down "inside" any of the provided refs
+			if (refs.some((ref) => ref.current && ref.current.contains(event.target))) {
+				return;
+			}
+
+			handler(event);
+		},
+		[handler, refs]
+	);
+
+	// layout effect is not run on the server
+	useLayoutEffect(() => {
+		if (listenWhen) {
+			document.addEventListener('mousedown', handleMouseDown);
+
+			return () => {
+				document.removeEventListener('mousedown', handleMouseDown);
+			};
 		}
-	};
-	useEffect(() => {
-		document.addEventListener('click', handleOutsideClick);
-		return () => {
-			document.removeEventListener('click', handleOutsideClick);
-		};
-	});
+		// NOTE: only call when the value of `listenWhen` changes
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [listenWhen]);
 };
