961735f4ee775c796d9a5ef4df9ab5a15db045f3
Jeremy Ortiz <jeremy@jeremyortiz.io>
2021-06-08 18:34:03 +1000

    form: add error pattern demos

---
+++ components/form/demos/design/phone-international-error.js
@@ -0,0 +1,51 @@
+/** @jsx jsx */
+
+import { jsx } from '@westpac/core';
+import {
+	Form,
+	FormGroup,
+	FormLabel,
+	Field,
+	Hint,
+	InputCluster,
+	Item,
+	ErrorMessage,
+} from '@westpac/form';
+import { TextInput } from '@westpac/text-input';
+import { Playground } from '../../../../website/src/components/playground/macro';
+
+const Demo = ({ context, showCode, showDemo }) => {
+	return (
+		<Playground context={context} showCode={showCode} showDemo={showDemo}>
+			<Form spacing="large">
+				<FormGroup>
+					<FormLabel htmlFor="phone-number">Phone number</FormLabel>
+					<Hint>Enter your country code (e.g. +61) and your number</Hint>
+					<ErrorMessage message="Enter your number in either format of 2135096995, 213-509-6995, or (213) 509-6995" />
+					<div id="phone-number">
+						<InputCluster horizontal>
+							<Item>
+								<Field label="Code" subLabel>
+									<TextInput type="tel" size="large" width={4} defaultValue="+891" />
+								</Field>
+							</Item>
+							<Item>
+								<Field label="Phone number" subLabel>
+									<TextInput
+										type="tel"
+										size="large"
+										width={10}
+										invalid
+										defaultValue="213-5096995"
+									/>
+								</Field>
+							</Item>
+						</InputCluster>
+					</div>
+				</FormGroup>
+			</Form>
+		</Playground>
+	);
+};
+
+export default Demo;
