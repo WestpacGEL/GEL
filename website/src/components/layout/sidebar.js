e117e6d8c941390007cc1ab4e29280c80f62ee07
Jonny Stening <info@jonathanstening.com>
2021-06-23 16:18:37 +1000

    fix(website): focus lock the sidebar when it's open, add react-focus-on dep

--- website/src/components/layout/sidebar.js
+++ website/src/components/layout/sidebar.js
@@ -3,6 +3,7 @@ import { jsx, useBrand, useMediaQuery } from '@westpac/core';
 import { Button } from '@westpac/button';
 import { CloseIcon } from '@westpac/icon';
 import { Fragment, useEffect, useLayoutEffect, useState, useRef, forwardRef } from 'react';
+import { FocusOn, AutoFocusInside } from 'react-focus-on';
 
 import { useSidebarContext } from '../providers/sidebar';
 import { BrandDropdown } from '../brand-dropdown';
@@ -42,47 +43,50 @@ export const Sidebar = ({ items }) => {
 
 	return (
 		<Fragment>
-			<div
-				css={mq({
-					boxSizing: 'border-box',
-					position: 'fixed',
-					top: 0,
-					bottom: 0,
-					zIndex: 11,
-					display: 'flex',
-					visibility: [isOpen ? 'visible' : 'hidden', null, null, null, 'visible'],
-					flexDirection: 'column',
-					overflow: 'hidden', //trim nav shadow
-					background: '#fff',
-					borderRight: `1px solid ${COLORS.border}`,
-					width: 300,
-					transform: [isOpen ? 'translateX(0)' : 'translateX(-300px)', null, null, null, 'none'],
-					transition: ['transform 0.15s, visibility 0.15s', null, null, null, 'none'],
-				})}
-				aria-hidden={!isOpen}
-				onTransitionEnd={handleTransitionEnd}
-			>
-				<CloseBtn ref={closeBtnRef} onClick={handleClose} />
-				<BrandDropdown />
-				<Navigation items={items} />
-			</div>
-			{/* Background overlay */}
-			{isOpen && (
+			<FocusOn enabled={isOpen} autoFocus={false}>
 				<div
-					onClick={handleClose}
 					css={mq({
-						display: ['block', null, null, null, 'none'],
+						boxSizing: 'border-box',
 						position: 'fixed',
-						zIndex: 10,
 						top: 0,
-						left: 0,
-						right: 0,
 						bottom: 0,
-						backgroundColor: 'rgba(0,0,0,0.4)',
-						cursor: 'pointer',
+						zIndex: 11,
+						display: 'flex',
+						visibility: [isOpen ? 'visible' : 'hidden', null, null, null, 'visible'],
+						flexDirection: 'column',
+						overflow: 'hidden', //trim nav shadow
+						background: '#fff',
+						borderRight: `1px solid ${COLORS.border}`,
+						width: 300,
+						transform: [isOpen ? 'translateX(0)' : 'translateX(-300px)', null, null, null, 'none'],
+						transition: ['transform 0.15s, visibility 0.15s', null, null, null, 'none'],
 					})}
-				/>
-			)}
+					aria-hidden={!isOpen}
+					onTransitionEnd={handleTransitionEnd}
+				>
+					<CloseBtn ref={closeBtnRef} onClick={handleClose} />
+					<BrandDropdown />
+					<Navigation items={items} />
+				</div>
+
+				{/* Background overlay */}
+				{isOpen && (
+					<div
+						onClick={handleClose}
+						css={mq({
+							display: ['block', null, null, null, 'none'],
+							position: 'fixed',
+							zIndex: 10,
+							top: 0,
+							left: 0,
+							right: 0,
+							bottom: 0,
+							backgroundColor: 'rgba(0,0,0,0.4)',
+							cursor: 'pointer',
+						})}
+					/>
+				)}
+			</FocusOn>
 		</Fragment>
 	);
 };
