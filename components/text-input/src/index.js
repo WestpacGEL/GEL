f695ab5a5e5df2768b6eb3e29e776e8ce87c1222
Jonny Stening <info@jonathanstening.com>
2021-06-29 17:18:47 +1000

    feat(text-input): export utils

--- components/text-input/src/index.js
+++ components/text-input/src/index.js
@@ -6,3 +6,5 @@ export { TextInputWithButton } from './TextInputWithButton';
 export { blenderTextInput } from './overrides/textInput';
 export { blenderSelect } from './overrides/select';
 export { blenderTextarea } from './overrides/textarea';
+
+export { round, sizeMap, textInputWithButtonBtnWidth, getHeight, getMaxWidth } from './_utils';
