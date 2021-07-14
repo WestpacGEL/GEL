f109fec27d2054f85d6edec926279db937f9e3ff
Jeremy Ortiz <jeremy@jeremyortiz.io>
2021-07-14 12:28:05 +1000

    hooks: refactor useWindowSize

--- components/hooks/src/useWindowSize.js
+++ components/hooks/src/useWindowSize.js
@@ -1,24 +1,57 @@
+// import { useEffect, useState } from 'react';
+
+// export const useWindowSize = () => {
+// 	const [windowSize, setWindowSize] = useState({
+// 		width: undefined,
+// 		height: undefined,
+// 	});
+// 	useEffect(() => {
+// 		function handleResize() {
+// 			setWindowSize({
+// 				width: window.innerWidth,
+// 				height: window.innerHeight,
+// 			});
+// 		}
+
+// 		window.addEventListener('resize', handleResize);
+
+// 		handleResize();
+
+// 		return () => window.removeEventListener('resize', handleResize);
+// 	}, []);
+
+// 	return windowSize;
+// };
+
 import { useEffect, useState } from 'react';
 
-export const useWindowSize = () => {
-	const [windowSize, setWindowSize] = useState({
-		width: undefined,
-		height: undefined,
-	});
+let isBrowser = typeof window !== 'undefined';
+
+function getSize() {
+	if (isBrowser) {
+		return {
+			height: window.innerHeight,
+			width: window.innerWidth,
+		};
+	}
+	return {
+		height: 0,
+		width: 0,
+	};
+}
+
+export function useWindowSize() {
+	let [windowSize, setWindowSize] = useState(getSize());
+
 	useEffect(() => {
 		function handleResize() {
-			setWindowSize({
-				width: window.innerWidth,
-				height: window.innerHeight,
-			});
+			setWindowSize(getSize());
 		}
-
 		window.addEventListener('resize', handleResize);
-
-		handleResize();
-
-		return () => window.removeEventListener('resize', handleResize);
-	}, []);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, [setWindowSize]);
 
 	return windowSize;
-};
+}
