3ec25500db4e5cfcbcdaa1cc4e613d8993044014
Jeremy Ortiz <jeremy@jeremyortiz.io>
2021-07-20 13:40:27 +1000

    header/footer/sidebar: add demo

---
+++ components/sidebar/demos/design/sidebar.js
@@ -0,0 +1,15 @@
+/** @jsx jsx */
+
+import { jsx } from '@westpac/core';
+import { Sidebar } from '@westpac/sidebar';
+import { Playground } from '../../../../website/src/components/playground/macro';
+
+const Demo = ({ context, showCode, showDemo }) => {
+	return (
+		<Playground context={context} showCode={showCode} showDemo={showDemo}>
+			<Sidebar />
+		</Playground>
+	);
+};
+
+export default Demo;
