3b54f294729fd81a416180004736aec1b291f9f5
Jonny Stening <info@jonathanstening.com>
2021-07-29 20:58:43 +1000

    feat(selector): add blender support

--- components/selector/blender/script.js
+++ components/selector/blender/script.js
@@ -2,19 +2,16 @@
 	var module = {};
 
 	module.init = function () {
-		$('[data-js="selector__version__"]').on(
-			'click',
-			'button[data-js="selector-optionBtn__version__"]',
-			function (e) {
-				var $this = $(this);
-				var $input = $(e.delegateTarget).find('input[type="hidden"]');
+		$('button[data-js="selector-optionBtn__version__"]').on('click', function () {
+			var $this = $(this);
+			var $selector = $this.closest('[data-js="selector__version__"]');
+			var $input = $selector.find('input[type="hidden"]');
 
-				$('[data-js="selector-optionBtn__version__"]').attr('aria-pressed', 'false');
-				$this.attr('aria-pressed', 'true');
+			$selector.find('[data-js="selector-optionBtn__version__"]').attr('aria-pressed', 'false');
+			$this.attr('aria-pressed', 'true');
 
-				$input.val($this.data('value'));
-			}
-		);
+			$input.val($this.data('value'));
+		});
 	};
 
 	GEL.selector = module;
