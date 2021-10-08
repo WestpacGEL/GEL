b8185784d63c9b8575d4ceca54562c21f808925d
Jonny Stening <info@jonathanstening.com>
2021-10-08 19:45:13 +1100

    refactor(form): fix address manual demos, add missing autocomplete attr, export address pattern and reuse

--- components/form/demos/design/address-manual-error.js
+++ components/form/demos/design/address-manual-error.js
@@ -1,79 +1,26 @@
 /** @jsx jsx */
 
 import { jsx, useMediaQuery } from '@westpac/core';
-import { Form, FormGroup, Field, Fieldset, InputCluster, Item } from '@westpac/form';
-import { TextInput, Select } from '@westpac/text-input';
+import { Form, Fieldset } from '@westpac/form';
 import { Fork, Content } from '@westpac/fork';
-import { Fragment } from 'react';
 
 import { Container, FormHeading } from './_utils';
 import { Playground } from '../../../../website/src/components/playground/macro';
-
-const Address = ({ hint }) => {
-	const hintId = hint ? 'street-hint' : undefined;
-	return (
-		<Fragment>
-			<FormGroup>
-				<Fieldset
-					legend="Street"
-					hint={hint ? 'Not a PO Box' : undefined}
-					hintIdPrefix={hintId}
-					ariadescribedby={hintId}
-				>
-					<InputCluster>
-						<Item>
-							<Field hideLabel label="Line 1 of 2">
-								<TextInput size="large" />
-							</Field>
-						</Item>
-						<Item>
-							<Field hideLabel label="Line 2 of 2">
-								<TextInput size="large" />
-							</Field>
-						</Item>
-					</InputCluster>
-				</Fieldset>
-			</FormGroup>
-			<FormGroup>
-				<Field label="Suburb" error="Error message goes here if activated">
-					<TextInput size="large" width={20} invalid />
-				</Field>
-			</FormGroup>
-			<FormGroup>
-				<Field label="State" error="Error message goes here if activated">
-					<Select size="large" width={10} invalid>
-						<option>Select</option>
-						<option>NSW</option>
-						<option>VIC</option>
-						<option>QLD</option>
-						<option>ACT</option>
-						<option>SA</option>
-						<option>WA</option>
-						<option>NT</option>
-					</Select>
-				</Field>
-			</FormGroup>
-			<FormGroup>
-				<Field label="Postcode" error="Error message goes here if activated">
-					<TextInput size="large" width={4} invalid />
-				</Field>
-			</FormGroup>
-		</Fragment>
-	);
-};
+import { AddressManual } from './address-manual';
 
 const Demo = ({ context, showCode, showDemo }) => {
 	const mq = useMediaQuery();
+
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
 			<Container>
 				<Form spacing="large">
-					<Address hint />
+					<AddressManual showErrors />
 					<Fieldset legend="Do you have a different mailing address?">
 						<Fork size="large" css={mq({ marginBottom: ['1.5rem', '1.875rem'] })}>
 							<Content text="Yes">
 								<FormHeading>Mailing address</FormHeading>
-								<Address />
+								<AddressManual mailing showErrors />
 							</Content>
 							<Content text="No" />
 						</Fork>
--- components/form/demos/design/address-manual.js
+++ components/form/demos/design/address-manual.js
@@ -1,45 +1,48 @@
 /** @jsx jsx */
 
-import { jsx, useMediaQuery } from '@westpac/core';
+import { jsx } from '@westpac/core';
 import { Form, FormGroup, Field, Fieldset, InputCluster, Item } from '@westpac/form';
 import { TextInput, Select } from '@westpac/text-input';
 import { Container } from './_utils';
 import { Playground } from '../../../../website/src/components/playground/macro';
 import { Fragment } from 'react';
 
-const Address = ({ hint }) => {
-	const hintId = hint ? 'street-hint' : undefined;
+export const AddressManual = ({ showErrors = false, mailing = false }) => {
+	const error = showErrors ? 'Error message goes here if activated' : '';
+	const invalid = showErrors;
+	const token = mailing ? 'shipping ' : '';
+
 	return (
 		<Fragment>
 			<FormGroup>
-				<Fieldset
-					legend="Street"
-					hint={hint ? 'Not a PO Box' : undefined}
-					hintIdPrefix={hintId}
-					ariadescribedby={hintId}
-				>
+				<Fieldset legend="Street" hint="Not a PO Box" error={error}>
 					<InputCluster>
 						<Item>
-							<Field hideLabel label="Line 1 of 2">
-								<TextInput size="large" />
+							<Field label="Line 1 of 2" hideLabel>
+								<TextInput size="large" autocomplete={`${token}address-line1`} invalid={invalid} />
 							</Field>
 						</Item>
 						<Item>
-							<Field hideLabel label="Line 2 of 2">
-								<TextInput size="large" />
+							<Field label="Line 2 of 2" hideLabel>
+								<TextInput size="large" autocomplete={`${token}address-line2`} />
 							</Field>
 						</Item>
 					</InputCluster>
 				</Fieldset>
 			</FormGroup>
 			<FormGroup>
-				<Field label="Suburb">
-					<TextInput size="large" width={20} />
+				<Field label="Suburb" error={error}>
+					<TextInput
+						size="large"
+						width={20}
+						autocomplete={`${token}address-level2`}
+						invalid={invalid}
+					/>
 				</Field>
 			</FormGroup>
 			<FormGroup>
-				<Field label="State">
-					<Select size="large" width={10}>
+				<Field label="State" error={error}>
+					<Select size="large" width={10} autocomplete={`${token}address-level1`} invalid={invalid}>
 						<option>Select</option>
 						<option>NSW</option>
 						<option>VIC</option>
@@ -52,8 +55,13 @@ const Address = ({ hint }) => {
 				</Field>
 			</FormGroup>
 			<FormGroup>
-				<Field label="Postcode">
-					<TextInput size="large" width={4} />
+				<Field label="Postcode" error={error}>
+					<TextInput
+						size="large"
+						width={4}
+						autocomplete={`${token}postal-code`}
+						invalid={invalid}
+					/>
 				</Field>
 			</FormGroup>
 		</Fragment>
@@ -61,12 +69,11 @@ const Address = ({ hint }) => {
 };
 
 const Demo = ({ context, showCode, showDemo }) => {
-	const mq = useMediaQuery();
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
 			<Container>
 				<Form spacing="large">
-					<Address hint />
+					<AddressManual />
 				</Form>
 			</Container>
 		</Playground>
