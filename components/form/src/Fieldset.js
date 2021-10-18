301d001cf646cfe8fa3473f6420fcc21aef89954
Jonny Stening <info@jonathanstening.com>
2021-10-18 12:27:56 +1100

    fix(form): reorder fieldset error/hint aria-describedby values, add role="list" to list type error messages

--- components/form/src/Fieldset.js
+++ components/form/src/Fieldset.js
@@ -42,7 +42,7 @@ export const Fieldset = ({
 	}, [instanceId]);
 
 	useEffect(() => {
-		const arr = [...(hint ? [`${id}-hint`] : []), ...(error ? [`${id}-error`] : [])];
+		const arr = [...(error ? [`${id}-error`] : []), ...(hint ? [`${id}-hint`] : [])];
 		setAriaDescribedByValue(arr.join(' '));
 	}, [id, hint, error]);
 
