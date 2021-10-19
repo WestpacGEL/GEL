42519cb3ce0d7fe64a1c0675f38fb28bd33aa2a3
Jonny Stening <info@jonathanstening.com>
2021-10-19 13:09:36 +1100

    refactor(form): reuse date of birth pattern component

--- components/form/demos/design/date-of-birth-error.js
+++ components/form/demos/design/date-of-birth-error.js
@@ -1,61 +1,17 @@
 /** @jsx jsx */
 
 import { jsx } from '@westpac/core';
-import { Form, FormGroup, Field, Fieldset, InputCluster, Item } from '@westpac/form';
-import { TextInput } from '@westpac/text-input';
+import { Form } from '@westpac/form';
 import { Container } from './_utils';
 import { Playground } from '../../../../website/src/components/playground/macro';
+import { DateOfBirthPattern } from './date-of-birth';
 
 const Demo = ({ context, showCode, showDemo }) => {
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
 			<Container>
 				<Form spacing="large">
-					<FormGroup>
-						<Fieldset
-							legend="Date of birth"
-							hint="Example: 31 3 1980"
-							error={['Day must be valid', 'Year must be valid']}
-						>
-							<InputCluster horizontal>
-								<Item>
-									<Field label="Day" subLabel>
-										<TextInput
-											type="number"
-											size="large"
-											width={2}
-											invalid
-											defaultValue={33}
-											autoComplete="bday-day"
-										/>
-									</Field>
-								</Item>
-								<Item>
-									<Field label="Month" subLabel>
-										<TextInput
-											type="number"
-											size="large"
-											width={2}
-											defaultValue={'02'}
-											autoComplete="bday-month"
-										/>
-									</Field>
-								</Item>
-								<Item>
-									<Field label="Year" subLabel>
-										<TextInput
-											type="number"
-											size="large"
-											width={4}
-											invalid
-											defaultValue={3000}
-											autoComplete="bday-year"
-										/>
-									</Field>
-								</Item>
-							</InputCluster>
-						</Fieldset>
-					</FormGroup>
+					<DateOfBirthPattern showErrors />
 				</Form>
 			</Container>
 		</Playground>
--- components/form/demos/design/date-of-birth.js
+++ components/form/demos/design/date-of-birth.js
@@ -5,33 +5,77 @@ import { Form, FormGroup, Field, Fieldset, InputCluster, Item } from '@westpac/f
 import { TextInput } from '@westpac/text-input';
 import { Container } from './_utils';
 import { Playground } from '../../../../website/src/components/playground/macro';
+import { Fragment } from 'react';
+
+export const DateOfBirthPattern = ({ showErrors = false }) => {
+	const error = showErrors ? ['Day must be valid', 'Year must be valid'] : [];
+	const invalid = showErrors;
+
+	const minBorn = new Date().getFullYear() - 122; // https://en.wikipedia.org/wiki/Jeanne_Calment
+	const maxBorn = new Date().getFullYear();
+
+	return (
+		<Fragment>
+			<FormGroup>
+				<Fieldset legend="Date of birth" hint="Example: 31 3 1980" error={error}>
+					<InputCluster horizontal>
+						<Item>
+							<Field label="Day" subLabel>
+								<TextInput
+									type="text"
+									inputMode="numeric"
+									size="large"
+									width={2}
+									pattern="[0-9]*"
+									min="1"
+									max="31"
+									autoComplete="bday-day"
+									invalid={invalid}
+								/>
+							</Field>
+						</Item>
+						<Item>
+							<Field label="Month" subLabel>
+								<TextInput
+									type="text"
+									inputMode="numeric"
+									size="large"
+									width={2}
+									pattern="[0-9]*"
+									min="1"
+									max="12"
+									autoComplete="bday-month"
+								/>
+							</Field>
+						</Item>
+						<Item>
+							<Field label="Year" subLabel>
+								<TextInput
+									type="text"
+									inputMode="numeric"
+									size="large"
+									width={4}
+									pattern="[0-9]*"
+									min={minBorn}
+									max={maxBorn}
+									autoComplete="bday-year"
+									invalid={invalid}
+								/>
+							</Field>
+						</Item>
+					</InputCluster>
+				</Fieldset>
+			</FormGroup>
+		</Fragment>
+	);
+};
 
 const Demo = ({ context, showCode, showDemo }) => {
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
 			<Container>
 				<Form spacing="large">
-					<FormGroup>
-						<Fieldset legend="Date of birth" hint="Example: 31 3 1980">
-							<InputCluster horizontal>
-								<Item>
-									<Field label="Day" subLabel>
-										<TextInput type="number" size="large" width={2} autoComplete="bday-day" />
-									</Field>
-								</Item>
-								<Item>
-									<Field label="Month" subLabel>
-										<TextInput type="number" size="large" width={2} autoComplete="bday-month" />
-									</Field>
-								</Item>
-								<Item>
-									<Field label="Year" subLabel>
-										<TextInput type="number" size="large" width={4} autoComplete="bday-year" />
-									</Field>
-								</Item>
-							</InputCluster>
-						</Fieldset>
-					</FormGroup>
+					<DateOfBirthPattern />
 				</Form>
 			</Container>
 		</Playground>
