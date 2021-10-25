e8ee4d982433e93887c76c3d9228ed5b66727989
Jonny Stening <info@jonathanstening.com>
2021-10-25 17:40:48 +1100

    refactor(form): reuse address manual mailing pattern component

--- components/form/demos/design/address-manual-mailing.js
+++ components/form/demos/design/address-manual-mailing.js
@@ -7,23 +7,29 @@ import { Container, FormHeading } from './_utils';
 import { Playground } from '../../../../website/src/components/playground/macro';
 import { AddressManualPattern } from './address-manual';
 
-const Demo = ({ context, showCode, showDemo }) => {
+export const AddressManualMailingPattern = ({ showErrors }) => {
 	const mq = useMediaQuery();
 
+	return (
+		<Fieldset legend="Do you have a different mailing address?">
+			<Fork size="large" css={mq({ marginBottom: ['1.5rem', '1.875rem'] })}>
+				<Content text="Yes">
+					{/* A11y: tabindex="0" so screen readers announce the heading in Forms mode */}
+					<FormHeading tabIndex="0">Mailing address</FormHeading>
+					<AddressManualPattern mailing showErrors={showErrors} />
+				</Content>
+				<Content text="No" />
+			</Fork>
+		</Fieldset>
+	);
+};
+
+const Demo = ({ context, showCode, showDemo }) => {
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
 			<Container>
 				<Form spacing="large">
-					<Fieldset legend="Do you have a different mailing address?">
-						<Fork size="large" css={mq({ marginBottom: ['1.5rem', '1.875rem'] })}>
-							<Content text="Yes">
-								{/* A11y: tabindex="0" so screen readers announce the heading in Forms mode */}
-								<FormHeading tabIndex="0">Mailing address</FormHeading>
-								<AddressManualPattern mailing />
-							</Content>
-							<Content text="No" />
-						</Fork>
-					</Fieldset>
+					<AddressManualMailingPattern />
 				</Form>
 			</Container>
 		</Playground>
