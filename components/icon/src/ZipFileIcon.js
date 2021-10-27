d602ea557fbca3208a5081291d29a210c6ffcf5d
Jonny Stening <info@jonathanstening.com>
2021-10-27 13:25:17 +1100

    fix(icon): update ZipFileIcon assistive text

--- components/icon/src/ZipFileIcon.js
+++ components/icon/src/ZipFileIcon.js
@@ -13,7 +13,7 @@ export const ZipFileIcon = (props) => (
 
 ZipFileIcon.defaultProps = {
 	...defaultProps,
-	assistiveText: 'ZipFile',
+	assistiveText: 'Zip file',
 	copyrightYear: '2020',
 };
 ZipFileIcon.propTypes = propTypes;
