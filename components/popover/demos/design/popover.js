784d3d318533c65d8c26483e68753a7d186d110a
Jeremy Ortiz <jeremy@jeremyortiz.io>
2021-12-12 19:17:43 +1100

    popover: update demo text

--- components/popover/demos/design/popover.js
+++ components/popover/demos/design/popover.js
@@ -6,12 +6,13 @@ import { Playground } from '../../../../website/src/components/playground/macro'
 
 const Demo = ({ context, showCode, showDemo }) => {
 	const content = `Sudden and magnificent, the sun's broad golden disc showed itself over the horizon facing them; and the first rays, shooting across the level water-meadows, took the animals full in the eyes and dazzled them. When they were able to look once more, the Vision had vanished, and the air was full of the carol of birds that hailed the dawn.`;
+	const shortContent = `Sudden and magnificent, the sun's broad golden disc showed itself over the horizon facing them.`;
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
 			<Popover heading="Sudden and magnificent" content={content} look="primary" soft>
 				With heading
 			</Popover>{' '}
-			<Popover content={content} look="primary" soft>
+			<Popover content={shortContent} look="primary" soft>
 				No heading
 			</Popover>
 		</Playground>
