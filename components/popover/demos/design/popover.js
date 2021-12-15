a922b4a7f8f82a7be225b692994dbd7ba348fe82
Jeremy Ortiz <jeremy@jeremyortiz.io>
2021-12-15 12:22:08 +1100

    popover: update demo

--- components/popover/demos/design/popover.js
+++ components/popover/demos/design/popover.js
@@ -1,20 +1,44 @@
 /** @jsx jsx */
 
-import { jsx } from '@westpac/core';
+import { jsx, useBrand } from '@westpac/core';
 import { Popover } from '@westpac/popover';
 import { Playground } from '../../../../website/src/components/playground/macro';
+import { HelpIcon } from '@westpac/icon';
+import { FormLabel } from '@westpac/form';
+
+const Label = (props) => <FormLabel css={{ display: 'inline-block' }} {...props} />;
+
+const Wrapper = ({ margin, ...props }) => (
+	<div css={{ display: 'inline-block', marginRight: margin && '5rem' }} {...props} />
+);
 
 const Demo = ({ context, showCode, showDemo }) => {
-	const content = `Sudden and magnificent, the sun's broad golden disc showed itself over the horizon facing them; and the first rays, shooting across the level water-meadows, took the animals full in the eyes and dazzled them. When they were able to look once more, the Vision had vanished, and the air was full of the carol of birds that hailed the dawn.`;
-	const shortContent = `Sudden and magnificent, the sun's broad golden disc showed itself over the horizon facing them.`;
+	const { COLORS } = useBrand();
+	const content = `Small overlays of content for housing secondary information. These are often used to provide explanatory information for complex ideas.`;
+	const shortContent = `Small overlays of content for housing secondary information.`;
 	return (
 		<Playground context={context} showCode={showCode} showDemo={showDemo}>
-			<Popover heading="Sudden and magnificent" content={content} look="primary" soft>
-				With heading
-			</Popover>{' '}
-			<Popover content={shortContent} look="primary" soft>
-				No heading
-			</Popover>
+			<Wrapper margin>
+				<Label>With heading</Label>
+				<Popover
+					heading="Popup heading"
+					content={content}
+					look="link"
+					iconAfter={HelpIcon}
+					iconColor={COLORS.text}
+					size="large"
+				/>
+			</Wrapper>
+			<Wrapper>
+				<Label>Without heading</Label>
+				<Popover
+					content={shortContent}
+					look="link"
+					iconAfter={HelpIcon}
+					iconColor={COLORS.text}
+					size="large"
+				/>
+			</Wrapper>
 		</Playground>
 	);
 };
