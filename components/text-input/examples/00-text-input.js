6d75b3017c1ce71b6f780925b1ba8998b8646d0d
Jeremy Ortiz <jeremy@jeremyortiz.io>
2021-12-16 14:38:48 +1100

    text-input: show width demo

--- components/text-input/examples/00-text-input.js
+++ components/text-input/examples/00-text-input.js
@@ -17,13 +17,10 @@ function Example({ brand }) {
 		<GEL brand={brand}>
 			<h2>Default</h2>
 			<TextInput />
-
 			<hr />
-
 			<h2>Controlled</h2>
 			<TextInput defaultValue={value} onChange={handleChange} />
 			<hr />
-
 			<h2>Size</h2>
 			<TextInput size="small" placeholder="small" />
 			<br />
@@ -35,54 +32,108 @@ function Example({ brand }) {
 			<br />
 			<br />
 			<TextInput size="xlarge" placeholder="xlarge" />
-
 			<hr />
-
 			<h2>Invalid</h2>
 			<TextInput invalid />
-
 			<hr />
-
 			<h2>Disabled</h2>
 			<TextInput disabled />
 			<br />
 			<br />
 			<TextInput defaultValue="This input is disabled and contains a value" disabled />
-
 			<hr />
-
 			<h2>Read-only</h2>
 			<TextInput defaultValue="This value is readonly" readOnly />
-
 			<hr />
-
 			<h2>Inline</h2>
 			<Form action="#">
 				<TextInput inline /> <TextInput inline /> <Button type="submit">Submit</Button>
 			</Form>
-
 			<hr />
-
 			<h2>Fixed width</h2>
-			<TextInput width={2} placeholder={2} />
+			<h3>Size</h3>
+			<TextInput width={2} placeholder={2} size="small" />
 			<br />
 			<br />
-			<TextInput width={3} placeholder={3} />
+			<TextInput width={2} placeholder={2} size="medium" />
 			<br />
 			<br />
-			<TextInput width={4} placeholder={4} />
+			<TextInput width={2} placeholder={2} size="large" />
 			<br />
 			<br />
-			<TextInput width={5} placeholder={5} />
+			<TextInput width={2} placeholder={2} size="xlarge" />
 			<br />
 			<br />
-			<TextInput width={10} placeholder={10} />
+			<TextInput width={3} placeholder={3} size="small" />
 			<br />
 			<br />
-			<TextInput width={20} placeholder={20} />
+			<TextInput width={3} placeholder={3} size="medium" />
 			<br />
 			<br />
-			<TextInput width={30} placeholder={30} />
+			<TextInput width={3} placeholder={3} size="large" />
+			<br />
+			<br />
+			<TextInput width={3} placeholder={3} size="xlarge" />
+			<br />
+			<br />
+			<TextInput width={4} placeholder={4} size="small" />
+			<br />
+			<br />
+			<TextInput width={4} placeholder={4} size="medium" />
+			<br />
+			<br />
+			<TextInput width={4} placeholder={4} size="large" />
+			<br />
+			<br />
+			<TextInput width={4} placeholder={4} size="xlarge" />
+			<br />
+			<br />
+			<TextInput width={5} placeholder={5} size="small" />
+			<br />
+			<br />
+			<TextInput width={5} placeholder={5} size="medium" />
+			<br />
+			<br />
+			<TextInput width={5} placeholder={5} size="large" />
+			<br />
+			<br />
+			<TextInput width={5} placeholder={5} size="xlarge" />
+			<br />
+			<br />
+			<TextInput width={10} placeholder={10} size="small" />
+			<br />
+			<br />
+			<TextInput width={10} placeholder={10} size="medium" />
+			<br />
+			<br />
+			<TextInput width={10} placeholder={10} size="large" />
+			<br />
+			<br />
+			<TextInput width={10} placeholder={10} size="xlarge" />
+			<br />
+			<br />
+			<TextInput width={20} placeholder={20} size="small" />
+			<br />
+			<br />
+			<TextInput width={20} placeholder={20} size="medium" />
+			<br />
+			<br />
+			<TextInput width={20} placeholder={20} size="large" />
+			<br />
+			<br />
+			<TextInput width={20} placeholder={20} size="xlarge" />
+			<br />
+			<br />
+			<TextInput width={30} placeholder={30} size="small" />
+			<br />
+			<br />
+			<TextInput width={30} placeholder={30} size="medium" />
+			<br />
+			<br />
+			<TextInput width={30} placeholder={30} size="large" />
+			<br />
+			<br />
+			<TextInput width={30} placeholder={30} size="xlarge" />
 		</GEL>
 	);
 }
