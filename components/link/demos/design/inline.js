66f2a0f8c25ea0a93ba18f501c7ecd7447a9c998
Jeremy Ortiz <jeremy@jeremyortiz.io>
2022-05-09 10:13:09 +1000

    link: demo updates

--- components/link/demos/design/inline.js
+++ components/link/demos/design/inline.js
@@ -4,6 +4,7 @@ import { jsx } from '@westpac/core';
 import { Link } from '@westpac/link';
 import { Body } from '@westpac/body';
 import { Playground } from '../../../../website/src/components/playground/macro';
+import { PdfFileIcon, ArrowRightIcon } from '@westpac/icon';
 
 const Demo = ({ context, showCode, showDemo }) => {
 	return (
@@ -12,7 +13,25 @@ const Demo = ({ context, showCode, showDemo }) => {
 				<p>
 					Lorem ipsum dolor{' '}
 					<Link href="#" type="inline">
-						Look, I’m an inline link
+						look, I’m an inline link
+					</Link>{' '}
+					sit amet consectetur, adipisicing elit. Libero facilis odit voluptate reprehenderit
+					laborum numquam ex optio doloribus magni repudiandae vero fugiat iusto tempora debitis
+					sunt laboriosam nobis, ut voluptatum?
+				</p>
+				<p>
+					Lorem ipsum dolor{' '}
+					<Link href="#" type="inline" iconBefore={PdfFileIcon}>
+						look, I’m an inline link
+					</Link>{' '}
+					sit amet consectetur, adipisicing elit. Libero facilis odit voluptate reprehenderit
+					laborum numquam ex optio doloribus magni repudiandae vero fugiat iusto tempora debitis
+					sunt laboriosam nobis, ut voluptatum?
+				</p>
+				<p>
+					Lorem ipsum dolor{' '}
+					<Link href="#" type="inline" iconAfter={ArrowRightIcon}>
+						look, I’m an inline link
 					</Link>{' '}
 					sit amet consectetur, adipisicing elit. Libero facilis odit voluptate reprehenderit
 					laborum numquam ex optio doloribus magni repudiandae vero fugiat iusto tempora debitis
