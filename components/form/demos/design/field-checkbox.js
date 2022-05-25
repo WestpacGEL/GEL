b156ba73bb8bb6e0c3a6f4118b5814bea6564cc0
Jeremy Ortiz <jeremy@jeremyortiz.io>
2022-05-25 14:41:03 +1000

    form: add field demos

---
+++ components/form/demos/design/field-checkbox.js
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+
+import { jsx } from '@westpac/core';
+import { Form, Field } from '@westpac/form';
+import { FormCheck, Option } from '@westpac/form-check';
+
+import { Playground } from '../../../../website/src/components/playground/macro';
+
+const Demo = ({ context, showCode, showDemo }) => {
+	return (
+		<Playground context={context} showCode={showCode} showDemo={showDemo}>
+			<Form>
+				<Field label="Label" hint="Hint text">
+					<FormCheck name="example-checkbox-radio">
+						<Option value="1">Option 1</Option>
+						<Option value="2">Option 2</Option>
+						<Option value="3">Option 3</Option>
+					</FormCheck>
+				</Field>
+			</Form>
+		</Playground>
+	);
+};
+
+export default Demo;
