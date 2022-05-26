dcf5f55a27b77557a1a3329c00080811d8f4cd3c
Daniel Cousens <dcousens@users.noreply.github.com>
2022-05-26 16:10:16 +1000

    add Articles migration

---
+++ website-backend/migrations/20220526060125_add_articles/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" TEXT NOT NULL,
+    "pageTitle" TEXT NOT NULL DEFAULT E'',
+    "author" TEXT,
+    "url" TEXT NOT NULL DEFAULT E'',
+    "content" JSONB NOT NULL DEFAULT E'[{"type":"paragraph","children":[{"text":""}]}]',
+    "cardTitle" TEXT NOT NULL DEFAULT E'',
+    "cardDescription" TEXT NOT NULL DEFAULT E'',
+    "cardDescriptionSecondary" TEXT NOT NULL DEFAULT E'',
+    "cardImage" TEXT NOT NULL DEFAULT E'',
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Article_author_idx" ON "Article"("author");
+
+-- AddForeignKey
+ALTER TABLE "Article" ADD CONSTRAINT "Article_author_fkey" FOREIGN KEY ("author") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
