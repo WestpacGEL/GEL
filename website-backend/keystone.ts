c3e91d218905c11f7ecc0f6f6b55d5e8af140ea1
Daniel Cousens <dcousens@users.noreply.github.com>
2022-05-26 15:07:24 +1000

    add my temporary debugging user

--- website-backend/keystone.ts
+++ website-backend/keystone.ts
@@ -3,6 +3,7 @@ import { statelessSessions } from '@keystone-6/core/session';
 import { createAuth } from '@keystone-6/auth';
 import { lists } from './schema';
 import { PORT, DATABASE_URL, SESSION_MAX_AGE, SESSION_SECRET } from './config';
+import { Context } from '.keystone/types';
 
 // Basic session and auth config
 const session = statelessSessions({
@@ -24,11 +25,12 @@ export default withAuth(
 		db: {
 			provider: 'postgresql',
 			url: DATABASE_URL,
-			// async onConnect(context) {
-			// 	if (process.argv.includes('--seed-data')) {
-			// 		await insertSeedData(context);
-			// 	}
-			// },
+			async onConnect(context) {
+				const _context = context as Context;
+				if (process.argv.includes('--seed-data')) {
+				_context.db.User.createOne({ data: { email: 'foo@daniel.cousens', password: 'foobar123' } })
+				}
+			},
 			useMigrations: true,
 		},
 		images: {
