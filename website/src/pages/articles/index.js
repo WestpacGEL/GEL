4eb73cd06a804bcf03ee76cd24ab7894dbb62f82
Jeremy Ortiz <jeremy@jeremy.ortiz.io>
2022-06-16 10:50:22 +1000

    website: add GEL logo to action bar

--- website/src/pages/articles/index.js
+++ website/src/pages/articles/index.js
@@ -29,7 +29,6 @@ TO DO
 - cards
     - hover
     - need to discuss min height stuff
-- sticky footer
 - spacing
     - double check spacing between homepage content top and bottom
     
@@ -514,6 +513,26 @@ const DotBrandGuidelineLogo = (props) => {
 	);
 };
 
+const GELLogo = (props) => (
+	<svg
+		width="45"
+		height="18"
+		viewBox="0 0 45 18"
+		fill="none"
+		role="img"
+		focusable="false"
+		xmlns="http://www.w3.org/2000/svg"
+		{...props}
+	>
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M0 9.0977V8.90267C0 3.58565 3.9039 0 9.1501 0C13.2742 0 16.934 1.8782 17.2756 6.41472H12.3225C12.0784 4.7805 11.0288 3.92677 9.17477 3.92677C6.71035 3.92677 5.22153 5.82963 5.22153 8.9759V9.17093C5.22153 12.0004 6.27077 14.1954 9.29663 14.1954C11.7368 14.1954 12.6638 12.9509 12.7617 11.561H9.49212V8.07323H17.6905V10.2197C17.6905 14.6587 14.7869 18 9.1501 18C3.22061 18 0 14.3172 0 9.0977ZM19.1549 0.268756H31.209V4.12192H24.1327V7.12211H29.7692V10.6831H24.1327V13.8537H31.5993V17.7072H19.1549V0.268756ZM38.1393 0.268756H33.1129V17.7072H44.7278V13.8294H38.1393V0.268756Z"
+			fill="#122935"
+		/>
+	</svg>
+);
+
 // ============================================================
 // Base
 // ============================================================
@@ -624,17 +643,19 @@ const ButtonIconOverride = ({ icon: Icon, left, right, color, state: _, ...rest 
 	<Icon color={COLORS.icon} {...rest} />
 );
 
-const ButtonDropdownOverride = (props) => {
+const ButtonDropdownOverride = ({ state: _, ...props }) => {
 	const { LAYOUT } = useBrand();
 	return (
 		<div
-			css={{ [`@media (min-width: ${LAYOUT.breakpoints.sm}px)`]: { display: 'none' } }}
+			css={{
+				[`@media (min-width: ${LAYOUT.breakpoints.sm}px)`]: { display: 'none' },
+			}}
 			{...props}
 		/>
 	);
 };
 
-const ButtonOverride = forwardRef(({ closed, setClosed, state: _, ...rest }, ref) => {
+const ButtonOverride = forwardRef(({ closed, setClosed, state: _, children, ...rest }, ref) => {
 	const {
 		state: { isOpen },
 	} = useButtonDropdownContext();
@@ -649,10 +670,17 @@ const ButtonOverride = forwardRef(({ closed, setClosed, state: _, ...rest }, ref
 			overrides={{
 				Icon: {
 					component: ButtonIconOverride,
+					styles: (styles) => ({
+						...styles,
+						marginLeft: '1rem',
+					}),
 				},
 			}}
 			{...rest}
-		/>
+		>
+			<GELLogo css={{ marginRight: '0.625rem' }} />
+			{children}
+		</Button>
 	);
 });
 
@@ -735,11 +763,12 @@ const ActionBarDropdown = (props) => {
 						mq({
 							position: 'relative',
 							zIndex: 2,
-							padding: ['0 0.75rem', '0 1.875rem'],
+							padding: ['0 0.75rem 0.75rem', '0 1.875rem 0.75rem'],
 							height: '4.125rem',
 							fontSize: '1rem',
 							backgroundColor: '#fff',
 							justifyContent: 'flex-start',
+							alignItems: 'flex-end',
 							':focus': {
 								outlineOffset: `-2px !important`,
 							},
@@ -805,13 +834,17 @@ const ActionBarDesktop = () => {
 					css={{
 						height: '100%',
 						display: 'flex',
-						alignItems: 'flex-end',
+						flexDirection: 'column',
+						justifyContent: 'flex-end',
+
 						borderRight: `1px solid ${COLORS.border}`,
 						marginRight: '1.5rem',
 					}}
 				>
-					{/* <BackToGelSvg /> */}
-					Design System <ArrowRightIcon css={{ margin: '0 1.5rem 0 1.25rem' }} />
+					<GELLogo css={{ marginBottom: '0.875rem' }} />
+					<div css={{ display: 'flex', alignItems: 'flex-end' }}>
+						Design System <ArrowRightIcon css={{ margin: '0 1.5rem 0 1.25rem' }} />
+					</div>
 				</div>
 			</a>
 			<ul role="list" css={{ display: 'flex', paddingLeft: 0, listStyle: 'none', margin: 0 }}>
