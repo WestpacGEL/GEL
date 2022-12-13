7f927f374ae3dbe1408aba1c7480ca71481f5e4f
Amos, David J <david.amos@westpac.com.au>
2022-12-14 10:58:34 +1100

    Initial tests for the Body component

--- components/body/tests/unit/body.spec.js
+++ components/body/tests/unit/body.spec.js
@@ -1,3 +1,38 @@
-test('TODO replace this', () => {
-	expect(true).toBe(true);
+import React from 'react';
+import { Body } from '@westpac/body';
+import { render } from '@testing-library/react';
+
+import { overridesTest } from '../../../../helpers/tests/overrides-test.js';
+import { nestingTest } from '../../../../helpers/tests/nesting-test.js';
+import { GEL } from '@westpac/core';
+import wbc from '@westpac/wbc';
+
+// The default tests every component should run
+overridesTest({
+	name: 'body', // the name has to be the package name without '@westpac/' scope
+	overrides: ['Body'], // every single override root key
+	Component: (props) => <Body {...props}>Body content</Body>, // the component with all components rendered
+});
+
+// another default test to check that the component errors when outside of GEL and renders when inside
+nestingTest({
+	name: 'body',
+	Component: (props) => <Body {...props} />,
+});
+
+describe('Body specific tests', () => {
+	test('Body renders its content', () => {
+		const text = 'Hello';
+		const content = <span>{text}</span>;
+		const SimpleBody = () => (
+			<GEL brand={wbc}>
+				<Body>{content}</Body>
+			</GEL>
+		);
+
+		const { container } = render(<SimpleBody />);
+
+		expect(container).toHaveTextContent(text);
+		expect(container).toContainHTML('span');
+	});
 });
