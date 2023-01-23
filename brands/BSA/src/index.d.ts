a4a81085ff7d48483da39681130ca727b08c0e01
Robertson, Nathan <nathan.robertson@westpac.com>
2023-01-23 14:30:50 +1000

    Renamed the CoreProps interface. Removed some temporary interfaces, Updated Brand Interfaces.

--- brands/BSA/src/index.d.ts
+++ brands/BSA/src/index.d.ts
@@ -342,7 +342,12 @@ declare module '@westpac/bsa' {
 			outlineOffset: string;
 		};
 	};
-	type Overrides = (tokens: string) => void;
+	type Overide = {
+		stypes: any;
+		component: any;
+		attributes: any;
+	};
+	type Overrides = Record<string, Overide>;
 	type Spacing = {
 		major: string;
 		minor: string;
