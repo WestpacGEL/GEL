777d60a1f020b16d520e38710b6e1c99b247f649
Giovanni Kenji Shiroma <kenjishiroma@Giovannis-MacBook-Pro.local>
2023-02-01 11:59:43 +1000

    feat(WLT-14195): adding cypress test to check if invalid has a red border

--- components/autocomplete/tests/integration/autocomplete.cypress.js
+++ components/autocomplete/tests/integration/autocomplete.cypress.js
@@ -1,5 +1,11 @@
+import DefaultPage from '../pages/default.page';
+
+const defaultPage = new DefaultPage();
 describe('Autocomplete', () => {
 	before(() => {
-		cy.visit(`http://localhost:8080/`);
+		defaultPage.visit();
+	});
+	it('should contain a red border when there is a prop invalid', () => {
+		defaultPage.invalidAutocomplete.get('[class$="Control"]').should('have.css', 'border', '1px solid rgb(147, 144, 162)');
 	});
 });
---
+++ components/autocomplete/tests/pages/default.page.ts
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+import CommonPage from './common.page';
+
+class DefaultPage extends CommonPage {
+	constructor() {
+		super('http://localhost:8080/autocomplete/default');
+	}
+	get invalidAutocomplete(){
+		return cy.get('[data-cy="invalid-autocomplete"]');
+	}
+}
+
+export default DefaultPage;
