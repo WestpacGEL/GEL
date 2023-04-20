7ced832fc726004aaaa505928d5c50ceb5274288
Jeremy Ortiz <jeremy@jeremy.ortiz.io>
2023-04-20 15:49:55 +1000

    feature: initial repeater a11y review updates

--- components/repeater/src/Repeater.tsx
+++ components/repeater/src/Repeater.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { jsx, useBrand, overrideReconciler } from '@westpac/core';
 import { useTransition, animated } from '@react-spring/web';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useRef, useEffect } from 'react';
 
 import { defaultRemoveBtn } from './overrides/removeBtn';
 import { defaultAddBtn } from './overrides/addBtn';
@@ -28,6 +28,10 @@ export interface RepeaterProps {
 	 * Enable separator version
 	 */
 	separator?: boolean;
+	/**
+	 * Index heading tag to use for index on separator version
+	 */
+	indexTag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 	/**
 	 * The override API
 	 */
@@ -65,6 +69,11 @@ export interface RepeaterProps {
 	};
 }
 
+interface Action {
+	type: string;
+	index: number;
+}
+
 // ==============================
 // Component
 // ==============================
@@ -72,6 +81,7 @@ export interface RepeaterProps {
 export const Repeater = ({
 	separator,
 	addText = 'Add another item',
+	indexTag = 'h3',
 	children,
 	overrides: componentOverrides,
 	...rest
@@ -93,25 +103,42 @@ export const Repeater = ({
 	};
 
 	const [items, setItems] = useState([{ id: generateID() }]);
+	const [action, setAction] = useState<Action>({ type: '', index: 0 });
+	const refArr = useRef(new Array());
 
 	const state = {
 		separator,
 		addText,
+		indexTag,
 		overrides: componentOverrides,
 		...rest,
 	};
 
 	const handleAdd = useCallback(() => {
 		setItems([...items, { id: generateID() }]);
+		setAction({ type: 'add', index: items.length });
 	}, [items]);
 
 	const handleRemove = useCallback(
-		(id: string) => {
+		(id: string, index: number) => {
 			const newItems = items.filter((item) => item.id !== id);
 			setItems(newItems);
+			setAction({ type: 'remove', index });
 		},
 		[items]
 	);
+
+	useEffect(() => {
+		if (action.type) {
+			if (action.type === 'add') {
+				refArr.current[items.length - 1].focus();
+			} else {
+				refArr.current.splice(action.index, 1);
+				const focusIndex = action.index === 0 ? 0 : action.index - 1;
+				refArr.current[focusIndex].focus();
+			}
+		}
+	}, [items.length, action]);
 
 	const {
 		Repeater: { component: Repeater, styles: repeaterStyles, attributes: repeaterAttributes },
@@ -143,9 +170,12 @@ export const Repeater = ({
 			<List state={state} {...listAttributes(state)} css={listStyles(state)}>
 				{transition((style, item, t, index) => {
 					return (
-						<animated.div style={style}>
+						<animated.li style={style}>
 							<Item
-								key={item.id}
+								ref={(el: HTMLElement) => {
+									refArr.current[index] = el;
+								}}
+								tabIndex="-1"
 								index={index}
 								state={state}
 								{...itemAttributes(state)}
@@ -165,8 +195,8 @@ export const Repeater = ({
 								</Content>
 								{items.length > 1 && (
 									<RemoveBtn
-										onClick={() => handleRemove(item.id)}
-										state={state}
+										onClick={() => handleRemove(item.id, index)}
+										state={{ ...state, id: item.id }}
 										{...RemoveBtnAttributes(state)}
 										css={RemoveBtnStyles(state)}
 									>
@@ -174,7 +204,7 @@ export const Repeater = ({
 									</RemoveBtn>
 								)}
 							</Item>
-						</animated.div>
+						</animated.li>
 					);
 				})}
 			</List>
