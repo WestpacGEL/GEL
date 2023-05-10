72995b0f1b31393a86b866cb88c3edf8d46ef29d
Duwon <72375200+Duwon98@users.noreply.github.com>
2023-05-10 14:04:46 +1000

    Update components/table/tests/unit/tbody.spec.tsx
    Co-authored-by: Tom King <46306101+tomjackking@users.noreply.github.com>

--- components/table/tests/unit/tbody.spec.tsx
+++ components/table/tests/unit/tbody.spec.tsx
@@ -5,7 +5,9 @@ import { nestingTest } from '../../../../helpers/tests/nesting-test.js';
 import { TbodyProps } from '../../src/Tbody';
 import wbc from '@westpac/wbc';
 
-// Todo: generic override function cause error for table component **fix override function for table component
+// TODO: Fix overridesTest function to work with table
+// There is no overridesTest function for the various table components due to how the overridesTest works,
+// creating warnings relating to unexpected <div> as children/parents
 
 // another default test to check that the component errors when outside of GEL and renders when inside
 nestingTest({
