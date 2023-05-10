114d863cffbc63397c3aa55bc05bd624553f1ff2
Duwon <72375200+Duwon98@users.noreply.github.com>
2023-05-10 14:06:43 +1000

    Update components/table/tests/unit/thead.spec.tsx
    Co-authored-by: Tom King <46306101+tomjackking@users.noreply.github.com>

--- components/table/tests/unit/thead.spec.tsx
+++ components/table/tests/unit/thead.spec.tsx
@@ -1,7 +1,6 @@
 import { Thead, Table, Tr } from '@westpac/table';
 import { GEL } from '@westpac/core';
 import { render } from '@testing-library/react';
-import { overridesTest } from '../../../../helpers/tests/overrides-test.js';
 import { nestingTest } from '../../../../helpers/tests/nesting-test.js';
 import { TheadProps } from '../../src/Thead';
 import wbc from '@westpac/wbc';
