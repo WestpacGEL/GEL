436e9759159edfef1afe53f352f648242dcf1a3c
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2023-06-12 17:13:07 +1000

    fix

--- components/flexi-cell/tests/unit/flexi-cell-adornment.spec.tsx
+++ components/flexi-cell/tests/unit/flexi-cell-adornment.spec.tsx
@@ -13,13 +13,14 @@ const SimpleFlexiCellAdornment = (props: FlexiCellAdornmentProps) => (
 describe.only('Given the FlexiCellAdornment is rendered', () => {
 	describe('when default props are defined', () => {
 		test('then the component should be displayed', () => {
-			const { getByText } = render(
-				<SimpleFlexiCellAdornment tag="aside">
+			const { getByText, getByRole } = render(
+				<SimpleFlexiCellAdornment tag="button">
 					<FlexiCell.Label>label</FlexiCell.Label>
 				</SimpleFlexiCellAdornment>
 			);
 
-			expect(getByText('label', { selector: 'aside' })).toBeVisible();
+			expect(getByRole('button')).toBeVisible();
+			expect(getByText('label')).toBeVisible();
 		});
 	});
 });
