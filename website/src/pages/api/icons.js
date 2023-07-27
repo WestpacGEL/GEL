795dc6fe43b4f6d0a11f26355b0d0e4d83f57b2f
Jeremy Ortiz <jeremy@jeremy.ortiz.io>
2023-07-27 22:29:09 +1000

    website: debugging icon api endpoint

--- website/src/pages/api/icons.js
+++ website/src/pages/api/icons.js
@@ -1,20 +1,29 @@
 import fs from 'fs';
+import path from 'path';
 
 export default async function handler(req, res) {
 	try {
-		const publicDir = __dirname.split('.next')[0] + 'public/';
-		const filePath = publicDir + 'design-system/icons/GEL_icons.zip';
-		const buffer = fs.createReadStream(filePath);
+		const filePath = path.join(process.cwd(), '/public/design-system/icons/GEL_icon.zip');
+		const exists = fs.existsSync(filePath);
 
-		await new Promise(function (resolve) {
-			res.setHeader('Content-Type', 'application/zip');
-			buffer.pipe(res);
-			buffer.on('end', resolve);
-			buffer.on('error', function (err) {
-				res.status(500).json({ error: true, message: 'Sorry, something went wrong!' });
-				res.end();
-			});
-		});
+		const publicDir = __dirname.split('.next')[0] + 'public/';
+		const filePath2 = publicDir + 'design-system/icons/GEL_icons.zip';
+		const exists2 = fs.existsSync(filePath2);
+
+		res
+			.status(200)
+			.json({ exists: exists, filePath: filePath, exists2: exists2, filePath2: filePath2 });
+		// const buffer = fs.createReadStream(filePath);
+
+		// await new Promise(function (resolve) {
+		// 	res.setHeader('Content-Type', 'application/zip');
+		// 	buffer.pipe(res);
+		// 	buffer.on('end', resolve);
+		// 	buffer.on('error', function (err) {
+		// 		res.status(500).json({ error: true, message: err });
+		// 		res.end();
+		// 	});
+		// });
 	} catch (err) {
 		res.status(400).json({ error: true, message: err });
 		res.end();
