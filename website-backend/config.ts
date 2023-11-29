793b9c4cae5adef4c0074e38bf0942bc99fce726
Josh Calder <8251494+borisno2@users.noreply.github.com>
2023-11-29 11:38:43 +1100

    use default env file location

--- website-backend/config.ts
+++ website-backend/config.ts
@@ -1,18 +1,8 @@
-import fs from 'fs';
-import path from 'path';
 import dotenv from 'dotenv';
 
-// Locate our env vars
-// If the app is running in NODE_ENV=production we're probably deployed
-// Check for a config.env a few dirs above the CWD, just outside the repo root
-const isProd = process.env.NODE_ENV === 'production';
-const prodConfigPath = path.resolve(process.cwd(), '../../website-backend.env');
-const configPath =
-	isProd && fs.existsSync(prodConfigPath) ? prodConfigPath : path.resolve(process.cwd(), '.env');
-
 // Import our config; this should happen before (most of) the other requires
-console.log(`Loading env vars from ${configPath}`);
-dotenv.config({ path: configPath });
+console.log(`Loading env vars`);
+dotenv.config();
 
 if (
 	!process.env.CLOUDINARY_CLOUD_NAME ||
